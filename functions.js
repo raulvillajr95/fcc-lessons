866c403df298c6a6aae366784b9c072393e68156
Raul <raulvillajr95@gmail.com>
2022-03-14 22:19:33 -0700

    learning about labels for inputs

--- functions.js
+++ functions.js
@@ -2,7 +2,9 @@ const months = ['March', 'Jan', 'Feb', 'Dec'];
 months.sort();
 
 const array1 = [1, 30, 4, 21, 100000]
-array1.sort();
+array1.sort(function(a,b) {
+  return a - b;
+});
 
 
 console.log(months);
