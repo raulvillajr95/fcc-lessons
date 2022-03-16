8b297166d3cece545bd50276b6d3d37bc2a636b6
Raul <raulvillajr95@gmail.com>
2022-03-15 23:04:56 -0700

    trying to solve the challenge where from 2 arrays, I must find what the different item is

--- functions.js
+++ functions.js
@@ -1,21 +1,23 @@
 
-let aquaticCreatures = creatures.filter(creature => creature.habitat == "Ocean")
-
-console.log(aquaticCreatures)
-
-
-
 /*
+let freelancers = [{name: "Harry", skill: "JavaScript"},{name: "Mark", skill: "Python"},{name: "David", skill:"JavaScript"}];
+
+let javascript_freelancers = freelancers.filter(x => x.skill == "JavaScript")
+console.log(javascript_freelancers)
+*/
+
+
 function diffArray(arr1, arr2) {
-  const newArr = [];
-  let ar1 = arr1.filter(function(x) {
-    
+  let merged = arr1.concat(arr2)
+  let newArr = merged.filter(function(n){
+    return arr1.includes(n) === false && !arr2.includes(n) === false
   })
-  return ar1;
+
+
+  return newArr
 }
 
-console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
-*/
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
 
 /*
 ideas 
@@ -23,4 +25,5 @@ ideas
 -test if each item in arr1 is in arr2, using some()??
 -if false, add to new arr?
 -repeat but now test arr2 items in arr1
+-test different arrays
 */
