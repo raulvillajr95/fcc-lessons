3662b8c437b7fd730c1953f2601489982b5e7ef0
Raul <raulvillajr95@gmail.com>
2022-03-15 19:27:18 -0700

    learning about the 'some' method

--- functions.js
+++ functions.js
@@ -1,11 +1,10 @@
-let x = function(x, y) {
-  return x * y;
-}
-
 let z = (x, y) => x * y;
 
 function checkPositive(arr) {
-  return arr.every(currVal => currVal > 0)
+  let positives = arr.filter(n => n > 0)
+  return positives.some(currVal => currVal % 2 === 0)
 }
 
-console.log(checkPositive([1, 2, 3, -4, 5]));
+console.log(checkPositive([-1, -2, -3, -4, -5]));
+
+console.log(-4 % 2)
