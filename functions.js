fed826c93a2e46eb3a5e8783f8618ce4d1c0b32b
Raul <raulvillajr95@gmail.com>
2022-03-15 21:10:43 -0700

    learning about fieldsets and legends

--- functions.js
+++ functions.js
@@ -1,17 +1,15 @@
-function sumAll(arr) {
-  let sorted = arr.sort((a,b) => a - b);
-  let nums = [];
-  for (let i = sorted[0]; i <= sorted[1]; i++) {
-    nums.push(i);
-  }
-  return nums.reduce((a,b) => a + b);
+function diffArray(arr1, arr2) {
+  const newArr = [];
+  
+  return newArr;
 }
 
-console.log(sumAll([1, 4]));
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
 
 /*
-ideas:
--probably sort the given array
--get new array of range of numbers
--add up all numbers using reduce()
+ideas 
+-loop through each arr1
+-test if each item in arr1 is in arr2, using some()??
+-if false, add to new arr?
+-repeat but now test arr2 items in arr1
 */
