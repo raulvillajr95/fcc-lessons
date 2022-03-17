8b40c1ee13c12a663348e2ab218b09ebeab68dac
Raul <raulvillajr95@gmail.com>
2022-03-16 22:20:11 -0700

    learning about the filter method on a deeper level

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+/*
 function whatIsInAName(collection, source) {
   // Only change code below this line
   let sourceK = Object.keys(source)[0]
@@ -11,6 +12,7 @@ function whatIsInAName(collection, source) {
 console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }));
 
 let obbhy = { last: "Capulet" };
+*/
 
 //console.log(Object.values(obbhy))
 
@@ -20,3 +22,24 @@ ideas
 -return source value == source key
 -similar to the old reduce() lesson??
 */
+
+let people = [
+  {name: "aaron", age: 65},
+  {name: "beth", age: 15},
+  {name: "cara", age: 13},
+  {name: "daniel", age: 3},
+  {name: "ella", age: 25},
+  {name: "fin", age: 16},
+  {name: "george", age: 18},
+]
+
+let range = {
+  lower: 13,
+  upper: 16
+}
+
+let teenagers = people.filter(function(person) {
+  return person.age >= this.lower && person.age <= this.upper;
+}, range)
+
+console.log(teenagers)
