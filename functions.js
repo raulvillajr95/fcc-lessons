f0c82ce0aed89c5aa39355e06c98a2b37dd9a067
Raul <raulvillajr95@gmail.com>
2022-03-17 15:32:50 -0700

    learning a bit about the strong and em tag

--- functions.js
+++ functions.js
@@ -1,19 +1,15 @@
 function whatIsInAName(collection, source) {
   const arr = [];
   // Only change code below this line
-  for (let i = 0; i < collection.length; i++) {
-    if(collection[i].hasOwnProperty(Object.keys(source))){
-      arr.push(collection[i])
-    }
-  }
+  let ar1 = collection.filter(function(element, index, source) {
+    return source
+  })
+
   // Only change code above this line
-  return arr;
+  return ar1;
 }
 
 console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }))
-
-//console.log(Object.values(obbhy))
-
 /*
 ideas 
 -using filter()
