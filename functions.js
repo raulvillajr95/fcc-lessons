29727a7f81acae7b84ffb039ac6fd03de0455bc2
Raul <raulvillajr95@gmail.com>
2022-03-18 15:12:14 -0700

    Attempt to pass challenge Wherefore Art Thou

--- functions.js
+++ functions.js
@@ -1,15 +1,13 @@
 function whatIsInAName(collection, source) {
-  const arr = [];
   // Only change code below this line
-  let ar1 = collection.filter(function(element, index, source) {
-    return source
-  })
-
+  for (let i = 0; i < collection.length; i++) {
+    console.log(collection[i].hasOwnProperty('last'))
+  }
   // Only change code above this line
-  return ar1;
 }
 
-console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }))
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })
+
 /*
 ideas 
 -using filter()
