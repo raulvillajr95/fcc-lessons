b91e52c7d5dd2151de60caaefe67e18de6115659
Raul <raulvillajr95@gmail.com>
2022-03-18 16:08:15 -0700

    Affect initial array (modifying, appending and deleting) MDN Practice

--- functions.js
+++ functions.js
@@ -1,30 +1,23 @@
-let arr = [
-  { id: 15 },
-  { id: -1 },
-  { id: 0 },
-  { id: 3 },
-  { id: 12.2 },
-  { },
-  { id: null },
-  { id: NaN },
-  { id: 'undefined' }
-]
+let words = ['spray', 'limit', 'exuberant', 'destruction', 'elite', 'present']
+const modifiedWords = words.filter( (word, index, arr) => {
+  arr[index+1] += ' extra'
+  return word.length < 6
+})
+console.log(modifiedWords)
 
-let invalidEntries = 0
+words = ['spray', 'limit', 'exuberant', 'destruction', 'elite', 'present']
+const appendedWords = words.filter( (word, index, arr) => {
+  arr.push('new')
+  return word.length < 6
+})
+console.log(appendedWords)
 
-function filterByID(item) {
-  if (Number.isFinite(item.id) && item.id !== 0)
-{
-    return true
-  }
-  invalidEntries++
-  return false;
-}
-
-let arrByID = arr.filter(filterByID)
-
-console.log('Filtered Array\n', arrByID)
-console.log('Number of Invalid Entries = ', invalidEntries)
+words = ['spray', 'limit', 'exuberant', 'destruction', 'elite', 'present']
+const deleteWords = words.filter( (word, index, arr) => {
+  arr.pop()
+  return word.length < 6
+})
+console.log(deleteWords)
 
 /*
 ideas 
