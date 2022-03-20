2fde192d36e9c7d006099ba52c0368b4f9887caa
Raul <raulvillajr95@gmail.com>
2022-03-19 18:33:53 -0700

    Extract object which matches second parameter object

--- functions.js
+++ functions.js
@@ -1,24 +1,38 @@
-let words = ['spray', 'limit', 'exuberant', 'destruction', 'elite', 'present']
-const modifiedWords = words.filter( (word, index, arr) => {
-  arr[index+1] += ' extra'
-  return word.length < 6
-})
-console.log(modifiedWords)
+const user = {
+  name: "Dom",
+  age: 32,
+  occupation: "Web Developer"
+};
 
-words = ['spray', 'limit', 'exuberant', 'destruction', 'elite', 'present']
-const appendedWords = words.filter( (word, index, arr) => {
-  arr.push('new')
-  return word.length < 6
-})
-console.log(appendedWords)
+//console.log(Object.entries(user))
 
-words = ['spray', 'limit', 'exuberant', 'destruction', 'elite', 'present']
-const deleteWords = words.filter( (word, index, arr) => {
-  arr.pop()
-  return word.length < 6
-})
-console.log(deleteWords)
 
+
+
+function whatIsInAName(collection, source) {
+  let arr = [];
+  let final = [];
+  // Only change code below this line
+  let ks = Object.keys(source)
+  for (let i = 0; i < collection.length; i++) {
+    for (let j = 0; j < ks.length; j++) {
+      if(collection[i].hasOwnProperty(ks[j]) && collection[i][ks[j]] == source[ks[j]]) {
+        arr.push(true);
+        
+      }
+      if (arr.length == ks.length) {
+        final.push(collection[i])
+      }
+    }
+    arr = [];
+  }
+
+  // Only change code above this line
+  return final;
+}
+
+console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }));
+//console.log(...party)
 /*
 ideas 
 -using filter()
