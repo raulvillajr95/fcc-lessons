cb638fbca17945ff96c9ad621b5294d1984c2b05
Raul <raulvillajr95@gmail.com>
2022-03-23 11:16:00 -0700

    Attempt to find smallest common multiple in range of numbers

--- functions.js
+++ functions.js
@@ -5,21 +5,28 @@ function smallestCommons(arr) {
     ran.push(i);
   }
 
-  let final = [ran[0]];
-  for (let j = 0; j < ran.length; j++) {
-    for (let k = 1; k <= ran[j]; k++) {
-      console.log(ran[j],k)
-      //if (k % final == 0) {}
+  let final = [];
+  for (let j = arr[0]; j < 100; j += arr[0]) {
+    for (let k = 0; k < ran.length; k++) {
+      if (j % arr[k] == 0) {
+        final.push(j)
+      }
+      /*
+      if (j % arr[1] == 0 && j % arr[2] == 0) {
+        return j;
+      }
+      */
     }
   }
-  return final;
+  return final
 }
 
-console.log(smallestCommons([1,5]))
+console.log(smallestCommons([3,4,5]))
 
 /*
 ideas 
 -run infinetly
 -if number is divisible by all of 'ran' digits, exit loop
 -also set a variable equal to that first number
+-needa find a way to run it through every number
 */
