f587c864364320b56fc3fc7b4d00687a91f9b0da
Raul <raulvillajr95@gmail.com>
2022-03-22 19:45:53 -0700

    Attempt to find smallest common multiple of a range of numbers

--- functions.js
+++ functions.js
@@ -1,28 +1,25 @@
-function sumPrimes(num) {
-  let finalL = [];
-
-  let primeTest = [];
-  for (let i = 1; i <= num; i++) {
-    for (let j = 1; j <= i; j++) {
-      if (i % j == 0) {
-        primeTest.push([i, j])
-      }
-    }
-    if (primeTest.length == 2) {
-      finalL.push(i)
-    }
-    primeTest=[]
+function smallestCommons(arr) {
+  let ran = [];
+  let sorted = arr.sort();
+  for (let i = sorted[0]; i <= sorted[1]; i++) {
+    ran.push(i);
   }
 
-  let summed = finalL.reduce((a, b) => a + b)
-  return summed;
+  let final = [ran[0]];
+  for (let j = 0; j < ran.length; j++) {
+    for (let k = 1; k <= ran[j]; k++) {
+      console.log(ran[j],k)
+      //if (k % final == 0) {}
+    }
+  }
+  return final;
 }
 
-console.log(sumPrimes(10))
+console.log(smallestCommons([1,5]))
 
 /*
 ideas 
--run through argi
--if number is not in argi, add it
--
+-run infinetly
+-if number is divisible by all of 'ran' digits, exit loop
+-also set a variable equal to that first number
 */
