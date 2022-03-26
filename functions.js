eacb8c20933c85895aeb510a9bb95e874acff90b
Raul <raulvillajr95@gmail.com>
2022-03-26 11:10:59 -0700

    Work on Steamroller Challenge for FCC

--- functions.js
+++ functions.js
@@ -1,22 +1,44 @@
 function steamrollArray(arr) {
   let final = []
+  let suckOne = []
   for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i]) == true) {
-      while (Array.isArray(arr[i]) == true) {
-        arr[i][0]
-      }
+    if (Array.isArray(arr[i]) && arr[i].length == 0) {
+      suckOne.push(arr[i])
     } else {
-      final.push(arr[i])
-      arr
+      final.push(arr[i].toString())
     }
   }
-  return final
+
+  let finalFinal = []
+  for (let i = 0; i < final.length; i++) {
+    if (final[i].includes(',')) {
+      let commas = final[i].split(',')
+      for (let j = 0; j < commas.length; j++) {
+        if (isNaN(Number(commas[j]))) {
+          finalFinal.push(commas[j])
+        } else {
+          finalFinal.push(Number(commas[j]))
+        }
+      }
+    } else {
+      if (isNaN(Number(final[i]))) {
+        finalFinal.push(final[i])
+      } else {
+        finalFinal.push(Number(final[i]))
+      }
+    }
+  }
+
+  return finalFinal
 }
 
-console.log(steamrollArray([1, [2], [3, [[4]]]]))
+console.log(steamrollArray([1, [], [3, [[4]]]]))
 
 /*
 to-do:
+-make exceptions for objects, they pass
+
+ideas:
 -if boolean true get value to new array
 -maybe use reduce??
 -use a while loop?
