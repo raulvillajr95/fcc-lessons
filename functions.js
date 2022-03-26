378cd49da23c44a27a3db52fbfcc424d54d522bf
Raul <raulvillajr95@gmail.com>
2022-03-25 20:40:38 -0700

    Solve 03-flex-header-2 exercise from TOP

--- functions.js
+++ functions.js
@@ -1,11 +1,25 @@
 function steamrollArray(arr) {
-  return arr;
+  let final = []
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i]) == true) {
+      while (Array.isArray(arr[i]) == true) {
+        arr[i][0]
+      }
+    } else {
+      final.push(arr[i])
+      arr
+    }
+  }
+  return final
 }
 
-console.log(Boolean({}))
+console.log(steamrollArray([1, [2], [3, [[4]]]]))
 
 /*
 to-do:
 -if boolean true get value to new array
 -maybe use reduce??
+-use a while loop?
+set it up somehow so that after every iteration it goes deeper
+until it's not a loop
 */
