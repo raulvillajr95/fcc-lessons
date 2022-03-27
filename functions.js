61af3120f1c66b0a1d31040643766282671b86c9
Raul <raulvillajr95@gmail.com>
2022-03-27 11:11:54 -0700

    Complete Binary Agents challenge from FCC

--- functions.js
+++ functions.js
@@ -1,17 +1,37 @@
 function binaryAgent(str) {
   let splitted = str.split(' ')
 
-  let numbers = []
+  let list = [];
   for (let i = 0; i < splitted.length; i++) {
-    numbers.push(splitted[i]);
+    let temp = 0;
+    if (splitted[i][7] == '1') {
+      temp += 1;
+    }
+    if (splitted[i][6] == '1') {
+      temp += 2;
+    }
+    if (splitted[i][5] == '1') {
+      temp += 4;
+    }
+    if (splitted[i][4] == '1') {
+      temp += 8;
+    }
+    if (splitted[i][3] == '1') {
+      temp += 16;
+    }
+    if (splitted[i][2] == '1') {
+      temp += 32;
+    }
+    if (splitted[i][1] == '1') {
+      temp += 64;
+    }
+    if (splitted[i][0] == '1') {
+      temp += 128;
+    }
+    list.push(temp)
   }
 
-  let final = []
-  for (let i = 0; i < numbers.length; i++) {
-    final.push(numbers[i].toString());
-  }
-
-  return final;
+  return String.fromCharCode(...list);
 }
 
 console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"))
