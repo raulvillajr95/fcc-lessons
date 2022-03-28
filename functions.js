0d651159c86937eb93d84e28796da89b6a74cf60
Raul <raulvillajr95@gmail.com>
2022-03-27 18:04:16 -0700

    Work on 06-flex-layout

--- functions.js
+++ functions.js
@@ -1,49 +1,15 @@
-function binaryAgent(str) {
-  let splitted = str.split(' ')
-
-  let list = [];
-  for (let i = 0; i < splitted.length; i++) {
-    let temp = 0;
-    if (splitted[i][7] == '1') {
-      temp += 1;
-    }
-    if (splitted[i][6] == '1') {
-      temp += 2;
-    }
-    if (splitted[i][5] == '1') {
-      temp += 4;
-    }
-    if (splitted[i][4] == '1') {
-      temp += 8;
-    }
-    if (splitted[i][3] == '1') {
-      temp += 16;
-    }
-    if (splitted[i][2] == '1') {
-      temp += 32;
-    }
-    if (splitted[i][1] == '1') {
-      temp += 64;
-    }
-    if (splitted[i][0] == '1') {
-      temp += 128;
-    }
-    list.push(temp)
-  }
-
-  return String.fromCharCode(...list);
+function truthCheck(collection, pre) {
+  let secondA = collection[1]
+  return secondA[pre] == 1
 }
 
-console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"))
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "name"))
 
 /*
 to-do:
--make exceptions for objects, they pass
+-make sure its a truthy value
+this could be true, anything greater than 0, etc.
 
 ideas:
--if boolean true get value to new array
--maybe use reduce??
--use a while loop?
-set it up somehow so that after every iteration it goes deeper
-until it's not a loop
+-turn value into boolean and check if it's true?
 */
