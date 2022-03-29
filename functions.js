9a00f49efc798608322ba6d6caee3a64b4490ef9
Raul <raulvillajr95@gmail.com>
2022-03-28 19:29:41 -0700

    Work on Causars Cipher project

--- functions.js
+++ functions.js
@@ -1,75 +1,69 @@
-function convertToRoman(num) {
-  let list = [];
-  let tou = parseInt(num.toString()[num.toString().length-4]);
-  let hun = parseInt(num.toString()[num.toString().length-3]);
-  let tns = parseInt(num.toString()[num.toString().length-2]);
-  let ons = num % 10;
-  if (tou == 1) {
-    list.push('M');
-  } else if (tou == 2) {
-    list.push('MM')
-  } else if (tou == 3) {
-    list.push('MMM')
+function rot13(str) {
+  let codesConverted = []
+  let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] in abc) {
+      switch (str.charCodeAt(i)) {
+        case 78:
+          codesConverted.push(65)       
+          break;
+        case 79:
+          codesConverted.push(66)       
+          break;
+        case 80:
+          codesConverted.push(67)       
+          break;
+        case 81:
+          codesConverted.push(68)       
+          break;
+        case 82:
+          codesConverted.push(69)       
+          break;
+        case 83:
+          codesConverted.push(70)       
+          break;
+        case 84:
+          codesConverted.push(71)       
+          break;
+        case 85:
+          codesConverted.push(72)       
+          break;
+        case 86:
+          codesConverted.push(73)       
+          break;
+        case 87:
+          codesConverted.push(74)       
+          break;
+        case 88:
+          codesConverted.push(75)       
+          break;
+        case 89:
+          codesConverted.push(76)       
+          break;
+        case 90:
+          codesConverted.push(67)       
+          break;
+        default:
+          codesConverted.push(str.charCodeAt(i) + 13)
+      }
+    } else {
+      codesConverted.push(str.charCodeAt(i))
+    }
   }
-  if (hun == 9) {
-    list.push('CM');
-  } else if (hun == 8) {
-    list.push('DCCC');
-  } else if (hun == 7) {
-    list.push('DCC');
-  } else if (hun == 6) {
-    list.push('DC');
-  } else if (hun == 5) {
-    list.push('D');
-  } else if (hun == 4) {
-    list.push('CD');
-  } else if (hun == 3) {
-    list.push('CCC');
-  } else if (hun == 2) {
-    list.push('CC');
-  } else if (hun == 1) {
-    list.push('C');
-  } 
-  if (tns == 9) {
-    list.push('XC');
-  } else if (tns == 8) {
-    list.push('LXXX');
-  } else if (tns == 7) {
-    list.push('LXX');
-  } else if (tns == 6) {
-    list.push('LX');
-  } else if (tns == 5) {
-    list.push('L');
-  } else if (tns == 4) {
-    list.push('XL');
-  } else if (tns == 3) {
-    list.push('XXX');
-  } else if (tns == 2) {
-    list.push('XX');
-  } else if (tns == 1) {
-    list.push('X');
+
+  let backToString = []
+  for (let i = 0; i < codesConverted.length; i++) {
+    backToString.push(String.fromCharCode(codesConverted[i]));
   }
-  if (ons == 9) {
-    list.push('IX');
-  } else if (ons == 8) {
-    list.push('VIII');
-  } else if (ons == 7) {
-    list.push('VII');
-  } else if (ons == 6) {
-    list.push('VI');
-  } else if (ons == 5) {
-    list.push('V');
-  } else if (ons == 4) {
-    list.push('IV');
-  } else if (ons == 3) {
-    list.push('III');
-  } else if (ons == 2) {
-    list.push('II');
-  } else if (ons == 1) {
-    list.push('I')
-  }
-  let final = list.join('');
-  return final
+
+  return backToString
 }
 
-console.log(convertToRoman(3999));
+console.log(rot13("SERR PBQR PNZC"));
+
+/*
+
+to-do:
+-need to straighten it out
+
+*/
