27a11f961e6605d32b2f92ac31978c353e899362
Raul <raulvillajr95@gmail.com>
2022-03-28 18:26:40 -0700

    Complete Roman Numeral Converter project on FCC

--- functions.js
+++ functions.js
@@ -1,17 +1,75 @@
-function palindrome(str) {
-  let what = str.match(/[A-Za-z0-9]+/g)
-  let what2 = what.join('')
-  let lowWhat2 = []
-  for (let i = 0; i < what2.length; i++) {
-    lowWhat2.push(what2[i].toLowerCase());
+function convertToRoman(num) {
+  let list = [];
+  let tou = parseInt(num.toString()[num.toString().length-4]);
+  let hun = parseInt(num.toString()[num.toString().length-3]);
+  let tns = parseInt(num.toString()[num.toString().length-2]);
+  let ons = num % 10;
+  if (tou == 1) {
+    list.push('M');
+  } else if (tou == 2) {
+    list.push('MM')
+  } else if (tou == 3) {
+    list.push('MMM')
   }
-
-  let almReversed = []
-  for (let i = what2.length-1; i >= 0; i--) {
-    almReversed.push(what2[i].toLowerCase())
+  if (hun == 9) {
+    list.push('CM');
+  } else if (hun == 8) {
+    list.push('DCCC');
+  } else if (hun == 7) {
+    list.push('DCC');
+  } else if (hun == 6) {
+    list.push('DC');
+  } else if (hun == 5) {
+    list.push('D');
+  } else if (hun == 4) {
+    list.push('CD');
+  } else if (hun == 3) {
+    list.push('CCC');
+  } else if (hun == 2) {
+    list.push('CC');
+  } else if (hun == 1) {
+    list.push('C');
+  } 
+  if (tns == 9) {
+    list.push('XC');
+  } else if (tns == 8) {
+    list.push('LXXX');
+  } else if (tns == 7) {
+    list.push('LXX');
+  } else if (tns == 6) {
+    list.push('LX');
+  } else if (tns == 5) {
+    list.push('L');
+  } else if (tns == 4) {
+    list.push('XL');
+  } else if (tns == 3) {
+    list.push('XXX');
+  } else if (tns == 2) {
+    list.push('XX');
+  } else if (tns == 1) {
+    list.push('X');
   }
-  let reversed = almReversed.join('')
-  return reversed == lowWhat2.join('')
+  if (ons == 9) {
+    list.push('IX');
+  } else if (ons == 8) {
+    list.push('VIII');
+  } else if (ons == 7) {
+    list.push('VII');
+  } else if (ons == 6) {
+    list.push('VI');
+  } else if (ons == 5) {
+    list.push('V');
+  } else if (ons == 4) {
+    list.push('IV');
+  } else if (ons == 3) {
+    list.push('III');
+  } else if (ons == 2) {
+    list.push('II');
+  } else if (ons == 1) {
+    list.push('I')
+  }
+  let final = list.join('');
+  return final
 }
 
-console.log(palindrome("A man, a plan, a canal. Panama"));
+console.log(convertToRoman(3999));
