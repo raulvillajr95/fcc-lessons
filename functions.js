aa866b493aca1c30ddc4216d970908a1662e8e26
Raul <raulvillajr95@gmail.com>
2022-04-09 18:26:42 -0700

    Use Array.map() to Dynamically Render Elements

--- functions.js
+++ functions.js
@@ -1,32 +1,47 @@
-class GateKeeper extends React.Component {
+const textAreaStyles = {
+  width: 235,
+  margin: 5
+};
+
+class MyToDoList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      input: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({ input: event.target.value })
-  }
-  render() {
-    let inputStyle = {
-      border: '1px solid black'
-    };
     // Change code below this line
-    if(this.state.input.length > 15) {
-      inputStyle.border = '3px solid red'
+    this.state = {
+      userInput: '',
+      toDoList: []
     }
     // Change code above this line
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit() {
+    const itemsArray = this.state.userInput.split(',');
+    this.setState({
+      toDoList: itemsArray
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+  render() {
+    let lista = this.state.toDoList
+    const items = lista.map(n => <li>{n}</li>)
     return (
       <div>
-        <h3>Don't Type Too Much:</h3>
-        <input
-          type="text"
-          style={inputStyle}
-          value={this.state.input}
-          onChange={this.handleChange} />
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.userInput}
+          style={textAreaStyles}
+          placeholder='Separate Items With Commas'
+        />
+        <br />
+        <button onClick={this.handleSubmit}>Create List</button>
+        <h1>My "To Do" List:</h1>
+        <ul>{items}</ul>
       </div>
     );
   }
-};
+}
