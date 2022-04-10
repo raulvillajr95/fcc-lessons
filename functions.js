88a5350c5cd267164a4280ef3e52d8adea61d462
Raul <raulvillajr95@gmail.com>
2022-04-10 13:37:35 -0700

    Render React on the Server with renderToString

--- functions.js
+++ functions.js
@@ -1,22 +1,11 @@
-const frontEndFrameworks = [
-  'React',
-  'Angular',
-  'Ember',
-  'Knockout',
-  'Backbone',
-  'Vue'
-];
-
-function Frameworks() {
-  const renderFrameworks = frontEndFrameworks.map(function(n, i) {
-  return <li key={i}>{n}</li>
-}); // Change this line
-  return (
-    <div>
-      <h1>Popular Front End JavaScript Frameworks</h1>
-      <ul>
-        {renderFrameworks}
-      </ul>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div/>
+  }
 };
+
+// Change code below this line
+ReactDOMServer.renderToString(<App/>)
