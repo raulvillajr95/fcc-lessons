7dfcdf3f9736a1b4552ad8e2b16e19d9dee347fc
Raul <raulvillajr95@gmail.com>
2022-04-12 18:33:40 -0700

    Else if example in HTML and JS

--- practice.js
+++ practice.js
@@ -1,16 +1,20 @@
-let prm = prompt('Who\'s there?')
+const select = document.querySelector('select');
+const para = document.querySelector('p');
 
-if (prm == 'Admin') {
-  let prm2 = prompt('Password?')
-  if (prm2 == 'TheMaster') {
-    alert('Welcome!')
-  } else if (prm2 == null || prm2 == '') {
-    alert('Canceled')
+select.addEventListener('change', setWeather);
+
+function setWeather() {
+  const choice = select.value;
+
+  if (choice === 'sunny') {
+    para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.';
+  } else if (choice === 'rainy') {
+    para.textContent = 'Rain is falling outside; take a rain coat and an umbrella, and don\'t stay out for too long.';
+  } else if (choice === 'snowing') {
+    para.textContent = 'The snow is coming down - it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
+  } else if (choice === 'overcast') {
+    para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
   } else {
-    alert('Wrong password')
+    para.textContent = '';
   }
-} else if (prm == null || prm == '') {
-  alert('Canceled')
-} else {
-  alert('I don\'t know you')
 }
