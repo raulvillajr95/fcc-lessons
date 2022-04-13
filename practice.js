b33f5232460eaff328643d04d4ae9635f3ddf89e
Raul <raulvillajr95@gmail.com>
2022-04-13 12:55:09 -0700

    Turn and 'if else' statement into a 'switch case' statement

--- practice.js
+++ practice.js
@@ -1,25 +1,26 @@
 const select = document.querySelector('select');
 const para = document.querySelector('p');
 
-let temperature = 90;
-
 select.addEventListener('change', setWeather);
 
 function setWeather() {
   const choice = select.value;
 
-  if (choice === 'sunny' && temperature < 86) {
-    para.textContent = `It is ${temperature} degrees outside - nice and sunny. Let\'s go out to the beach, or the park, and get an ice cream.`;
-  }
-  else if (choice === 'sunny' && temperature >= 86) {
-    para.textContent = `It is ${temperature} degrees outside - REALY HOT! If you want to go outside, make sure to put some sunscreen on.`;
-  } else if (choice === 'rainy') {
-    para.textContent = 'Rain is falling outside; take a rain coat and an umbrella, and don\'t stay out for too long.';
-  } else if (choice === 'snowing') {
-    para.textContent = 'The snow is coming down - it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
-  } else if (choice === 'overcast') {
-    para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
-  } else {
-    para.textContent = '';
+  switch (choice) {
+    case 'sunny':
+      para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream';
+      break;
+    case 'rainy':
+      para.textContent = 'Rain is falling outside; take a rain coat and an umbrella, and don\'t stay out for too long.';
+      break;
+    case 'snowing':
+      para.textContent = 'The snow is coming down - it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
+      break;
+    case 'overcast':
+      para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
+      break;
+    default:
+      para.textContent = '';
+      break;
   }
 }
