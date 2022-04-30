f565177d63619fcc279ca28c736c8772e7be2257
Raul <raulvillajr95@gmail.com>
2022-04-29 21:36:17 -0700

    Learning problem solving with programs, exercise 2-1

--- practice.js
+++ practice.js
@@ -1,10 +1,34 @@
-// Luhn Checksum Validation
-let num = prompt('what it do')
+/*
+start with 8 #'s
+after every new line,
+  replace 1 '#' with ' ' space on each side
+*/
 
-let newNum = []
-
-for (let i = 0; i < num.length; i++) {
-  newNum.push(num[i]);
+function line() {
+  return "########"
 }
 
-console.log(newNum)
+function squareLines () {
+  for (let i = 1; i <= 4; i++) {
+    console.log(line())
+  }
+}
+
+function takeAway(str) {
+  // replace first and last '#' with white space ' '
+  return str.slice(2)
+}
+
+function addSpaces(str, am) {
+  return ' '.repeat(am) + str + ' '.repeat(am)
+}
+
+function pyramid(str) {
+  for (let i = 0; i <= 3; i++) {
+    let sliced = str.slice(2 * i)
+    let replaced = addSpaces(sliced, i)
+    console.log(replaced)
+  }
+}
+
+pyramid(line())
