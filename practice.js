8c5c2f02aafc5efba6c839a3f9f5bb4df6facb6d
Raul <raulvillajr95@gmail.com>
2022-04-30 18:45:10 -0700

    Create X using hash symbols

--- practice.js
+++ practice.js
@@ -75,8 +75,57 @@ function bothPyramidsInsideOut() {
   reversePyramid(line())
 }
 
-function addSpaceInMiddle(str) {
-  str.length/2
+/*
+ONE STEP AT A TIME
+
+First, insert spaces in middle
+then level it with spaces on the sides
+  maybe just spaces on the left side??
+*/
+
+function addSpaceInMiddle(str, num) {
+  let middle = (str.length/2)
+  let inserted = str.slice(0,middle) + ' '.repeat(num) + str.slice(middle)
+  return inserted
 }
 
-bothPyramids()
+function addSpaceInMiddle2Lines() {
+  for (let i = 3; i >= 0; i--) {
+    console.log(addSpaceInMiddle('0123456789', i))
+  }
+  for (let i = 0; i <= 3; i++) {
+    console.log(addSpaceInMiddle('0123456789', i))
+  }
+}
+
+function spaceInMidWDecrease() {
+  let cnt = 5
+  let spc = 0
+  for (let i = 3; i >= 0; i--) {
+    cnt -= 1
+    let sliced = '##########'.slice(cnt*2)
+    console.log(" ".repeat(spc) + addSpaceInMiddle(sliced , i*4))
+    spc +=1
+  }
+
+  // let cnt2 = 5
+  // for (let i = 0; i <= 3; i++) {
+  //   cnt2 -= 1
+  //   let sliced = '##########'.slice(cnt*2)
+  //   console.log(addSpaceInMiddle(sliced , i*4))
+  // }
+
+  let spc2 = 3
+  for (let i = 0; i <= 3; i++) {
+    let sliced = "########".slice(i*2)
+    //let replaced = addSpaces(sliced, i)
+    console.log(' '.repeat(spc2) + addSpaceInMiddle(sliced, i*4))
+    spc2 -=1
+  }
+}
+
+function addLeftSpaceOnce(str) {
+  return ' ' + str
+}
+
+spaceInMidWDecrease()
