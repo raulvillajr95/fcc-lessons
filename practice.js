00e246197176fc3ec67c435e30c5b288a08dbbf7
Raul <raulvillajr95@gmail.com>
2022-05-04 18:40:59 -0700

    WHY THE FUCK IS SORTING NUMBERS SO DIFFICULT?????????

--- practice.js
+++ practice.js
@@ -1,17 +1,54 @@
-const timerText = document.getElementById('timer-text')
-const btnStart = document.getElementById('btn-start')
-const btnPause = document.getElementById('btn-pause')
+const punctuation = [87,28, 100, 78];
 
-let count = 0
-let intervalID
+let sortedPunc = [];
 
-btnStart.addEventListener('click', function () {
-  intervalID = setInterval(function () {
-    count += 1
-    timerText.textContent = count
-  }, 1000)
+for (let i = 0; i < punctuation.length; i++) {
+  if (i == 0) {
+    sortedPunc.push(punctuation[i])
+  } else {
+    for (let j = 0; j < sortedPunc.length; j++) {
+      if ((punctuation[i] > sortedPunc[j] && punctuation[i] < sortedPunc[j+1]) || (i == punctuation.length-1 && punctuation[i] > sortedPunc[j])) {
+        sortedPunc.push(punctuation[i])
+        break
+      } else if (punctuation[i] > sortedPunc[j] && punctuation[i] < sortedPunc[j+1]) {
+        sortedPunc.splice(j+1,0,punctuation[i])
+      } else {
+        sortedPunc.unshift(punctuation[i])
+        break
+      }
+    }
+  }
+}
 
-  btnPause.addEventListener('click', function () {
-    clearInterval(intervalID)
-  })
-})
+console.log(sortedPunc)
+
+/*
+      if (punctuation[i] > sortedPunc[j]) {
+        sortedPunc.splice(sortedPunc[j],0,punctuation[i])
+      } else {
+        sortedPunc.unshift(punctuation[i])
+      }
+
+
+
+Sorting by grade
+
+ideas:
+-check for number is last
+-sort 4+ numbers
+-sort number singles, but double layered e.g. [[87],[28],[100],[78],[84],[98],[75],[70]]
+-sort the actual list
+
+const punctuation = [
+  [87, 10001, "Fred"],
+  [28, 10002, "Tom"],
+  [100, 10003, "Alistair"],
+  [78, 10004, "Sasha"],
+  [84, 10005, "Erin"],
+  [98, 10006, "Belinda"],
+  [75, 10007, "Leslie"],
+  [70, 10008, "Candy"],
+  [81, 10009, "Aretha"],
+  [68, 10010, "Veronica"]
+];
+*/
