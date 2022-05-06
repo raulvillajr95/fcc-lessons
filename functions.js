fb63052aaf51f83a4e11af2f453350e9527f7cb4
Raul <raulvillajr95@gmail.com>
2022-05-05 18:22:54 -0700

    Sort array by grades

--- functions.js
+++ functions.js
@@ -1,18 +1,99 @@
-/*
-map through 60
-*/
-
+// Map through 60, to be used with setInterval()
 function loopInSeconds() {
-  let all60 = [59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1, 0]
-  let i = 0
+  let all60 = [59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1, 0];
+  let i = 0;
 
-  console.log(all60[i])
+  return all60[i]
   i += 1
   if (all60[i] == 0) {
-    console.log(all60[i])
+    return all60[i]
     i = 0
   }
 }
 
-setInterval(loopInSeconds, 300)
+// Sort numbers in array
+function sortNum(arr) {
+  let len = arr.length;
+  let emp = []
+  let highest = 0
+  for (let j = 0; j < len; j++) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > highest) {
+        highest = arr[i]
+      }
+    }
+    emp.unshift(highest)
+    let index = arr.indexOf(highest)
+    if (index > -1) {
+      arr.splice(index, 1)
+    }
+    highest = 0
+  }
+  return emp
+}
 
+// Sort number, but each number is under a layer e.g. [[1], [2], [3]]
+function sortNumLayered(arr) {
+  let len = arr.length
+
+  let emp = []
+
+  let highest = 0
+
+  for (let j = 0; j < len; j++) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i][0] > highest) {
+        highest = arr[i]
+      }
+    }
+    emp.unshift(highest)
+    let index = arr.indexOf(highest)
+    if (index > -1) {
+      arr.splice(index, 1)
+    }
+    highest = 0
+  }
+  return emp
+}
+
+// Reverse array
+function reverseArr(arr) {
+  let len = arr.length
+  let emp = []
+  let highest = 0
+  for (let j = 0; j < len; j++) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i][0] > highest) {
+        highest = arr[i]
+      }
+    }
+    emp.unshift(highest)
+    let index = arr.indexOf(highest)
+    if (index > -1) {
+      arr.splice(index, 1)
+    }
+    highest = 0
+  }
+  return emp
+}
+
+// Sort Array by first index number e.g. [[1, 'Fred'], [2, 'Jesus']]
+function sortNumFirstArray(arr) {
+  let len = arr.length
+  let emp = []
+  let highest = [0,,]
+  for (let j = 0; j < len; j++) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i][0] > highest[0]) {
+        highest = arr[i]
+      }
+    }
+    emp.unshift(highest)
+    let index = arr.indexOf(highest)
+    if (index > -1) {
+      arr.splice(index, 1)
+    }
+    highest = [0,,]
+  }
+  return emp
+}
--- practice.js
+++ practice.js
@@ -1,38 +1,46 @@
-const punctuation = [28, 100, 78];
+let arr = [
+  [87, 10001, "Fred"],
+  [28, 10002, "Tom"],
+  [100, 10003, "Alistair"],
+  [78, 10004, "Sasha"],
+  [84, 10005, "Erin"],
+  [98, 10006, "Belinda"],
+  [75, 10007, "Leslie"],
+  [70, 10008, "Candy"],
+  [81, 10009, "Aretha"],
+  [68, 10010, "Veronica"]
+];
 
-let sortedPunc = [];
+let len = arr.length
 
-for (let i = 0; i < punctuation.length; i++) {
-  if (i == 0) {
-    sortedPunc.push(punctuation[i])
-  } else {
-    for (let j = 0; j < sortedPunc.length; j++) {
-      if ((punctuation[i] > sortedPunc[j] && punctuation[i] < sortedPunc[j+1]) || (i == punctuation.length-1 && punctuation[i] > sortedPunc[j])) {
-        sortedPunc.push(punctuation[i])
-        break
-      } else if (punctuation[i] > sortedPunc[j] && punctuation[i] < sortedPunc[j+1]) {
-        sortedPunc.splice(j+1,0,punctuation[i])
-      } else {
-        sortedPunc.unshift(punctuation[i])
-        break
-      }
+let emp = []
+
+// Test
+let test = []
+
+let highest = [0,,]
+
+for (let j = 0; j < len; j++) {
+  for (let i = 0; i < arr.length; i++) {
+    test.push(arr[i])
+    if (arr[i][0] > highest[0]) {
+      highest = arr[i]
     }
   }
+  emp.unshift(highest)
+  let index = arr.indexOf(highest)
+  if (index > -1) {
+    arr.splice(index, 1)
+  }
+  highest = [0,,]
 }
+console.log(emp)
 
 /*
-      if (punctuation[i] > sortedPunc[j]) {
-        sortedPunc.splice(sortedPunc[j],0,punctuation[i])
-      } else {
-        sortedPunc.unshift(punctuation[i])
-      }
-
-
-
 Sorting by grade
 
 ideas:
--sort number singles, but double layered e.g. [[87],[28],[100],[78],[84],[98],[75],[70]]
+-sort doubles, numbers & names
 -sort the actual list
 
 const punctuation = [
@@ -47,29 +55,18 @@ const punctuation = [
   [81, 10009, "Aretha"],
   [68, 10010, "Veronica"]
 ];
+
+
+let arr = [
+  [87, "Fred"],
+  [28, "Tom"],
+  [100, "Alistair"],
+  [78, "Sasha"],
+  [84, "Erin"],
+  [98, "Belinda"],
+  [75, "Leslie"],
+  [70, "Candy"],
+  [81, "Aretha"],
+  [68, "Veronica"]
+];
 */
-
-
-let arr = [87, 28, 100, 78, 84, 98, 75, 70, 81, 68] 
-let len = arr.length
-
-let emp = []
-
-let highest = 0
-
-for (let j = 0; j < len; j++) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > highest) {
-      highest = arr[i]
-      console.log(arr[i])
-    }
-  }
-  emp.unshift(highest)
-  let index = arr.indexOf(highest)
-  if (index > -1) {
-    arr.splice(index, 1)
-  }
-  highest = 0
-}
-
-console.log(emp)
