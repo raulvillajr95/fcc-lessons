ff1e5e65b8c3d8695db4e2c770bdbc067b67619a
Raul <raulvillajr95@gmail.com>
2022-05-22 18:52:16 -0700

    Complete 'Loops 2' test challenge from mdn

--- practice.js
+++ practice.js
@@ -1,9 +1,26 @@
-const myArray = ['tomatoes', 'chick peas', 'onions', 'rice', 'black beans'];
-const list = document.createElement('ul');
+const name = 'Mustafa';
+const para = document.createElement('p');
 
-for (let i = 0; i < myArray.length; i++) {
-  list.innerHTML += '<li>' + myArray[i] + '</li>'
+const phonebook = [
+  {name: 'Chris', number: '1549'},
+  {name: 'Li Kang', number: '9634'},
+  {name: 'Anne', number: '9065'},
+  {name: 'Francesca', number: '3001'},
+  {name: 'Mustafa', number: '6888'},
+  {name: 'Tina', number: '4312'},
+  {name: 'Bert', number: '7780'},
+  {name: 'Jada', number: '2282'}
+]
+
+for (let i = 0; i < phonebook.length; i++) {
+  // console.log(phonebook[i].name)
+  if (phonebook[i].name === 'Mustafa') {
+    para.innerHTML = `<p>${phonebook[i].name}'s number is ${phonebook[i].number}</p>`
+    break
+  }
 }
 
+// Add my code here
+
 const section = document.querySelector('section');
-section.appendChild(list);
+section.appendChild(para);
