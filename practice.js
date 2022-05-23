fdf66c98e5cc3831b539c6f2f8eb7be8c49112fe
Raul <raulvillajr95@gmail.com>
2022-05-22 19:05:38 -0700

    Complet 'Loops 3' test challenge from mdn

--- practice.js
+++ practice.js
@@ -1,26 +1,23 @@
-const name = 'Mustafa';
+let i = 500;
 const para = document.createElement('p');
 
-const phonebook = [
-  {name: 'Chris', number: '1549'},
-  {name: 'Li Kang', number: '9634'},
-  {name: 'Anne', number: '9065'},
-  {name: 'Francesca', number: '3001'},
-  {name: 'Mustafa', number: '6888'},
-  {name: 'Tina', number: '4312'},
-  {name: 'Bert', number: '7780'},
-  {name: 'Jada', number: '2282'}
-]
-
-for (let i = 0; i < phonebook.length; i++) {
-  // console.log(phonebook[i].name)
-  if (phonebook[i].name === 'Mustafa') {
-    para.innerHTML = `<p>${phonebook[i].name}'s number is ${phonebook[i].number}</p>`
-    break
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if(num % i === 0) {
+      return false;
+    }
   }
+
+  return true;
 }
 
-// Add my code here
+for (let j = i; j >= 2; j--) {
+  if (isPrime(j) == true) {
+    para.textContent += `${j}, `
+  } else {
+    continue
+  }
+}
 
 const section = document.querySelector('section');
 section.appendChild(para);
