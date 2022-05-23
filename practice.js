ba5e69d825d6efdcf0e48b99d88cb7a7adcc696d
Raul <raulvillajr95@gmail.com>
2022-05-22 17:55:38 -0700

    Complete 'Filling in a guest list' exercise from MDN

--- practice.js
+++ practice.js
@@ -1,41 +1,28 @@
-const output = document.querySelector('.output');
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
 
-for (let i = 10; i >= 0; i--) {
-  console.log(i)
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: ';
 
-  const para = document.createElement('p');
-  output.appendChild(para);
-  if (i === 10) {
-    para.textContent = 'Countdown 10';
-  } else if (i === 0) {
-    para.textContent = 'Blast off!';
+for (let i = 0; i < people.length; i++) {
+  if (people[i] == 'Phil' || people[i] == 'Lola') {
+    if (i == people.length - 1) {
+      
+    }
+    refused.textContent += `${people[i]}, `
   } else {
-    para.textContent = i;
+    admitted.textContent += `${people[i]}, `
   }
 }
 
+admitted.textContent = admitted.textContent.slice(0, -2) + '.'
+refused.textContent = refused.textContent.slice(0,-2) + '.'
+
+console.log(admitted.textContent)
+console.log(refused.textContent)
+
 /*
-- Loop from 10 down to 0. We've provided you with an initializer — let i = 10;.
-
-- For each iteration, create a new paragraph and append it to the output <div>,
-  which we've selected using const output =
-  document.querySelector('.output');. In comments, we've provided you with
-  three code lines that need to be used somewhere inside the loop:
-  - const para = document.createElement('p'); — creates a new
-    paragraph.
-  - output.appendChild(para); — appends the paragraph to the output
-    <div>.
-  - para.textContent = — makes the text inside the paragraph equal to
-    whatever you put on the right-hand side, after the equals sign.
-
-- Different iteration numbers require different text to be put in the paragraph for that
-  iteration (you'll need a conditional statement and multiple para.textContent =
-  lines):
-  - If the number is 10, print "Countdown 10" to the paragraph.
-  - If the number is 0, print "Blast off!" to the paragraph.
-  - For any other number, print just the number to the paragraph.
-
-- Remember to include an iterator! However, in this example we are counting down
-  after each iteration, not up, so you don't want i++ — how do you iterate
-  downwards?
+to-do
+-mess with the loop
 */
