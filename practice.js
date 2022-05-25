6c6e2f0cbd15715641e97012607b00158fb69a9a
Raul <raulvillajr95@gmail.com>
2022-05-24 17:35:21 -0700

    Attempt to solve exercise 04

--- practice.js
+++ practice.js
@@ -1,52 +1,18 @@
-/*
-W 'Last loop value': 1
+function removeFromArray(arr, rem) {
+  let  finalArr = []
 
-L 'Which values get shown by the "for" loop?'
-1. 0, 1, 2, 3, 4
-2. 1, 2, 3, 4,
-
-W 'Output even numbers in the loop':
-for (let i = 2; i <= 10; i++) {
-  if (i % 2 == 0) {
-    console.log(i)
-  }
-}
-
-W 'Replace "for" with "while"':
-let j = 0;
-while (j < 3) {
-  console.log(`number ${j}!`);
-  j++;
-}
-
-W 'Repeat until the input is correct':
-while (true) {
-  let num = prompt('Pick a number greater than 100:')
-  if (num > 100 || num === '') {
-    alert('Correct')
-    break;
-  } else {
-    alert('Please pick another number.')
-  }
-}
-
-W 'Output prime numbers':
-function primes(n) {
-  let listy = []
-  for (let i = 2; i <= n; i++) {
-    let list = []
-    for (let j = 2; j <= i; j++) {
-      if (i % j == 0) {
-        list.push(j)
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arguments.length; j++) {
+      console.log(`${arr[i]}, ${arguments[j]}`)
+      if (arr[i] == arguments[j]) {
+        
       } else {
-        continue
+        finalArr.push(arr[i])
       }
     }
-    if (list.length == 1) {
-      listy.push(i)
-    }
-    list = []
   }
-  console.log(listy)
+  
+  return finalArr
 }
-*/
+
+console.log(removeFromArray([1,2,3,4], 3, 4))
