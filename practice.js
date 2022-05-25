52dd603562f8f549687a3222dd6a8a3b4b0b7fd9
Raul <raulvillajr95@gmail.com>
2022-05-24 18:51:06 -0700

    Attempt #2 at solving exercise 04

--- practice.js
+++ practice.js
@@ -1,18 +1,29 @@
 function removeFromArray(arr, rem) {
   let  finalArr = []
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 1; j < arguments.length; j++) {
-      console.log(`${arr[i]}, ${arguments[j]}`)
-      if (arr[i] == arguments[j]) {
-        
-      } else {
-        finalArr.push(arr[i])
-      }
+  let arr 
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   for (let j = 1; j < arguments.length; j++) {
+  //     console.log(`${arr[i]}, ${arguments[j]}`)
+  //     if (arr[i] !== arguments[j]) {
+  //       continue
+  //     } else {
+  //       finalArr.push(arr[i])
+  //     }
+  //   }
+  // }
+
+  for (let i = 1; i < arguments.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      console.log(`${arguments[i]}, ${arr[j]}`)
+
     }
   }
   
-  return finalArr
+  // return finalArr
 }
 
-console.log(removeFromArray([1,2,3,4], 3, 4))
+// console.log(removeFromArray([1,2,3,4], 3, 4))
+
+removeFromArray([1,2,3,4], 3, 4)
