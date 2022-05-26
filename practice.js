ff58ee091bb7bb545182643939033dd990df4363
Raul <raulvillajr95@gmail.com>
2022-05-26 12:47:57 -0700

    Manipulate the DOM with javascript

--- practice.js
+++ practice.js
@@ -1,13 +1,29 @@
-// const container = document.querySelector('#container')
+const container = document.querySelector('#container')
 
-// const content = document.createElement('div')
-// content.classList.add('content')
-// content.textContent = 'This is the glorious text-content!'
+// 1.
+const para = document.createElement('p')
+para.textContent = 'Hey I\'m red!'
+para.setAttribute('style','color: red;')
+container.appendChild(para)
 
-// container.appendChild(content)
-// console.log(container)
+// 2.
+const heading3 = document.createElement('h3')
+heading3.textContent = "I'm a blue h3!"
+heading3.setAttribute('style', 'color: blue;')
+container.appendChild(heading3)
 
-const body = document.querySelector('html > head title')
+// 3.
+const div = document.createElement('div')
+div.setAttribute('style', 'border: 1px solid black; background-color: pink;')
+/* ------ */
+const heading1 = document.createElement('h1')
+heading1.textContent = "I'm in a div"
+div.appendChild(heading1)
+const para2 = document.createElement('p')
+para2.textContent = "ME TOO!"
+div.appendChild(para2)
+/* ------ */
+container.appendChild(div)
 
-body.textContent = 'New Title'
-console.log(body)
+
+console.log(container)
