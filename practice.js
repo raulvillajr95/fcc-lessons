773f678175ee22481b2c788923eb089e61f5644e
Raul <raulvillajr95@gmail.com>
2022-05-27 19:15:15 -0700

    Learn how to manipulate DOM attributes

--- practice.js
+++ practice.js
@@ -1,3 +1,6 @@
-const root = document.body.NodeList
-
-console.log(root)
+let paras = document.body.getElementsByTagName("p");
+for (let para of Array.from(paras)) {
+  if (para.getAttribute("data-classified") == "secret") {
+    para.remove();
+  }
+}
