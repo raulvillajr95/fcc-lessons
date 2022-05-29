c7531b0d9f4cc23dde1fef4f4348e7e629c826f3
Raul <raulvillajr95@gmail.com>
2022-05-28 19:02:48 -0700

    Setting up to create first table

--- practice.js
+++ practice.js
@@ -1,6 +1,23 @@
-/* Build A Table 
+/* Build A Table */
 
+const MOUNTAINS = [
+  {name: "Kilimanjaro", height: 5895, place: "Tanzania"},
+  {name: "Everest", height: 8848, place: "Nepal"},
+  {name: "Mount Fuji", height: 3776, place: "Japan"},
+  {name: "Vaalserberg", height: 323, place: "Netherlands"},
+  {name: "Denali", height: 6168, place: "United States"},
+  {name: "Popocatepetl", height: 5465, place: "Mexico"},
+  {name: "Mont Blanc", height: 4808, place: "Italy/France"}
+]
+
+const mountainsDiv = document.getElementById('#mountains')
+
+/*
 ideas:
+1st-creat table with name, height, & place headings
+2nd-recreate the sample one
+  th's and 1 mountain
+
 -copy given code to see what gives
 -try some stuff out
 -reread
