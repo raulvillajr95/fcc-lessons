001d6aa5f609a9f4a421cf685fb6e3c88a9c872c
Raul <raulvillajr95@gmail.com>
2022-05-28 18:52:26 -0700

    'Build A Table' Exercise by EJS

--- practice.js
+++ practice.js
@@ -1,11 +1,7 @@
-let cat = document.querySelector("img");
-let angle = Math.PI / 2;
-function animate(time, lastTime) {
-  if (lastTime != null) {
-    angle += (time - lastTime) * 0.001
-  }
-  cat.style.top = (Math.sin(angle) * 20) + "px";
-  cat.style.left = (Math.cos(angle) * 200) + "px";
-  requestAnimationFrame(newTime => animate(newTime, time));
-}
-requestAnimationFrame(animate);
+/* Build A Table 
+
+ideas:
+-copy given code to see what gives
+-try some stuff out
+-reread
+*/
