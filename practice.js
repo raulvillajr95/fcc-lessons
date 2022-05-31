91541d93f37547e1c84acd0ff0b4a3d97fdf7aa1
Raul <raulvillajr95@gmail.com>
2022-05-30 22:56:41 -0700

    Just saving, don't know what's going on

--- practice.js
+++ practice.js
@@ -1,58 +1,38 @@
-/* Build A Table */
-
-const MOUNTAINS = [
-  {name: "Kilimanjaro", height: 5895, place: "Tanzania"},
-  {name: "Everest", height: 8848, place: "Nepal"},
-  {name: "Mount Fuji", height: 3776, place: "Japan"},
-  {name: "Vaalserberg", height: 323, place: "Netherlands"},
-  {name: "Denali", height: 6168, place: "United States"},
-  {name: "Popocatepetl", height: 5465, place: "Mexico"},
-  {name: "Mont Blanc", height: 4808, place: "Italy/France"}
-];
-
-const mountainsDiv = document.querySelector('#mountains');
-
-const table = document.createElement('table');
-const row = document.createElement('tr');
-
-mountainsDiv.appendChild(table);
-table.appendChild(row);
+const body = document.querySelector('body')
 
 
-/* loop to create heading section */
-let headings = ['name', 'height', 'place']
-for (let i = 0; i < headings.length; i++) {
-  const rowHeading = document.createElement('th')
-  rowHeading.innerText = headings[i]
-  row.appendChild(rowHeading);
-}
+// console.log(body.children.length)
 
-/* loop to create body */
-let mntKeys = ['name', 'height', "place"]
-for (let i = 0; i < MOUNTAINS.length; i++) {
-  const mntRow = document.createElement('tr')
-  table.appendChild(mntRow)
-  for (let j = 0; j < 3; j++) {
-    const rowCell = document.createElement('td')
+// for (const child of body.children) {
+//   console.log(child)
+// }
 
-    // make condition for 'height'
-    if (j == 1) {
-      rowCell.setAttribute('style','text-align: right;')
-    }
-    rowCell.innerText = MOUNTAINS[i][mntKeys[j]]
-
-    mntRow.appendChild(rowCell)
+function byTagName(node) {
+  if (node.children.length === 0) {
+    return
+  }
+  for (const child of body.children) {
+    console.log(child)
+    byTagName(child)
   }
 }
 
-console.log(mountainsDiv)
+byTagName(body)
+
+const tree = {
+  name: 'body',
+  children: [
+    {
+      name: 'h1',
+      children: [
+        {}
+      ]
+    }
+    {}
+  ]
+}
 
 /*
 ideas:
-2nd-recreate the sample one
-  th's and 1 mountain
-
--nested for loop??? 3 within 7??
--try stuff out
--reread
+-create manual body tree
 */
