5c5dddd9a5668049a5a0878ef9babcc87e3034fc
Raul <raulvillajr95@gmail.com>
2022-05-31 21:28:08 -0700

    Learn about removing event handlers

--- practice.js
+++ practice.js
@@ -1,19 +1,7 @@
-let cat = document.querySelector("#cat");
-let hat = document.querySelector("#hat");
+let button = document.querySelector("button");
 
-let angle = 0;
-let lastTime = null;
-function animate(time) {
-  if (lastTime != null) {
-    angle += (time - lastTime) * 0.001;
-  }
-  lastTime = time;
-  cat.style.top = (Math.sin(angle) * 40 + 40) + "px";
-  cat.style.left = (Math.cos(angle) * 200 + 230) + "px";
-
-  hat.style.top = (Math.sin(angle) * 40 + 40) + "px";
-  hat.style.right = (Math.cos(angle) * 200 + 230) + "px";
-
-  requestAnimationFrame(animate);
+function once() {
+  console.log("Done.")
+  button.removeEventListener("click", once);
 }
-requestAnimationFrame(animate);
+button.addEventListener("click", once)
