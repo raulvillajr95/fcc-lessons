70cb7ec6f38c5c0c4db5bfb20c3ec8096d1c2a17
Raul <raulvillajr95@gmail.com>
2022-06-01 17:46:16 -0700

    Learning touch events

--- practice.js
+++ practice.js
@@ -1,7 +1,15 @@
-let button = document.querySelector("button");
-
-function once() {
-  console.log("Done.")
-  button.removeEventListener("click", once);
+function update(event) {
+  for (let dot; dot = document.querySelector("dot");) {
+    dot.remove();
+  }
+  for (let i = 0; i < event.touches.length; i++) {
+    let {pageX, pageY} = event.touches[i];
+    let dot = document.createElement("dot");
+    dot.style.left = (pageX - 50) + "px";
+    dot.style.top = (pageY - 50) + "px";
+    document.body.appendChild(dot);
+  }
 }
-button.addEventListener("click", once)
+window.addEventListener("touchstart", update);
+window.addEventListener("touchmove", update);
+window.addEventListener("touchend", update);
