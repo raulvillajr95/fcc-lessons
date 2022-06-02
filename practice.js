83baba373b710b6b79689ff07ce7306f869b5eb3
Raul <raulvillajr95@gmail.com>
2022-06-02 16:04:14 -0700

    Complete 'Tabs' exercise

--- practice.js
+++ practice.js
@@ -6,15 +6,41 @@ const tab3 = document.querySelector('[data-tabname="three"]')
 let tabs = [tab1, tab2, tab3]
 
 function asTabs(node) {
-  let nodeSet = new Array(node.children)
-
   for (let i = 0; i < 3; i++) {
     let button = document.createElement("button")
     button.innerText = `${tabs[i].getAttribute("data-tabname")}`
+    button.className = `button${i+1}`
+    // set onclick attributes
+    button.setAttribute('onclick', `visibility('tab${i+1}')`)
     tabPanel.prepend(button)
   }
-
-  console.log(nodeSet)
-  console.log(node.div)
 }
 asTabs(document.querySelector("tab-panel"));
+
+let buttons = {
+  tab1 : document.querySelector('.button1'),
+  tab2 : document.querySelector('.button2'),
+  tab3 : document.querySelector('.button3')
+}
+
+// make all 3 invisible
+function invisible() {
+  tab1.style.display = 'none';
+  tab2.style.display = 'none';
+  tab3.style.display = 'none';
+}
+invisible()
+
+function visibility(tbStr) {
+  console.log(buttons[tbStr])
+  if (tbStr == 'tab1') {
+    invisible()
+    tab1.style.display = "block";
+  } else if (tbStr == 'tab2') {
+    invisible()
+    tab2.style.display = "block";
+  } else if (tbStr == 'tab3') {
+    invisible()
+    tab3.style.display = "block";
+  }
+}
