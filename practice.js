aede08340e54f80fb61b36e86e4ed5f0d511f1f4
Raul <raulvillajr95@gmail.com>
2022-06-01 20:03:32 -0700

    Complete 'Balloon' exercise from eloquentJS

--- practice.js
+++ practice.js
@@ -1,15 +1,22 @@
-function update(event) {
-  for (let dot; dot = document.querySelector("dot");) {
-    dot.remove();
+const balloon = document.querySelector('p')
+let fontS = 10;
+balloon.style.fontSize = `${fontS}px`;
+
+window.document.addEventListener("keydown", event => {
+  if (fontS > 200) {
+    balloon.textContent = '💥'
+  } else {
+    balloon.textContent = '🎈'
   }
-  for (let i = 0; i < event.touches.length; i++) {
-    let {pageX, pageY} = event.touches[i];
-    let dot = document.createElement("dot");
-    dot.style.left = (pageX - 50) + "px";
-    dot.style.top = (pageY - 50) + "px";
-    document.body.appendChild(dot);
+  if (event.key == "ArrowDown") {
+    console.log('You pressed DOWN')
+    console.log(fontS)
+    fontS *= 0.9
+    balloon.style.fontSize = `${fontS}px`
+  } else if (event.key == "ArrowUp") {
+    console.log('You pressed UP')
+    console.log(fontS)
+    fontS *= 1.1
+    balloon.style.fontSize = `${fontS}px`
   }
-}
-window.addEventListener("touchstart", update);
-window.addEventListener("touchmove", update);
-window.addEventListener("touchend", update);
+})
