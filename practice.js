daf2cde0cfb63da1dec739326f5ee7b64c2c0a8b
Raul <raulvillajr95@gmail.com>
2022-06-07 20:48:48 -0700

    Learn about white space text nodes

--- practice.js
+++ practice.js
@@ -1,6 +1,1 @@
-let div = document.querySelector('div')
-
-div.scrollTop = 750
-div.scrollLeft = 750
-
-console.log(div.scrollTop, div.scrollLeft)
+console.log(document.querySelector('#p1').nextSibling)
