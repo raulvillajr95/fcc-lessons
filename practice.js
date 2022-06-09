2079b595fc55bf44ad9765dbea3604644501cc55
Raul <raulvillajr95@gmail.com>
2022-06-08 20:09:51 -0700

    Learn about .insertRule() and .deleteRule()

--- practice.js
+++ practice.js
@@ -1,1 +1,7 @@
-console.log(document.querySelector('#p1').nextSibling)
+document.querySelector('#styleElement').sheet.insertRule('p{color:red}', 1)
+
+console.log(document.querySelector('#styleElement').sheet.cssRules[1].cssText)
+
+document.querySelector('#styleElement').sheet.deleteRule(1)
+
+console.log(document.querySelector('#styleElement').sheet.cssRules[1].cssText)
