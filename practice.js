957697d3ec015d0d220dfca54969178c79ac2bce
Raul <raulvillajr95@gmail.com>
2022-06-12 20:27:26 -0700

    'Item Lister' project finished

--- practice.js
+++ practice.js
@@ -1,3 +1,51 @@
 let form = document.getElementById('addForm')
 let itemList = document.getElementById('items')
+let filter = document.getElementById('filter')
 
+form.addEventListener('submit', addItem)
+itemList.addEventListener('click', removeItem)
+filter.addEventListener('keyup', filterItems)
+
+function addItem(e) {
+  e.preventDefault()
+
+  let newItem = document.getElementById('item').value
+
+  let li = document.createElement('li')
+
+  li.className = 'list-group-item'
+  
+  li.appendChild(document.createTextNode(newItem))
+
+  let deleteBtn = document.createElement('button')
+
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete'
+
+  deleteBtn.appendChild(document.createTextNode('X'))
+
+  li.appendChild(deleteBtn)
+
+  itemList.appendChild(li)
+}
+
+function removeItem(e) {
+  if (e.target.classList.contains('delete')) {
+    if (confirm('Are You Sure?')) {
+      let li = e.target.parentElement
+      itemList.removeChild(li)
+    }
+  }
+}
+
+function filterItems(e) {
+  let text = e.target.value.toLowerCase()
+  let items = itemList.getElementsByTagName('li')
+  Array.from(items).forEach(function(item) {
+    let itemName = item.firstChild.textContent;
+    if (itemName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  })
+}
