d161399f80ed2021aa167a40d13b93199d605b21
Raul <raulvillajr95@gmail.com>
2022-06-16 19:17:13 -0700

    Complete 'Sum object properties' task

--- practice.js
+++ practice.js
@@ -1,51 +1,22 @@
-let form = document.getElementById('addForm')
-let itemList = document.getElementById('items')
-let filter = document.getElementById('filter')
-
-form.addEventListener('submit', addItem)
-itemList.addEventListener('click', removeItem)
-filter.addEventListener('keyup', filterItems)
-
-function addItem(e) {
-  e.preventDefault()
-
-  let newItem = document.getElementById('item').value
-
-  let li = document.createElement('li')
-
-  li.className = 'list-group-item'
-  
-  li.appendChild(document.createTextNode(newItem))
-
-  let deleteBtn = document.createElement('button')
-
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete'
-
-  deleteBtn.appendChild(document.createTextNode('X'))
-
-  li.appendChild(deleteBtn)
-
-  itemList.appendChild(li)
-}
-
-function removeItem(e) {
-  if (e.target.classList.contains('delete')) {
-    if (confirm('Are You Sure?')) {
-      let li = e.target.parentElement
-      itemList.removeChild(li)
+function isEmpty(obj) {
+  let lst = []
+  for (const key in obj) {
+    if (key) {
+      lst.push(key)
     }
   }
+  return lst.length == 0
 }
 
-function filterItems(e) {
-  let text = e.target.value.toLowerCase()
-  let items = itemList.getElementsByTagName('li')
-  Array.from(items).forEach(function(item) {
-    let itemName = item.firstChild.textContent;
-    if (itemName.toLowerCase().indexOf(text) != -1) {
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  })
+let salaries = {
+}
+
+let sum = 0;
+if (isEmpty(salaries)) {
+  console.log(sum)
+} else {
+  for (const key in salaries) {
+    sum += salaries[key]
+  }
+  console.log(sum)
 }
