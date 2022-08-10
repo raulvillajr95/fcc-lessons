b187006afdf280cb9c29c5d621124d0dadcf6f83
Raul <raulvillajr95@gmail.com>
2022-08-09 21:09:36 -0700

    Learning how to cutomized a checkbox

--- practice.js
+++
@@ -1,7 +0,0 @@
-const name = document.querySelector("#username")
-const greetMeButton = document.querySelector(".greet-btn")
-const greetingOutput = document.querySelector(".greeting")
-
-greetMeButton.addEventListener('click', (event) => {
-  greetingOutput.innerText = `Hello ${name.value}`;
-})
