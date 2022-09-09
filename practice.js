9b557135054b57f692a3d396ff39b9bb6251ba29
Raul <raulvillajr95@gmail.com>
2022-09-08 21:22:10 -0700

    Learning about mixins with classes

--- practice.js
+++ practice.js
@@ -1,35 +1,32 @@
-const TAX_RATE = 0.08;
-const PHONE = 99.99;
-const ACCESSORY_PRICE = 9.99;
-
-let bank_balance = Number(prompt("Bank account balance: "));
-let phonesAccPurchased = 0;
-let total = 0;
-
-function calulateTax(amt, tax) {
-  return amt + (amt * tax);
-}
-function priceFormat(price) {
-  return "$" + price.toFixed(2);
+// vastly simplified `mixin(..)` example:
+function mixin(sourceObj, targetObj) {
+  for (var key in sourceObj) {
+    // only copy if not already present
+    if (!(key in targetObj)) {
+      targetObj[key] = sourceObj[key];
+    }
+  }
+  return targetObj;
 }
 
-while ((PHONE + ACCESSORY_PRICE) <= bank_balance) {
-  phonesAccPurchased += 1;
-  bank_balance -= calulateTax((PHONE + ACCESSORY_PRICE), TAX_RATE)
-  total += calulateTax((PHONE + ACCESSORY_PRICE), TAX_RATE)
-}
+var Vehicle = {
+	engines: 1,
 
-console.log("Total is " + priceFormat(total) + ", and you bought " + phonesAccPurchased + " phone combos.")
+	ignition: function() {
+		console.log( "Turning on my engine." );
+	},
 
-/*
-Goal: Given bank balance, how many
-phones+accesories can you buy?
+	drive: function() {
+		this.ignition();
+		console.log( "Steering and moving forward!" );
+	}
+};
 
-Return in form: "Total is $###.##, and you
-bought # phone combos."
+var Car = mixin(Vehicle, {
+	wheels: 4,
 
--at the end, implement prompt()
-
-*instructions weren't clear so I'm just free
-balling it.
-*/
+	drive: function() {
+		Vehicle.drive.call(this);
+		console.log( "Rolling on all " + this.wheels + " wheels!" )
+	}
+});
