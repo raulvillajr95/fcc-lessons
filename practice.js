7aa67443e1df4d3b1afa3294e0822fdc5b9fb5f2
Raul <raulvillajr95@gmail.com>
2022-09-28 19:52:35 -0700

    Learning about asynquence

--- practice.js
+++ practice.js
@@ -1,5 +1,63 @@
-var a = new Array(1,2,3);
-console.log(a) // [1, 2, 3]
+function success1(done) {
+  setTimeout(function() {
+    done(1);
+  }, 100);
+}
 
-var b = [1,2,3];
-console.log(b) // [1, 2, 3]
+function success2(done) {
+  setTimeout(function() {
+    done(2);
+  }, 100);
+}
+
+function failure3(done) {
+  setTimeout(function() {
+    done.fail(3);
+  }, 100);
+}
+
+function output(msg) {
+  console.log(msg)
+}
+
+ASQ().race(
+  failure3,
+  success1
+)
+.or(output);
+
+ASQ().any(
+  success1,
+  failure3,
+  success2
+)
+.val(function() {
+  var args = [].slice.call(arguments);
+  console.log(
+    args
+  );
+});
+
+ASQ().first(
+  failure3,
+  success1,
+  success2
+)
+.val(output);
+
+ASQ().last(
+  failure3,
+  success1,
+  success2
+)
+.val(output);
+
+ASQ().none(
+  failure3
+)
+.val(output)
+.none(
+  failure3,
+  success1
+)
+.or(output);
