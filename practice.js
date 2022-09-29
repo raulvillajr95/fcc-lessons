188d805b302da7f92e4bce5840cce119b9f77e6f
Raul <raulvillajr95@gmail.com>
2022-09-29 11:51:46 -0700

    The '3x + 1' equation

--- practice.js
+++ practice.js
@@ -1,63 +1,10 @@
-function success1(done) {
-  setTimeout(function() {
-    done(1);
-  }, 100);
+let num = 10010100101010
+
+while (num !== 1) {
+  if (num % 2 === 1) {
+    num = (num * 3) + 1;
+  } else if (num % 2 === 0) {
+    num = num / 2;
+  }
+  console.log(num)
 }
-
-function success2(done) {
-  setTimeout(function() {
-    done(2);
-  }, 100);
-}
-
-function failure3(done) {
-  setTimeout(function() {
-    done.fail(3);
-  }, 100);
-}
-
-function output(msg) {
-  console.log(msg)
-}
-
-ASQ().race(
-  failure3,
-  success1
-)
-.or(output);
-
-ASQ().any(
-  success1,
-  failure3,
-  success2
-)
-.val(function() {
-  var args = [].slice.call(arguments);
-  console.log(
-    args
-  );
-});
-
-ASQ().first(
-  failure3,
-  success1,
-  success2
-)
-.val(output);
-
-ASQ().last(
-  failure3,
-  success1,
-  success2
-)
-.val(output);
-
-ASQ().none(
-  failure3
-)
-.val(output)
-.none(
-  failure3,
-  success1
-)
-.or(output);
