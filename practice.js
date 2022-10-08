0f657a7f4af0625eb1c76fb8520d52c3250f5082
Raul <raulvillajr95@gmail.com>
2022-10-07 23:42:05 -0700

    Learn about objects and contructors

--- practice.js
+++ practice.js
@@ -1,19 +1,26 @@
-var o = { a: 1, b: 2 };
-var p = Object.create( o );
-p.c = 3;
-p.d = 4;
-
-for (var prop of Reflect.enumerate( p )) {
-	console.log( prop );
+function Player(name, marker) {
+  this.name = name
+  this.marker = marker
+  this.sayName = function() {
+    console.log(name)
+  }
 }
-// c d a b
 
-for (var prop in p) {
-	console.log( prop );
-}
-// c d a b
+const player1 = new Player('steve', 'X')
+const player2 = new Player('also steve', 'O')
+player1.sayName()
+player2.sayName()
 
-JSON.stringify( p );
-// {"c":3,"d":4}
 
-Object.keys( p );
+// function Book(title, author, pages, read) {
+//   this.title = title
+//   this.author = author
+//   this.pages = pages
+//   this.read = read
+//   this.info = function() {
+//     return `${title} by ${author}, ${pages}, ${read}`
+//   }
+// }
+
+// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
+// console.log(theHobbit.info())
