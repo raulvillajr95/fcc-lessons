efad2737063d06736813806943f511de8c403eea
Raul <raulvillajr95@gmail.com>
2022-10-14 22:53:23 -0700

    Learn about constructors and prototypes

--- practice.js
+++ practice.js
@@ -1,7 +1,60 @@
-var object = new Object()
-console.log(object)
+class Animal {
+  constructor (name, energy) {
+    this.name = name
+    this.energy = energy
+  }
+  eat(amount) {
+    console.log(`${this.name} is eating.`)
+    this.energy += amount
+  }
+  sleep(length) {
+    console.log(`${this.name} is sleeping.`)
+    this.energy += length
+  }
+  play(length) {
+    console.log(`${this.name} is playing.`)
+    this.energy -= length
+  }
+  static nextToEat(animals) {
+    const sortedByLeastEnergy = animals.sort((a,b) => {
+      return a.energy - b.energy
+    })
 
-var book = {
-  "name": "The Principles of Object-Oriented Javascript",
-  'year': 2014
+    return sortedByLeastEnergy[0].name
+  }
+}
+
+const leo = new Animal('Leo', 7)
+
+//////////////////////////////
+
+class Dog extends Animal {
+  constructor(name, energy, breed) {
+    super(name, energy)
+
+    this.breed = breed
+  }
+  bark() {
+    console.log('Woof Woof!')
+    this.energy -= .1
+  }
+}
+
+const charlie = new Dog('Charlie', 10, 'Goldendoodle')
+console.log(charlie.constructor)
+
+//////////////////////////////
+
+function Cat(name, energy, declawed) {
+  Animal.call(this, name, energy)
+
+  this.declawed = declawed
+}
+
+Cat.prototype = Object.create(Animal.prototype)
+Cat.prototype.constructor = Cat
+
+Cat.prototype.meow = function() {
+  console.log('Meow!')
+  this.energy -= .1
 }
