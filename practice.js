2e5e667d8b51becf20d5723a3262b5297216fd5d
Raul <raulvillajr95@gmail.com>
2022-10-22 18:35:21 -0700

    Learn about States within Observer pattern

--- practice.js
+++ practice.js
@@ -1,20 +1,106 @@
-const myPrivateVar = new WeakMap()
-const myPrivateMethod = new WeakMap()
-
-class MyNamespace {
+class ObserverList {
   constructor() {
-    myPrivateVar.set(this, 0)
-    myPrivateMethod.set(this, foo => console.log(foo))
-    this.myPublicVar = 'foo';
+    this.observerList = [];
   }
 
-  myPublicFunction(bar) {
-    let privateVar = myPrivateVar.get(this);
-    const privateMethod = myPrivateMethod.get(this)
+  add(obj) {
+    return this.observerList.push(obj);
+  }
 
-    privateVar++
-    myPrivateVar.set(this, privateVar)
+  count() {
+    return this.observerList.length;
+  }
 
-    privateMethod(bar)
+  get(index) {
+    if (index > -1 && index < this.observerList.length) {
+      return this.observerList[index];
+    }
+  }
+
+  indexOf(obj, startIndex) {
+    let i = startIndex;
+
+    while (i < this.observerList.length) {
+      if (this.observerList[i] === obj) {
+        return i;
+      }
+      i++;
+    }
+
+    return -1;
+  }
+
+  removeAt(index) {
+    this.observerList.splice(index, 1);
   }
 }
+
+class Subject {
+  constructor() {
+    this.observers = new ObserverList();
+  }
+
+  addObserver(observer) {
+    this.observers.add(observer);
+  }
+
+  removeObserver(observer) {
+    this.observers.removeAt(this.observers.indexOf(observer, 0));
+  }
+
+  notify(context) {
+    const observerCount = this.observers.count();
+    for(let i = 0; i < observerCount; i++) {
+      this.observers.get(i).update(context);
+    }
+  }
+}
+
+class Observer {
+  constructor() {}
+  update() {
+
+  }
+}
+
+class ConcreteSubject extends Subject {
+  constructor(element) {
+
+    super();
+    this.element = element;
+
+    this.element.onclick = () => {
+      this.notify(this.element.checked);
+    };
+  }
+}
+
+class ConcreteObserver extends Observer {
+  constructor(element) {
+    super();
+    this.element = element;
+  }
+
+  update(value) {
+    this.element.checked = value;
+  }
+}
+
+const addBtn = document.getElementById('addNewObserver');
+const container = document.getElementById('observersContainer');
+const controlCheckbox = new ConcreteSubject(
+  document.getElementById('mainCheckbox')
+);
+
+const addNewObserver = () => {
+  
+  const check = document.createElement('input');
+  check.type = 'checkbox';
+  const checkObserver = new ConcreteObserver(check);
+
+  controlCheckbox.addObserver(checkObserver);
+
+  container.appendChild(check);
+}
+
+addBtn.onclick = addNewObserver;
