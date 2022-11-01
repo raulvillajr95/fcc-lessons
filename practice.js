e62b918ef7f817076a1fdebfacf66f0312230930
Raul <raulvillajr95@gmail.com>
2022-10-31 21:09:45 -0700

    Learning about minmax

--- practice.js
+++ practice.js
@@ -1,29 +1,37 @@
-// 2 ✅ Write factory method that returns an object
-let Person = function(name) {
-  let logOut = () => {
-    return `${name} has logged out!`
+function minimax(depth, nodeIndex, isMax, scores, h) {
+
+  if (depth == h) {
+    return scores[nodeIndex]
   }
-
-  return {logOut}
-}
-
-let raul = Person('Raul');
-console.log(raul.logOut())
-
-
-// ✅ 6 Use inheritance in objects using the factory pattern
-let Person2 = function(name) {
-  let logIn = () => {
-    return `${name} has logged in!`
-  }
-
-  let {logOut} = Person(name);
-
-  return {
-    logIn,
-    logOut
+  
+  if (isMax) {
+    return Math.max(minimax(depth+1, nodeIndex*2, false, scores, h),
+      minimax(depth+1, nodeIndex*2 + 1, false, scores, h));
+  } else {
+    return Math.min(minimax(depth+1, nodeIndex*2, true, scores, h),
+      minimax(depth+1, nodeIndex*2 + 1, true, scores, h));
   }
 }
 
-let jose = Person2('Jose');
-console.log(jose.logOut())
+let scores = [4,8,9,3,2,-2,9,-1];
+let n = scores.length;
+let h = Math.log2(n);
+let res = minimax(0,0,true,scores,h);
+console.log("The optimal value is: " + res)
+console.log(scores.indexOf(res))
+
+/*
+ideas:
+-i wanna truly understand how minimax works
+-the very bottom of the tree might be 'scores'
+  -it might change every time an x is placed
+  -when you find the optimal value,
+  get that index and place an "O there"
+    -i could use the index and place it on the board
+    of Xs and Os
+    -*essesntially, can the score array be be aligned
+    with the board array
+-i need a scoring system, it could be a range of points
+  -ex: 10 for the best possible move, and -10 for
+  the worst possible move
+*/
