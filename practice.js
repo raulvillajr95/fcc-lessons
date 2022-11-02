696dca0ed393c46cd533913863a3529b31420cdd
Raul <raulvillajr95@gmail.com>
2022-11-02 12:24:03 -0700

    idk wtf is going on

--- practice.js
+++ practice.js
@@ -13,52 +13,135 @@ function minimax(depth, nodeIndex, isMax, scores, h) {
   }
 }
 
-let board = [""]
+let board = ["","","",""];
+let x = 0;
+let o = 0;
+let scores = [-1,1,1,-1];
 
-let scores = [];
+// Turn 1 - Me
+let myFirstChoice = Number(prompt(`pick an index: ["${board[0]}","${board[1]}","${board[2]}","${board[3]}"]`))
+board[myFirstChoice] = "X"
+console.log("Turn 1:",board)
 
-for (let i = 0; i < 524288; i++) {
-  scores.push(i)
+for (let i = 0; i < board.length; i++) {
+  if (board[i] === "X") {
+    scores[i] -= 1
+  } else if (board[i] === "O") {
+    scores[i] -= 1
+  }
 }
+console.log(scores)
 
+// Turn 2 - CPU
+console.log("right before turn 2",scores)
 let n = scores.length;
-let h = Math.log2(n);
+// maybe change 3 to n
+let h = Math.log(n)/Math.log(4);
 let res = minimax(0,0,true,scores,h);
 console.log("The optimal value is: " + res)
 let index = scores.indexOf(res)
-//board[index] = "O"
 
-let ex = [];
-//console.log(ex)
+board[index] = "O"
+console.log("Turn 2:", board)
+
+console.log(scores)
+for (let i = 0; i < board.length; i++) {
+  if (board[i] === "X") {
+    scores[i] = -1
+  } else if (board[i] === "O") {
+    scores[i] = -1
+  }
+}
+
+// Turn 3 - Me
+let mySecondChoice = Number(prompt(`pick an index: ["${board[0]}","${board[1]}","${board[2]}","${board[3]}"]`))
+board[mySecondChoice] = "X"
+console.log("Turn 3:",board)
+
+console.log(scores)
+scores = [-1,1,1,-1];
+for (let i = 0; i < board.length; i++) {
+  if (board[i] === "X") {
+    scores[i] -= 1
+    console.log("reached X")
+  } else if (board[i] === "O") {
+    scores[i] -= 1
+    console.log("reached O")
+  } else {
+    scores[i] += 1
+  }
+}
+
+// Turn 4 - CPU
+console.log("right before turn 4",scores)
+n = scores.length;
+// maybe change 3 to n
+h = Math.log(n)/Math.log(4);
+res = minimax(0,0,true,scores,h);
+console.log("The optimal value is: " + res)
+index = scores.indexOf(res)
+// console.log("turn 4", index)
+
+board[index] = "O"
+console.log("Turn 4:", board)
+// console.log(scores)
+
+console.log(scores)
+for (let i = 0; i < board.length; i++) {
+  if (board[i] === "X") {
+    scores[i] = null
+  } else if (board[i] === "O") {
+    scores[i] = null
+  }
+}
+
+// End of game
+// console.log("Scores", x, o)
+let scoresPrincipal = [-1,1,1,-1];
+for (let i = 0; i < board.length; i++) {
+  if (board[i] === "O") {
+    o += scoresPrincipal[i]
+  } else if (board[i] === "X") {
+    x += scoresPrincipal[i]
+  }
+}
+
+if (x > o) {
+  winner = "X"
+} else if (o > x) {
+  winner = "O"
+} else if (o == x) {
+  winner = "TIE, nobody"
+}
+
+console.log(null == 0)
+// console.log(`End: ${winner} wins!`)
+// console.log("Scores", x, o)
 
 // 1 win, 0 draw, -1 lose
 
 /*
 to-do:
--create a 1 box tic tac toe cpu
-  -still using min max, not just "if empty, fill in box"
-  -obviously at 1 box, it's just 1 move, only cpu, not me then cpu
-  -i can copy my current project onto a new practice one
-  -set up a score for the box
-    -goal: just place box
--create a 2 box tic tac toe cpu
-  -still using min max, not just "if empty, fill in box"
-  -at 2 boxes, it's 2 moves, me then cpu 
-  -set up a score for the box
-    -goal: place box on left
--create a 3 box tic tac toe cpu
-  -still using min max, not just "if empty, fill in box"
-  -at 3 boxes, it's 3 moves, me then cpu then me
-  -set up a score for the boxes
-    -goal: place box on left
 -create a 4 box tic tac toe cpu
   -still using min max, not just "if empty, fill in box"
   -at 4 boxes, it's 4 moves, me then cpu then me then cpu
   -set up a score for the boxes
-    -goal: place box on top 2
+    -goal: place box on left 2
+-then try 4 but 2 arrays of 2
+["",""]
+["",""]
 -and so on, just increase box number, 5, 6, 7, 8, 9
 
 ideas:
+-for 4 boxes,
+  i could slice array off that was picked
+    save the index number
+  place everything back together
+-i could do 3 arrays of 3 instead of 1 array of 9
+and work that out some how
+["","",""]
+["","",""]
+["","",""]
 -give it a set of correct array, like the one I already have
   -if the choice leads to that then make the move
   -using scores like -1= lose, 0 = tie, 1 = win
