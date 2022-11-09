cfc19b60cfc3dc4f87b8d3031e363c93454c2819
Raul <raulvillajr95@gmail.com>
2022-11-08 22:55:49 -0800

    Function notation to class 'my way'

--- practice.js
+++ practice.js
@@ -1,38 +1,36 @@
-let timer;
-
 class Clock {
+  constructor() {
+    this.stop = () => {clearInterval(this.timer)};
+    this.timer = setInterval(this.render, 1000)
+  }
 
-  constructor({ template }) {
-    this.render = function() {
-  
-      let date = new Date();
-  
-      let hours = date.getHours();
-      if (hours < 10) hours = '0' + hours;
-  
-      let mins = date.getMinutes();
-      if (mins < 10) mins = '0' + mins;
-  
-      let secs = date.getSeconds();
-      if (secs < 10) secs = '0' + secs;
-  
-      let output = template
-        .replace('h', hours)
-        .replace('m', mins)
-        .replace('s', secs);
-  
-      console.log(output);
-    };
-    this.start = function() {
-      this.render();
-      timer = setInterval(this.render, 1000);
-    };
-    this.stop = function() {
-      clearInterval(timer);
-    };
+  render() {
+    let template = 'h:m:s';
+
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output)
+  }
+
+  start() {
+    this.render();
+    this.timer;
   }
 }
 
-let clock = new Clock({template: 'h:m:s'});
-clock.start();
-
+let clock = new Clock();
+clock.start()
