30a56abb5b5451ff1df0ca8580fe6d25be6da263
Raul <raulvillajr95@gmail.com>
2022-11-24 16:05:10 -0800

    py4e exercise 3.3 complete

--- prac.py
+++ prac.py
@@ -1,7 +1,19 @@
-inp = input('Enter Fahrenheit Temperature:')
 try:
-  fahr = float(inp)
-  cel = (fahr - 32.0) * 5.0 / 9.0
-  print(cel)
+  score = float(input("Enter score: "))
+
+  if score > 1.0:
+    print("Bad score")
+  elif score >= 0.9:
+    print("A")
+  elif score >= 0.8:
+    print("B")
+  elif score >= 0.7:
+    print("C")
+  elif score >= 0.6:
+    print("D")
+  elif score >= 0.0:
+    print("F")
+  else:
+    print("Bad score")
 except:
-  print('Please enter a number')
+  print('Bad score')
