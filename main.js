76e64c5c8ecebf8e17d769f4669ea58f27c83a1b
Raul <raulvillajr95@gmail.com>
2022-12-02 13:01:54 -0800

    Complete py4e 5.1 exercise

--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-class Bottles {
-  song() {
-    return this.verses(99, 0);
-  }
+let foo;
 
-  verses(hi, lo) {
-    return downTo(hi, lo).map(n => this.verse(n)).join('\n');
-  }
-
-  verse(n) {
-    return (
-      `${n === 0 ? 'No more' : n} bottle${n === 1 ? '' : 's'}` +
-      ' of beer on the wall, ' +
-      `${n === 0 ? 'no more' : n} bottle${n === 1 ? '' : 's'} of beer.\n` +
-      `${n > 0 ? `Take ${n > 1 ? 'one' : 'it'} down and pass it around`
-        : 'Go to the store and buy some more'}, ` +
-      `${n-1 < 0 ? 99 : n-1 === 0 ? 'no more' : n-1} bottle${n-1 === 1 ? '' : 's'}` +
-      ' of beer on the wall.\n'
-    );
-  }
-}
-
-const downTo = (max, min) => {
-  const numbers = [];
-  for (let n = max; n >= min; n--) {
-    numbers.push(n);
-  }
-  return numbers;
-};
-
-let bottle = new Bottles()
-
-
-console.log(88, new Bottles().verse(99))
+console.log(`Variable 'foo' is equal to: ${foo}`)
--- prac.py
+++ prac.py
@@ -1,37 +1,18 @@
-# 4. d
-# 5. d
+def average(total, count):
+  return total/count
 
-# 6.
-def computepay(hours, rate):
-  hr = float(hours)
-  rt = float(rate)
-
-  if hr <= 40:
-    return hr * rt
-  elif hr > 40:
-    ex = hr - 40
-    return ex * (1.5 * rt) + 40 * rt
-#print("Pay",computepay(input('Enter Hours: '), input('Enter Rate: ')))
-
-# 7.
-def computegrade(score):
+total = 0
+count = 0
+while True:
+  inp = input('Enter a number: ')
+  if inp == 'done':
+    break
   try:
-    sc = float(score)
-
-    if sc > 1.0:
-      return "Bad score"
-    elif sc >= 0.9:
-      return "A"
-    elif sc >= 0.8:
-      return "B"
-    elif sc >= 0.7:
-      return "C"
-    elif sc >= 0.6:
-      return "D"
-    elif sc >= 0.0:
-      return "F"
-    else:
-      return "Bad score"
+    num = float(inp)
   except:
-    return "Bad score"
-#print(computegrade(input('Enter score: ')))
+    print('Invalid input')
+    continue
+  total = total + num
+  count = count + 1
+
+print(total, count, average(total, count))
