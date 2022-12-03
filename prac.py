74c87cefa5407654b9a1470c83309ca7213258fa
Raul <raulvillajr95@gmail.com>
2022-12-03 10:38:25 -0800

    Complete 5.2 exercise

--- prac.py
+++ prac.py
@@ -1,8 +1,27 @@
-def average(total, count):
-  return total/count
+# 5.1
+# def average(total, count):
+#   return total/count
 
-total = 0
-count = 0
+# total = 0
+# count = 0
+# while True:
+#   inp = input('Enter a number: ')
+#   if inp == 'done':
+#     break
+#   try:
+#     num = float(inp)
+#   except:
+#     print('Invalid input')
+#     continue
+#   total = total + num
+#   count = count + 1
+
+# print(total, count, average(total, count))
+
+
+# 5.2
+max = None
+min = None
 while True:
   inp = input('Enter a number: ')
   if inp == 'done':
@@ -12,7 +31,8 @@ while True:
   except:
     print('Invalid input')
     continue
-  total = total + num
-  count = count + 1
-
-print(total, count, average(total, count))
+  if max is None or num > max:
+    max = num
+  if min is None or num < min:
+    min = num
+print(max, min)
