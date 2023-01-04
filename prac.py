98eee28b379e58642664f6eab200dc9b023bcae7
Raul <raulvillajr95@gmail.com>
2023-01-04 12:10:57 -0800

    Complete py4e ch 9

--- prac.py
+++ prac.py
@@ -1,22 +1,62 @@
-import string
+# 9.1
+# fname = input('Enter file name: ')
+# fhandle = open(fname)
+# wordKeys = dict()
+# for line in fhandle:
+#   for word in line.split():
+#     if word not in wordKeys:
+#       wordKeys[word] = 1
+#     else:
+#       wordKeys[word] = wordKeys[word] + 1
+# print(wordKeys)
 
-fname = input('Enter the file name: ')
-try:
-    fhand = open(fname)
-except:
-    print('File cannot be opened:', fname)
-    exit()
+# 9.2
+# fname = input('Enter file name: ')
+# fhandle = open(fname)
+# day_count = dict()
+# for line in fhandle:
+#   sline = line.split()
+#   if len(sline) < 3: continue
+#   if sline[0] != 'From': continue
+#   day_count[sline[2]] = day_count.get(sline[2], 0) + 1
+# print(day_count)
 
-counts = dict()
-for line in fhand:
-    line = line.rstrip()
-    line = line.translate(line.maketrans('', '', string.punctuation))
-    line = line.lower()
-    words = line.split()
-    for word in words:
-        if word not in counts:
-            counts[word] = 1
-        else:
-            counts[word] += 1
+# 9.3
+# fname = input('Enter file name: ')
+# fhandle = open(fname)
+# day_count = dict()
+# for line in fhandle:
+#   sline = line.split()
+#   if len(sline) < 3: continue
+#   if sline[0] != 'From': continue
+#   day_count[sline[1]] = day_count.get(sline[1], 0) + 1
+# print(day_count)
 
-print(counts)
+# 9.4
+fname = input('Enter file name: ')
+fhandle = open(fname)
+email_count = dict()
+for line in fhandle:
+  sline = line.split()
+  if len(sline) < 3: continue
+  if sline[0] != 'From': continue
+  email_count[sline[1]] = email_count.get(sline[1], 0) + 1
+email_max_count = -1
+email_max_name = ''
+for email in email_count:
+  if email_count[email] > email_max_count:
+    email_max_count = email_count[email]
+    email_max_name = email
+print(email_max_name, email_max_count)
+
+# 9.5
+# fname = input('Enter file name: ')
+# fhandle = open(fname)
+# domain_count = dict()
+# for line in fhandle:
+#   sline = line.split()
+#   if len(sline) < 3: continue
+#   if sline[0] != 'From': continue
+#   domain = sline[1].split('@')[1]
+#   domain_count[domain] = domain_count.get(domain, 0) + 1
+# print(domain_count)
