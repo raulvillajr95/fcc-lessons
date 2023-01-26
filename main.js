be7c95b8c0256fef66a07da1a90c42f0684d49ed
Raul <raulvillajr95@gmail.com>
2023-01-25 21:13:08 -0800

    Creat 'n' height right aligned '#' tree using mx+b formula

--- main.js
+++ main.js
@@ -4,65 +4,31 @@
  *
  */
 
-// function translate2d(dx, dy) {
-//   return function (x, y) {
-//     return [(dx += x), (dy += y)];
-//   };
-// }
-// const moveCoordinatesRight2Px = translate2d(2, 0);
-// const result = moveCoordinatesRight2Px(4, 8);
-
-// function scale2d(sx, sy) {
-//   return function (x, y) {
-//     return [sx * x, sy * y];
-//   };
-// }
-// const doubleScale = scale2d(2, 2);
-// const result2 = doubleScale(6, -3);
-
-// function composeTransform(f, g) {
-//   return function (x, y) {
-//     let midResult = f(x, y);
-//     return g(midResult[0], midResult[1]);
-//   };
-// }
-// const composeTransformations = composeTransform(
-//   moveCoordinatesRight2Px,
-//   doubleScale
-// );
-// const result3 = composeTransformations(0, 1);
-
-function translate2d(dx, dy) {
-  return function (nx, ny) {
-    dx += nx;
-    dy += ny;
-    return [dx, dy];
-  };
+function staircase(num) {
+  for (let i = 0; i < num; i++) {
+    let hashLine = [];
+    for (let j = 0; j < num; j++) {
+      if (i >= -j + (num - 1)) {
+        hashLine.push("#");
+      } else {
+        hashLine.push(" ");
+      }
+      // console.log(j + 3, i);
+    }
+    console.log(hashLine.join(""));
+  }
 }
-// let longName = translate2d(-6, 10);
-// const result = longName(4, 8); // [6, 8]
-// console.log(result);
 
-function scale2d(sx, sy) {
-  return function (nx, ny) {
-    sx *= nx;
-    sy *= ny;
-    return [sx, sy];
-  };
-}
-// const scaleName = scale2d(3, 3);
-// const result2 = scaleName(6, -3);
-// console.log(result2);
+staircase(15);
 
-function composeTransform(f, g) {
-  return function (nx, ny) {
-    let [newx, newy] = f(nx, ny);
-    return g(newx, newy);
-  };
-}
-// const composed = composeTransform(longName, scaleName);
-// console.log(composed(0, 0));
+// [00' ', 01' ', 02' ', 03'#']
+// [10' ', 11' ', 12'#', 13'#']
+// [20' ', 21'#', 22'#', 23'#']
+// [30'#', 31'#', 32'#', 33'#']
+// [y, x] = [i, j]
 
-const memTrasformed = memoizeTransform(translate2d(2, 2));
-console.log(memTrasformed(2, 2), "first");
-console.log(memTrasformed(2, 2), "second");
+// Flipped(x axis)
+// [30'#', 31'#', 32'#', 33'#']
+// [20' ', 21'#', 22'#', 23'#']
+// [10' ', 11' ', 12'#', 13'#']
+// [00' ', 01' ', 02' ', 03'#']
