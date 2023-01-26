1d4b9b9a0d60e06b0d28b151ed320b66b369f8d6
Raul <raulvillajr95@gmail.com>
2023-01-26 15:33:08 -0800

    Pink box css/js animation

--- main.js
+++ main.js
@@ -2,33 +2,11 @@
  *
  *
  *
+ *
+ *
+ *
  */
-
-function staircase(num) {
-  for (let i = 0; i < num; i++) {
-    let hashLine = [];
-    for (let j = 0; j < num; j++) {
-      if (i >= -j + (num - 1)) {
-        hashLine.push("#");
-      } else {
-        hashLine.push(" ");
-      }
-      // console.log(j + 3, i);
-    }
-    console.log(hashLine.join(""));
-  }
-}
-
-staircase(15);
-
-// [00' ', 01' ', 02' ', 03'#']
-// [10' ', 11' ', 12'#', 13'#']
-// [20' ', 21'#', 22'#', 23'#']
-// [30'#', 31'#', 32'#', 33'#']
-// [y, x] = [i, j]
-
-// Flipped(x axis)
-// [30'#', 31'#', 32'#', 33'#']
-// [20' ', 21'#', 22'#', 23'#']
-// [10' ', 11' ', 12'#', 13'#']
-// [00' ', 01' ', 02' ', 03'#']
+const trigger = document.querySelector(".trigger");
+trigger.addEventListener("click", () => {
+  trigger.classList.toggle("clicked");
+});
