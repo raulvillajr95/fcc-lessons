5289caa2986bdc41de9161c7252cc1a433d24a16
Raul <raulvillajr95@gmail.com>
2023-01-26 21:41:14 -0800

    Create hamburger mobile menu

--- main.js
+++ main.js
@@ -18,3 +18,15 @@ function toggleOnHover(list, effectedElement, classSelector) {
   });
 }
 toggleOnHover(".item2", ".category-drop", "visible-cate-drop");
+
+function toggleOnClick(list, effectedElement, classSelector) {
+  let element = document.querySelector(list);
+  let effected = document.querySelector(effectedElement);
+  element.addEventListener("click", () => {
+    effected.classList.toggle(classSelector);
+  });
+}
+// Testing...hover name to bring out menu
+toggleOnClick(".title", ".nav2", "nav2-sidebar");
+toggleOnClick(".ham-nav1", ".nav2", "nav2-sidebar");
+toggleOnClick(".ham-nav2", ".nav2", "nav2-sidebar");
