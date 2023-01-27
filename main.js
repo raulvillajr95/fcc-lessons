7a3dcee9df7d687ae0c72dfd5d25908578d10dbe
Raul <raulvillajr95@gmail.com>
2023-01-26 19:18:53 -0800

    Create dropdown

--- main.js
+++ main.js
@@ -6,7 +6,15 @@
  *
  *
  */
-const trigger = document.querySelector(".trigger");
-trigger.addEventListener("click", () => {
-  trigger.classList.toggle("clicked");
-});
+
+function toggleOnHover(list, effectedElement, classSelector) {
+  let element = document.querySelector(list);
+  let effected = document.querySelector(effectedElement);
+  element.addEventListener("mouseover", () => {
+    effected.classList.toggle(classSelector);
+  });
+  element.addEventListener("mouseout", () => {
+    effected.classList.toggle(classSelector);
+  });
+}
+toggleOnHover(".item2", ".category-drop", "visible-cate-drop");
