fcbbb21473b572f4d67c56966059852cf6eba9a7
Raul <raulvillajr95@gmail.com>
2023-02-04 18:48:42 -0800

    Answer things

--- loops.js
+++ loops.js
@@ -6,20 +6,3 @@
  *
  *
  */
-
-function XO(str) {
-  let xCounter = 0;
-  let oCounter = 0;
-
-  for (let i = 0; i < str.length; i++) {
-    if (str[i].toLowerCase() === "x") {
-      xCounter += 1;
-    } else if (str[i].toLowerCase() === "o") {
-      oCounter += 1;
-    }
-  }
-
-  return xCounter === oCounter;
-}
-
-console.log(XO("ooxx"));
