a7fb688543d7e6cb52a6983efa568bce8237ef10
Raul <raulvillajr95@gmail.com>
2023-02-09 19:18:29 -0800

    Promises example from MDN

--- prac.py
+++ prac.py
@@ -1,51 +1,12 @@
-from typing import NamedTuple
+# 
+# 
+# 
+# 
+
 import re
 
-class Token(NamedTuple):
-    type: str
-    value: str
-    line: int
-    column: int
-
-def tokenize(code):
-    keywords = {'IF', 'THEN', 'ENDIF', 'FOR', 'NEXT', 'GOSUB', 'RETURN'}
-    token_specification = [
-        ('NUMBER', r'\d+(\.\d*)?'),
-        ('ASSIGN', r':='),
-        ('END', r';'),
-        ('ID', r'[A-Za-z]+'),
-        ('OP', r'[+\-*/]'),
-        ('NEWLINE', r'\n'),
-        ('SKIP', r'[ \t]+'),
-        ('MISMATCH', r'.'),
-    ]
-    tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
-    line_num = 1
-    line_start = 0
-    for mo in re.finditer(tok_regex, code):
-        kind = mo.lastgroup
-        value = mo.group()
-        column = mo.start() - line_start
-        if kind == 'NUMBER':
-            value = float(value) if '.' in value else int(value)
-        elif kind == 'ID' and value in keywords:
-            kind = value
-        elif kind == 'NEWLINE':
-            line_start = mo.end()
-            line_num += 1
-            continue
-        elif kind == 'SKIP':
-            continue
-        elif kind == 'MISMATCH':
-            raise RuntimeError(f'{value!r} unexpected on line {line_num}')
-        yield Token(kind, value, line_num, column)
-
-statements = '''
-    IF quantity THEN
-        total := total + price * quantity;
-        tax := price * 0.05;
-    ENDIF;
-'''
-
-for token in tokenize(statements):
-    print(token)
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('^From .* ([0-9][0-9]):', line)
+    if len(x) > 0: print(x)
