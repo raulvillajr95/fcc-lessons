8cb19266ced24558dffaae1a9de4c29316a5769f
Raul <raulvillajr95@gmail.com>
2023-02-17 21:01:08 -0800

    Solve fibonacci sequence recursively(long version)

--- main.js
+++ main.js
@@ -8,16 +8,16 @@
  *
  */
 
-function pow(x, n) {
-  let result = 1;
+// function pow(x, n) {
+//   let result = 1;
 
-  for (let i = 0; i < n; i++) {
-    result *= x;
-  }
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
 
-  return result;
-}
-console.log(pow(2, 3));
+//   return result;
+// }
+// console.log(pow(2, 3));
 
 // function fib(n) {
 //   // let arr = [1, 1];
@@ -29,3 +29,16 @@ console.log(pow(2, 3));
 //   }
 // }
 // console.log(fib(5));
+// 0 = 0 + 0
+//
+
+// using 2 parameters
+function fib(n) {
+  if (n <= 2) {
+    return 1;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
+}
+
+console.log(fib(77));
