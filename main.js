ffd11e068e9fc0809a12a832966ab7c4a919e4cb
Raul <raulvillajr95@gmail.com>
2023-02-18 19:02:01 -0800

    Fibonacci sequence recursive function, fast version

--- main.js
+++ main.js
@@ -8,37 +8,46 @@
  *
  */
 
-// function pow(x, n) {
-//   let result = 1;
-
-//   for (let i = 0; i < n; i++) {
-//     result *= x;
-//   }
-
-//   return result;
-// }
-// console.log(pow(2, 3));
-
+// Recursive(slow)
 // function fib(n) {
-//   // let arr = [1, 1];
-//   if (n <= 0) {
-//     return 0;
+//   if (n <= 2) {
+//     return 1;
 //   } else {
-//     let x = [fib(n - 1), fib(n - 1) + 1];
-//     return x;
+//     return fib(n - 2) + fib(n - 1);
 //   }
 // }
-// console.log(fib(5));
-// 0 = 0 + 0
-//
 
-// using 2 parameters
+// Non recursive(fast)
 function fib(n) {
-  if (n <= 2) {
-    return 1;
+  let arr = [0, 1];
+  for (let i = 0; i < n; i++) {
+    arr.push(arr[arr.length - 2] + arr[arr.length - 1]);
+  }
+  // return arr[n];
+  return arr;
+}
+// console.log(fib(50));
+
+function fib2(n, total = [0, 1]) {
+  if (n <= 0) {
+    return total[total.length - 2];
   } else {
-    return fib(n - 1) + fib(n - 2);
+    return fib2(
+      n - 1,
+      total.concat(total[total.length - 2] + total[total.length - 1])
+    );
   }
 }
+console.log(fib2(77));
 
-console.log(fib(77));
+function randomUntilFive(result = 0, count = 0) {
+  if (result === 5) {
+    console.log(`The random result: ${result}`);
+    console.log(`How many times random is executed: ${count}`);
+    return;
+  }
+  result = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+  count++;
+  randomUntilFive(result, count);
+}
+// randomUntilFive();
