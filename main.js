9f78180a15657faac7d65c4233ebd91641d3fb48
Raul <raulvillajr95@gmail.com>
2023-02-21 17:01:12 -0800

    Product of an array

--- main.js
+++ main.js
@@ -8,33 +8,12 @@
  *
  */
 
-// let arr = [];
-// for (let i = 0; i < 32; i++) {
-//   arr.unshift(num % 2);
-//   num = Math.floor(num / 2);
-// }
-// let counter = 0;
-// for (let j = 0; j < arr.length; j++) {
-//   counter += arr[j];
-// }
-// return counter;
-
-function countBits(num) {
-  let arr = [];
-  while (Math.floor(num / 2) != 0) {
-    arr.unshift(num % 2);
-    num = Math.floor(num / 2);
+function productOfArray(arr) {
+  if (arr.length === 1) {
+    return arr[0];
+  } else {
+    return arr.pop() * productOfArray(arr);
   }
-  arr.unshift(num % 2);
-
-  let counter = 0;
-  for (let i = 0; i < arr.length; i++) {
-    counter += arr[i];
-  }
-
-  return counter;
 }
 
-console.log(countBits(13));
-
-// 26
+console.log(productOfArray([1, 2, 3, 10]));
