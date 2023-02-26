54022b734bd9c1a9f9b5b7fb80853b9a15ba04db
Raul <raulvillajr95@gmail.com>
2023-02-25 18:27:37 -0800

    Find sum of even valued terms

--- main.js
+++ main.js
@@ -43,6 +43,25 @@ function fibonacciSummed1(n) {
 // console.log(fibonacciSummed1(10));
 
 // Fibonacci even(for)
+function fibonacciEven1(n) {
+  let arr = [1, 2];
+  let message = 2;
+  if (n <= 1) {
+    message = 0;
+  } else if (n <= 2) {
+    message = 2;
+  } else {
+    for (let i = 0; i < n - 2; i++) {
+      arr.push(arr[arr.length - 2] + arr[arr.length - 1]);
+      if (arr[arr.length - 1] % 2 === 0) {
+        message += arr[arr.length - 1];
+      }
+      // console.log(arr);
+    }
+  }
+  return message;
+}
+// console.log(fibonacciEven1(1));
 
 // Fibonacci sequence(while)
 function fibonacci2(n) {
@@ -83,6 +102,26 @@ function fibonacciSummed2(n) {
 // console.log(fibonacciSummed2(10));
 
 // Fibonacci even(while)
+function fibonacciEven2(n) {
+  let arr = [1, 2];
+  let message = 2;
+  if (n <= 1) {
+    message = 0;
+  } else if (n <= 2) {
+    message = 2;
+  } else {
+    let i = 0;
+    while (i < n - 2) {
+      arr.push(arr[arr.length - 2] + arr[arr.length - 1]);
+      if (arr[arr.length - 1] % 2 === 0) {
+        message += arr[arr.length - 1];
+      }
+      i++;
+    }
+  }
+  return message;
+}
+// console.log(fibonacciEven2(40));
 
 // Fibonacci sequence(recursive)
 function fibonacci3(n, arr = [1, 2]) {
@@ -113,6 +152,26 @@ function fibonacciSummed3(n, arr = [1, 2]) {
   // console.log(arr);
   return message;
 }
-console.log(fibonacciSummed3(10));
+// console.log(fibonacciSummed3(10));
 
 // Fibonacci even(recursive)
+// only loopig once
+function fibonacciEven3(n, arr = [1, 2]) {
+  let message = 0;
+  if (n <= 1) {
+    message = 0;
+  } else if (n <= 2) {
+    message = 2;
+  } else {
+    debugger;
+    arr.push(arr[arr.length - 2] + arr[arr.length - 1]);
+    if (arr[arr.length - 1] % 2 === 0) {
+      message = arr[arr.length - 1];
+      console.log('even');
+    }
+    message += fibonacciEven3(n - 1, arr);
+  }
+  console.log(arr, 'in');
+  return message;
+}
+console.log(fibonacciEven3(10), 'end');
