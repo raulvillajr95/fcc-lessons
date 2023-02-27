1a187ae5772a247782b153e0a9b4c1713478aa74
Raul <raulvillajr95@gmail.com>
2023-02-27 13:58:33 -0800

    Find smallest integer in array

--- loops.js
+++ loops.js
@@ -5,13 +5,41 @@
  *
  *
  *
- *  codewars/exercism/hackerrank challenges
- *  switch back and forth
+ *  every 10 challenges, checkout 'Largest prime factor'
  *  challenges myself to do all them in recursively
  */
 
 // if looping, use recursion
-function noSpace(str) {
-  return str.replaceAll(/\s/gi, '');
+class SmallestIntegerFinder {
+  findSmallestInt(arr, counter = arr.length, smallest = Infinity) {
+    // console.log(arr, counter, 'stats');
+    if (counter <= 0) {
+      if (arr[counter] < smallest) {
+        smallest = arr[counter];
+      }
+      return smallest;
+    } else {
+      if (arr[counter] < smallest) {
+        smallest = arr[counter];
+      }
+      return this.findSmallestInt(arr, counter - 1, smallest);
+    }
+  }
 }
-console.log(noSpace('Astro Boy'));
+let small = new SmallestIntegerFinder();
+console.log(small.findSmallestInt([78, 56, 232, 12, 18]));
+
+// function findSmallestInt(arr, counter = arr.length, smallest = Infinity) {
+//   if (counter <= 0) {
+//     if (arr[counter] < smallest) {
+//       smallest = arr[counter];
+//     }
+//     return smallest;
+//   } else {
+//     if (arr[counter] < smallest) {
+//       smallest = arr[counter];
+//     }
+//     return findSmallestInt(arr, counter - 1, smallest);
+//   }
+// }
+// console.log(findSmallestInt([78, 56, 232, 12, 18]), 'end');
--- main.js
+++ main.js
@@ -12,6 +12,9 @@
  * Project Euler #3
  * Largest prime factor
  *  get prime factors of a number
+ *  is number divisible by 2, 3, 5, 7, 11, 13, 17, 19(prime numbers up to that number)
+ *    if so, divide
+ *    now is that number divisible
  *  get largest single number of all prime factors
  *
  * * jot down the process
@@ -21,22 +24,29 @@
  *
  */
 
-function primes(n) {
-  let listy = [];
-  for (let i = 2; i <= n; i++) {
-    let list = [];
-    for (let j = 2; j <= i; j++) {
-      if (i % j == 0) {
-        list.push(j);
-      } else {
-        continue;
-      }
+// is a number a prime
+// list of prime factors
+// biggest in list of prime factors
+
+async function prime(n, number = [], primes = []) {
+  number[0] = n;
+  number = Number(number);
+  if (n < 2) {
+    // console.log(primes);
+    return primes.length === 0;
+  } else {
+    await Promise.resolve(false);
+
+    if (number % n === 0 && number != n) {
+      // console.log(n);
+      primes.push(n);
     }
-    if (list.length == 1) {
-      listy.push(i);
-    }
-    list = [];
+    // console.log(n, number);
+    return await prime(n - 1, number, primes);
   }
-  return listy;
 }
-// console.log(primes(3));
+prime(2000001)
+  .then((sum) => console.log(sum))
+  .catch((err) => console.log(err.stack));
+
+// comment
