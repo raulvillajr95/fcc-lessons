09f7080a1dd2f112930a9779723731f2dfb77e8f
Raul <raulvillajr95@gmail.com>
2023-03-06 21:02:11 -0800

    Insert value without if/else, khan academy now complaining about 'array undefined' values

--- loops.js
+++ loops.js
@@ -8,3 +8,23 @@
  */
 
 // if looping, do recursion
+function twoSort(arr) {
+  const firstWord = arr.sort()[0];
+  return firstWord.split('').join('***');
+}
+console.log(
+  twoSort([
+    'turns',
+    'out',
+    'random',
+    'test',
+    'cases',
+    'are',
+    'easier',
+    'than',
+    'writing',
+    'out',
+    'basic',
+    'ones',
+  ])
+);
--- main.js
+++ main.js
@@ -7,21 +7,38 @@
  *
  *
  */
-let insert = function (arr, rightIn, val) {
-  for (let i = rightIn; i >= 0; i--) {
-    arr[i + 1] = arr[i];
-    if (val >= arr[i + 1]) {
-      arr[i + 1] = val;
-      break;
-    } else if (i === 0) {
-      arr[i] = val;
-    }
+var insert = function (array, rightIndex, value) {
+  // first hard coded all the way through
+  // then for loop(no if statements)
+  // array[6] = array[5];
+  // console.log(array, 'array');
+  // array[5] = array[4];
+  // console.log(array, 'array');
+  // array[4] = array[3];
+  // console.log(array, 'array');
+  // array[3] = array[2];
+  // console.log(array, 'array');
+  // array[2] = array[1];
+  // console.log(array, 'array');
+  // array[1] = array[0];
+  // console.log(array, 'array');
+  var i;
+  for (i = rightIndex; value < array[i]; i--) {
+    array[i + 1] = array[i];
+    console.log(array, 'array');
   }
+  array[i + 1] = value;
 };
 
+// combine if & else if
+// remove if from for loop
+
 var ex = [3, 5, 7, 11, 13, 2, 9, 6];
+// var ex = [2, 3, 7, 8, 10, 13, 5];
 
 insert(ex, 4, 2);
 // insert(ex, 5, 9);
 // insert(ex, 6, 6);
+
+// insert(ex, 5, 5);
 console.log(ex);
