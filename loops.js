c75f45741d15c9887a952aec4e9a4c8e9e61142d
Raul <raulvillajr95@gmail.com>
2023-03-06 16:32:47 -0800

    Insert value before an index 👍

--- loops.js
+++ loops.js
@@ -5,22 +5,6 @@
  *
  *
  *
- *  every 10 challenges, checkout 'Largest prime factor'
- *  challenges myself to do all them in recursively
  */
 
-// if looping, use recursion
-// function finalGrade(exam, projects) {
-//   let grade;
-//   if (exam > 90 || projects > 10) {
-//     grade = 100;
-//   } else if (exam > 75 && projects >= 5) {
-//     grade = 90;
-//   } else if (exam > 50 && projects >= 2) {
-//     grade = 75;
-//   } else {
-//     grade = 0;
-//   }
-//   return grade;
-// }
-// console.log(finalGrade(85, 5));
+// if looping, do recursion
--- main.js
+++ main.js
@@ -7,63 +7,21 @@
  *
  *
  */
-
-const programCode = function (processingInstance) {
-  with (processingInstance) {
-    size(200, 275);
-    frameRate(30);
-
-    // write code here
-    fill(0, 0, 0);
-    var swap = function (array, firstIndex, secondIndex) {
-      var temp = array[firstIndex];
-      array[firstIndex] = array[secondIndex];
-      array[secondIndex] = temp;
-    };
-
-    var indexOfMinimum = function (array, startIndex) {
-      var minValue = array[startIndex];
-      var minIndex = startIndex;
-
-      for (var i = minIndex + 1; i < array.length; i++) {
-        if (array[i] < minValue) {
-          minIndex = i;
-          minValue = array[i];
-        }
-      }
-      return minIndex;
-    };
-
-    var selectionSort = function (array) {
-      var position;
-
-      let lineYStart = 60;
-      let lineXStart = 60;
-      for (var i = 0; i < array.length - 1; i++) {
-        println(array + ' during for');
-
-        // put text on canvas
-        textFont(createFont('monospace'), 14);
-        text(array, 50, (i + 1) * 55);
-
-        // lines
-        if (i > 0) {
-          line(60 + position * 15, 5 + lineYStart, lineXStart, 35 + lineYStart);
-          lineYStart += 55;
-          lineXStart += 12;
-          println(`${i}, ${position + 1}`);
-        }
-
-        position = indexOfMinimum(array, i);
-        swap(array, i, position);
-      }
-    };
-
-    var array = [1, 2, 8, 3, 5];
-    selectionSort(array);
+let insert = function (arr, rightIn, val) {
+  for (let i = rightIn; i >= 0; i--) {
+    arr[i + 1] = arr[i];
+    if (val >= arr[i + 1]) {
+      arr[i + 1] = val;
+      break;
+    } else if (i === 0) {
+      arr[i] = val;
+    }
   }
 };
 
-// process canvas
-const canvas = document.getElementById('mycanvas');
-const processingInstance = new Processing(canvas, programCode);
+var ex = [3, 5, 7, 11, 13, 2, 9, 6];
+
+insert(ex, 4, 2);
+// insert(ex, 5, 9);
+// insert(ex, 6, 6);
+console.log(ex);
