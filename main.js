8d91c8e60e2962fbc7598f44ed54f17976b67801
Raul <raulvillajr95@gmail.com>
2023-03-11 12:41:21 -0800

    Karatsuba multiplication, prog ass. #1

--- main.js
+++ main.js
@@ -8,115 +8,22 @@
  *
  */
 
-/* A Queue object for queue-like functionality over JavaScript arrays. */
-var Queue = function () {
-  this.items = [];
-};
-Queue.prototype.enqueue = function (obj) {
-  this.items.push(obj);
-};
-Queue.prototype.dequeue = function () {
-  return this.items.shift();
-};
-Queue.prototype.isEmpty = function () {
-  return this.items.length === 0;
-};
+// Merge arrays
+function merge(a, b) {
+  let c = [];
+  let i = 0;
+  let j = 0;
 
-/*
- * Performs a breadth-first search on a graph
- * @param {array} graph - Graph, represented as adjacency lists.
- * @param {number} source - The index of the source vertex.
- * @returns {array} Array of objects describing each vertex, like
- *     [{distance: _, predecessor: _ }]
- */
-var doBFS = function (graph, source) {
-  var bfsInfo = [];
-
-  for (var i = 0; i < graph.length; i++) {
-    bfsInfo[i] = {
-      distance: null,
-      predecessor: null,
-    };
-  }
-
-  bfsInfo[source].distance = 0;
-
-  var queue = new Queue();
-  queue.enqueue(source);
-
-  // Traverse the graph
-  // console.log(source, 'source'); // 3
-  // console.log(graph, 'graph'); // array of arrays
-  // console.log(bfsInfo, 'bfsInfo'); // array of objects
-  // visit neighbors
-
-  while (!queue.isEmpty()) {
-    //////
-    var u = queue.dequeue();
-    // var u = source;
-    for (var v = 0; v < graph[u].length; v++) {
-      var neighbor = graph[u][v];
-      if (bfsInfo[neighbor].distance == null) {
-        bfsInfo[neighbor].distance = bfsInfo[u].distance + 1;
-        bfsInfo[neighbor].predecessor = u;
-        queue.enqueue(neighbor);
-      }
+  for (let k = 0; k < a.length + b.length; k++) {
+    if (a[i] < b[j]) {
+      c[k] = a[i];
+      i++;
+    } else if (b[j] < a[i]) {
+      c[k] = b[j];
+      j++;
     }
-    ///////
   }
-  // learn about queue
-  // var you = queue.dequeue();
-  // for (let i = 0; i < 4; i++) {
-  //   console.log(you, 'you');
-  //   console.log(queue, 'que');
-  //   queue.enqueue(i);
-  //   console.log(queue);
-  // }
 
-  // As long as the queue is not empty:
-  //  Repeatedly dequeue a vertex u from the queue.
-  //
-  //  For each neighbor v of u that has not been visited:
-  //     Set distance to 1 greater than u's distance
-  //     Set predecessor to u
-  //     Enqueue v
-  //
-  //  Hint:
-  //  use graph to get the neighbors,
-  //  use bfsInfo for distances and predecessors
-
-  return bfsInfo;
-};
-
-var adjList = [
-  [1],
-  [0, 4, 5],
-  [3, 4, 5],
-  [2, 6],
-  [1, 2],
-  [1, 2, 6],
-  [3, 5],
-  [],
-];
-var bfsInfo = doBFS(adjList, 3);
-for (var i = 0; i < adjList.length; i++) {
-  console.log(
-    'vertex ' +
-      i +
-      ': distance = ' +
-      bfsInfo[i].distance +
-      ', predecessor = ' +
-      bfsInfo[i].predecessor
-  );
+  return c;
 }
-
-/*
-Program.assertEqual(bfsInfo[0], {distance: 4, predecessor: 1});
-Program.assertEqual(bfsInfo[1], {distance: 3, predecessor: 4});
-Program.assertEqual(bfsInfo[2], {distance: 1, predecessor: 3});
-Program.assertEqual(bfsInfo[3], {distance: 0, predecessor: null});
-Program.assertEqual(bfsInfo[4], {distance: 2, predecessor: 2});
-Program.assertEqual(bfsInfo[5], {distance: 2, predecessor: 2});
-Program.assertEqual(bfsInfo[6], {distance: 1, predecessor: 3});
-Program.assertEqual(bfsInfo[7], {distance: null, predecessor: null});
-*/
+console.log(merge([2, 3, 5, 7], [2, 4, 6, 8]));
--- prac.py
+++ prac.py
@@ -18,3 +18,33 @@
 #         break
 #     print(data.decode(),end='')
 # mysock.close()
+
+# Exercise 2
+# import socket
+# url = input('Enter URL: ')
+# mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# # mysock.connect(('data.pr4e.org', 80))
+# try:
+#     mysock.connect((url.split('/')[2], 80))
+# except:
+#     print('Enter a valid url')
+#     quit()
+# # cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+# cmd = b'GET %s HTTP/1.0\r\n\r\n' % (url).encode()
+# mysock.send(cmd)
+# text = ''
+# while True:
+#     data = mysock.recv(512)
+#     if len(data) < 1:
+#         break
+#     text = text + data.decode()
+# print(''.join([*text][0:3000]))
+# print("Total characters:",len(text))
+# mysock.close()
+
+# Exercise 3
+# import urllib.request, urllib.parse
+
+# url = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+# data = urllib.parse('http://data.pr4e.org/romeo.txt')
+# print(data)
