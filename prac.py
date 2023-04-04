7dd275d26cbb0b692fed4a40ffe703a9a39a1818
Raul <raulvillajr95@gmail.com>
2023-04-04 09:23:31 -0700

    Complete exercise 1: Google geocoding

--- prac.py
+++ prac.py
@@ -1,97 +1,47 @@
-# Exercise 1
-# import socket
-# url = input('Enter URL: ')
-# mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# # mysock.connect(('data.pr4e.org', 80))
-# try:
-#     mysock.connect((url.split('/')[2], 80))
-# except:
-#     print('Enter a valid url')
-#     quit()
-# # cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
-# cmd = b'GET %s HTTP/1.0\r\n\r\n' % (url).encode()
-# print(cmd, 'cmd')
-# mysock.send(cmd)
-# while True:
-#     data = mysock.recv(512)
-#     if len(data) < 1:
-#         break
-#     print(data.decode(),end='')
-# mysock.close()
-
-# Exercise 2
-# import socket
-# url = input('Enter URL: ')
-# mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# # mysock.connect(('data.pr4e.org', 80))
-# try:
-#     mysock.connect((url.split('/')[2], 80))
-# except:
-#     print('Enter a valid url')
-#     quit()
-# # cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
-# cmd = b'GET %s HTTP/1.0\r\n\r\n' % (url).encode()
-# mysock.send(cmd)
-# text = ''
-# while True:
-#     data = mysock.recv(512)
-#     if len(data) < 1:
-#         break
-#     text = text + data.decode()
-# print(''.join([*text][0:3000]))
-# print("Total characters:",len(text))
-# mysock.close()
-
-# Exercise 3
+# # Exercise 1
 # import urllib.request, urllib.parse, urllib.error
-
-# inp = input('Enter URL: ')
-# url = urllib.request.urlopen(inp).read()
-# # data = urllib.parse('http://data.pr4e.org/romeo.txt')
-# text = url.decode()
-# print(''.join([*text][0:3000]))
-# print('Total characters:', len(text))
-
-# Exercise 4
-# import urllib.request, urllib.parse, urllib.error
-# from bs4 import BeautifulSoup
+# import json
 # import ssl
+# api_key = False
+# if api_key is False:
+#     api_key = 42
+#     serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+# else :
+#     serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 # ctx = ssl.create_default_context()
 # ctx.check_hostname = False
 # ctx.verify_mode = ssl.CERT_NONE
-# url = input('Enter - ')
-# html = urllib.request.urlopen(url, context=ctx).read()
-# soup = BeautifulSoup(html, 'html.parser')
-# count = 0
-# ptags = soup('p')
-# for tag in ptags:
-#     count = count + 1
-# print(count)
-
-# Exercise 5
-# import socket
-# url = input('Enter URL: ')
-# mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# try:
-#     mysock.connect((url.split('/')[2], 80))
-# except:
-#     print('Enter a valid url')
-#     quit()
-# cmd = b'GET %s HTTP/1.0\r\n\r\n' % (url).encode()
-# mysock.send(cmd)
-# text = ''
 # while True:
-#     data = mysock.recv(512)
-#     if len(data) < 1:
-#         break
-#     text = text + data.decode()
-# mysock.close()
-# beyond_headers = ''.join(text.split('\r\n\r\n')[1:])
-# print(beyond_headers, end='')
+#     address = input('Enter location: ')
+#     if len(address) < 1: break
+#     parms = dict()
+#     parms['address'] = address
+#     if api_key is not False: parms['key'] = api_key
+#     url = serviceurl + urllib.parse.urlencode(parms)
+#     print('Retrieving', url)
+#     uh = urllib.request.urlopen(url, context=ctx)
+#     data = uh.read().decode()
+#     print('Retrieved', len(data), 'characters')
+#     try:
+#         js = json.loads(data)
+#     except:
+#         js = None
+#     if not js or 'status' not in js or js['status'] != 'OK':
+#         print('==== Failure To Retrieve ====')
+#         print(data)
+#         continue
+#     print(json.dumps(js, indent=4))
+#     lat = js['results'][0]['geometry']['location']['lat']
+#     lng = js['results'][0]['geometry']['location']['lng']
+#     print('lat', lat, 'lng', lng)
+#     location = js['results'][0]['formatted_address']
+#     print(location)
+#     try:
+#         country_code = js['results'][0]['address_components'][3]['short_name']
+#         print(country_code)
+#     except:
+#         print('Not a country')        
 
-
-import re
-
-def no_space(x):
-    return re.sub('\s', '', x)
-print(no_space("8 j 8   mBliB8g  imjB8B8  jl  B"))
+# def greeting(name):
+#     return "Hello, " + name + " how are you doing today?"
+# print(greeting('Raul'))
