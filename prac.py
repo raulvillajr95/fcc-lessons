b0c2938d3372dc6714aab45c16470691cdfb116b
Raul <raulvillajr95@gmail.com>
2023-04-10 11:45:45 -0700

    Retrieve json from url

--- prac.py
+++ prac.py
@@ -85,3 +85,34 @@
 #     except:
 #         print('Not a country')
 
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+    
+serviceurl = 'http://py4e-data.dr-chuck.net/comments_42.json'
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = serviceurl
+print('Retrieving', url)
+uh = urllib.request.urlopen(url, context=ctx)
+data = uh.read().decode()
+print('Retrieved', len(data), 'characters')
+
+try:
+    js = json.loads(data)
+except:
+    js = None
+    
+if not js or 'status' not in js or js['status'] != 'OK':
+    print('==== Failure To Retrieve ====')
+    print(data)
+
+print(json.dumps(js, indent=4))
+
+count = 0
+for comment in js['comments']:
+    count += int(comment['count'])
+print(count)
