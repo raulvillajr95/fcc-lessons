130d1a0e1278a453a3767540d47d128e9377ddd8
Raul <raulvillajr95@gmail.com>
2023-05-08 21:03:17 -0700

    E1.9, create a list of names and birthdays

--- main.cpp
+++ main.cpp
@@ -5,20 +5,10 @@ using namespace std;
 int main() 
 {
 
-  cout << "Hope is the thing with feathers" << endl;
-  cout << "That perches in the soul," << endl;
-  cout << "And sings the tune without the words," << endl;
-  cout << "And never stops at all," << endl;
-  cout << "" << endl;
-  cout << "And sweetest in the gale is heard;" << endl;
-  cout << "And sore must be the storm" << endl;
-  cout << "That could abash the little bird" << endl;
-  cout << "That kept so many warm." << endl;
-  cout << "" << endl;
-  cout << "I've heard it in the chillest land," << endl;
-  cout << "And on the strangest sea;" << endl;
-  cout << "Yet, never, in extremity," << endl;
-  cout << "It asked a crumb of me." << endl;
+  cout << "Emily Dickinson 05/08/1995" << endl;
+  cout << "Raul Villalobos 06/10/2000" << endl;
+  cout << "Barry Allen     07/22/1999" << endl;
+  cout << "Kai Cenat       08/18/1996" << endl;
 
   return 0;
 }
