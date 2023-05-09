4e2153bcdfb367a9f464162b5579ff1a379b1202
Raul <raulvillajr95@gmail.com>
2023-05-08 17:14:09 -0700

    Make sure array is only increasing or constant

--- loops.js
+++ loops.js
@@ -7,17 +7,25 @@
  *
  */
 
-function add(num) {
-  return num + 1
-}
-function mult(num) {
-  return num * 30
+function range(a, b) {
+  let r = []
+  let step = b - a > 0 ? 1 : -1
+  do{ 
+    r.push(a)
+  } while (step * (b - (a += step)) >= 0)
+  return r
 }
 
-function chain(num, arr) {
-  for (let i = 0;i < arr.length;i++) {
-    num = arr[i](num)
+function isMonotone(arr) {
+  if (arr.length <= 1) return true
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i+1]) return false
   }
-  return num
+  return true
 }
-console.log(chain(2, [add, mult]))
+console.log(isMonotone(range(1,10)))
+console.log(isMonotone(range(4,12)))
+console.log(isMonotone([5,5,5,5,5]))
+console.log(isMonotone([]))
+console.log(isMonotone(range(5,1)))
+console.log(isMonotone([1,2,3,3,4,5]))
