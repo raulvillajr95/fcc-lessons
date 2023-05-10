9ed6ce6e4d36b4fc34a374d5981f3a023d7b69cd
Raul <raulvillajr95@gmail.com>
2023-05-10 11:57:57 -0700

    P1.4 draw face

--- loops.js
+++ loops.js
@@ -7,7 +7,28 @@
  *
  */
 
-function closestMultiple10(num) {
-  return Math.round(num / 10) * 10
+function splitTheBill(group) {
+  let total = 0;
+  let amntPeople = Object.keys(group).length;
+  for (const key of Object.keys(group)) {
+    total += group[key];
+  }
+  console.log(total)
+
+  let avg = total / amntPeople;
+  console.log(avg)
+
+  for (const key of Object.keys(group)) {
+    group[key] -= Number(avg.toFixed(2));
+  }
+
+  for (const key of Object.keys(group)) {
+    group[key] = Number(group[key].toFixed(2))
+  }
+  return group;
 }
-console.log(closestMultiple10(37))
+console.log(splitTheBill({
+    A: 20, 
+    B: 15, 
+    C: 10
+}))
--- main.cpp
+++ main.cpp
@@ -2,17 +2,15 @@
 
 using namespace std;
 
-#include <string>
 int main() 
 {
 
-  cout << "Hello, my name is Hal!" << endl;
-  cout << "What would you like me to do?" << endl;
-  
-  string user_input;
-  getline(cin, user_input);
-
-  cout << "I am sorry, I cannot do that." << endl;
+  cout << "  //////" << endl;
+  cout << " +-----+" << endl;
+  cout << "(| O x |)" << endl;
+  cout << " |  <  |" << endl;
+  cout << " | --u |" << endl;
+  cout << " +-----+" << endl;
 
   return 0;
 }
