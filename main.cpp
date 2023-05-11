bbd0245a8ce7b40353c6e3be3356f0eadeb5972f
Raul <raulvillajr95@gmail.com>
2023-05-10 17:26:17 -0700

    Chart for gas types of different planets

--- main.cpp
+++ main.cpp
@@ -5,18 +5,15 @@ using namespace std;
 int main() 
 {
 
-  cout << " * * * * * * ==========================" << endl;
-  cout << "  * * * * *  ==========================" << endl;
-  cout << " * * * * * * ==========================" << endl;
-  cout << "  * * * * *  ==========================" << endl;
-  cout << " * * * * * * ==========================" << endl;
-  cout << "  * * * * *  ==========================" << endl;
-  cout << "=======================================" << endl;
-  cout << "=======================================" << endl;
-  cout << "=======================================" << endl;
-  cout << "=======================================" << endl;
-  cout << "=======================================" << endl;
-  cout << "=======================================" << endl;
+  cout << " Planets " << endl;
+  cout << "Type        Name    Primary Gas    Secondary Gas" << endl;
+  cout << "gas giant   Jupiter Hydrogen       Helium" << endl;
+  cout << "gas giant   Saturn  Hydrogen       Helium" << endl;
+  cout << "gas giant   Uranus  Hydrogen       Helium" << endl;
+  cout << "gas giant   Neptune Hydrogen       Helium" << endl;
+  cout << "terrestrial Venus   Carbon Dioxide Nitrogen" << endl;
+  cout << "terrestrial Mars    Carbon Dioxide Nitrogen" << endl;
+  cout << "terrestrial Earth   Nitrogen       Oxygen" << endl;
 
   return 0;
 }
