35f358831b1f84030867f69e18e75652b483a23f
Raul <raulvillajr95@gmail.com>
2023-05-14 11:51:19 -0700

    c++ step 6: provide input and output

--- main.cpp
+++ main.cpp
@@ -5,3 +5,14 @@
   change_due = change_due % PENNIES_PER_DOLLAR;
   quarters = change_due / PENNIES_PER_QUARTER;
 */
+
+/*
+  Step 6: provide input and output
+  cout << "Enter bill value (1 = $1 bill, 5 = $5 bill, etc.): ";
+  cin >> bill_value;
+  cout << "Enter item price in pennies: ";
+  cin >> item_price;
+
+  cout << "Dollar coins: " << setw(6) << dollar_coins << endl
+       << "Quarters:     " << setw(6) << quarters << endl;
+*/
