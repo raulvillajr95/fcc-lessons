f0ae9c325a03585763d8eea21e10c0063aae9da4
Raul <raulvillajr95@gmail.com>
2023-05-18 19:13:11 -0700

    Modified linkedList

--- loops.js
+++ loops.js
@@ -7,17 +7,22 @@
  *
  */
 
-function steps(num) {
-  let count = 1
-  
-  while(num != 1){
-    if (num % 2 === 0) {
-      num = num / 2
-    } else if (num % 2 === 1) {
-      num = (num * 3) + 1
-    }
-    count++
+function convert(num) {
+  if (num % 3 != 0 && num % 5 != 0 && num % 7 != 0) {
+    return num.toString()
   }
-  return count
+  let message = ''
+  if (num % 3 === 0) {
+    message += 'Pling'
+  }
+  if (num % 5 === 0) {
+    message += 'Plang'
+  }
+  if (num % 7 === 0) {
+    message += 'Plong'
+  }
+  return message
 }
-console.log(collatz(12))
+console.log(convert(1))
+console.log(convert(5))
+console.log(convert(15))
