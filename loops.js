15b20b048c55bc46355661d62c309f9719fc3d15
Raul <raulvillajr95@gmail.com>
2023-05-23 12:54:49 -0700

    Allow input of quarters to 'How to 2.1'

--- loops.js
+++ loops.js
@@ -7,22 +7,4 @@
  *
  */
 
-class HighScores {
-  constructor(list) {
-    this.list = list
-  }
-
-  get scores() {
-    return this.list
-  }
-
-  get latest() {
-  }
-
-  get personalBest() {
-  }
-
-  get personalTopThree() {
-  }
-}
 
