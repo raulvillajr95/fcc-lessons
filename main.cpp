945de6d1d101127c2eea3d4fde36394395f0623f
Raul <raulvillajr95@gmail.com>
2023-05-24 14:01:45 -0700

    E2.14 print a grid pattern using variables

--- main.cpp
+++ main.cpp
@@ -5,15 +5,13 @@ using namespace std;
 
 int main()
 {
-  cout << "Please enter an integer between 1,000 and 999,999: ";
-  string number;
-  cin >> number;
+  string comb = "+--+--+--+\n|  |  |  |";
+  string bottom = "+--+--+--+";
 
-  int str_length = number.length();
-  string first = number.substr(0, str_length - 4);
-  string last = number.substr(str_length - 3, 3);
-
-  cout << first << last << endl;
+  cout << comb << endl;
+  cout << comb << endl;
+  cout << comb << endl;
+  cout << bottom << endl;
 
   return 0;
 }
