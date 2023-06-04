3e48789e50a01a046bbfe99569be5df2e031a14c
Raul <raulvillajr95@gmail.com>
2023-06-04 09:32:33 -0700

    complete P2.1 calculate easter sunday of given year

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-int main()
-{
-  cout << "Enter month number: ";
-  int num;
-  cin >> num;
+int main() {
+  cout << "enter year: ";
+  int y;
+  cin >> y;
 
-  // longest month: 9
-  string months = "January   February  March     April     May       June      July      August    September October   November  December ";
+  int a = y % 19;
+  int b = y / 100;
+  int c = y % 100;
+  int d = b / 4;
+  int e = b % 4;
+  int g = (8 * b + 13) / 25;
+  int h = (19 * a + b - d - g + 15) % 30;
+  int j = c / 4;
+  int k = c % 4;
+  int m = (a + 11 * h) / 319;
+  int r = (2 * e + 2 * j - k - h + m + 32) % 7;
+  int n = (h - m + r + 90) / 25;
+  int p = (h - m + r + n + 19) % 32;
 
-  int start = 10 * num - 10;
-  string month = months.substr(start, 10);
-
-  cout << month << endl;
+  cout << "Month: " << n << endl;
+  cout << "Day: " << p << endl;
 
   return 0;
 }
