8af1fce1a176b5472411b4b0aea15136bed947ba
Raul <raulvillajr95@gmail.com>
2023-06-14 23:37:03 -0700

    Complete P2.3, min() with algorithm header

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
+
+int main() {
+  cout << "Enter the number of movie rentals: ";
+  int movies;
+  cin >> movies;
+  cout << "Enter the number of members referred to the video club: ";
+  int members_ref;
+  cin >> members_ref;
+
+  cout << "The discount is equal to: " << min(movies + members_ref, 75) << ".00 percent." << endl;
+
+  return 0;
+}
