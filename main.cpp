a9a40bc8b2b761bc9da5e67696c2add5fa3fd9ec
Raul <raulvillajr95@gmail.com>
2023-06-30 23:33:35 -0700

    Complete P2.4, calculate total cost of owning a car for 5 years

--- main.cpp
+++ main.cpp
@@ -16,5 +16,12 @@ int main() {
   int resale_value;
   cin >> resale_value;
 
+  float gas_price_per_mile = gas_price / 30.0;
+
+  float total_gas_cost = gas_price_per_mile * miles_per_year;
+  float total_car_cost = cost_new_car - resale_value;
+  float total_cost = total_gas_cost + total_car_cost;
+  cout << "Total cost: " << total_cost << endl;
+
   return 0;
 }
--- prac.py
+++ prac.py
@@ -6,4 +6,4 @@ def find_eulerian_tour(graph):
 
     return answer
 
-print(find_eulerian_tour([(0, 1), (1, 5), (1, 7), (4, 5), (4, 8), (1, 6), (3, 7), (5, 9), (2, 4), (0, 4), (2, 5), (3, 6), (8, 9)]))
+print(find_eulerian_tour([(1, 2), (2, 3), (3, 1)]))
