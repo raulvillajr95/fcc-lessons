25e39ce59fa163a05efc4ad60a25b715a102b290
Raul <raulvillajr95@gmail.com>
2023-07-01 18:53:04 -0700

    Complete P2.5, compute bookstore order total

--- main.cpp
+++ main.cpp
@@ -3,25 +3,18 @@
 using namespace std;
 
 int main() {
-  cout << "Cost of new car: ";
-  int cost_new_car;
-  cin >> cost_new_car;
-  cout << "Estimated miles driven per year: ";
-  int miles_per_year;
-  cin >> miles_per_year;
-  cout << "Gas price: ";
-  float gas_price;
-  cin >> gas_price;
-  cout << "Resale value after 5 years: ";
-  int resale_value;
-  cin >> resale_value;
+  cout << "Total book price: ";
+  float total_book_price;
+  cin >> total_book_price;
+  cout << "Number of books: ";
+  int number_of_books;
+  cin >> number_of_books;
 
-  float gas_price_per_mile = gas_price / 30.0;
+  float tax = 0.075 * total_book_price;
+  int shipping_charge = 2 * number_of_books;
 
-  float total_gas_cost = gas_price_per_mile * miles_per_year;
-  float total_car_cost = cost_new_car - resale_value;
-  float total_cost = total_gas_cost + total_car_cost;
-  cout << "Total cost: " << total_cost << endl;
+  float price_of_order = total_book_price + tax + shipping_charge;
+  cout << "Total: " << price_of_order << endl;
 
   return 0;
 }
