d335878bda1c859f8cfdd5ef60fc261c0d2c0d2d
Raul <raulvillajr95@gmail.com>
2023-07-07 14:14:23 -0700

    complete P2.7, input float price amount, out put dollars and cents separately

--- prac.py
+++ prac.py
@@ -1,12 +1,5 @@
-dice = {"a": 1}
-print(dice["b"])
+import re
 
-def odd(num):
-    dic = {}
-    for i in range(len(num)):
-        if dic[num[i]]:
-            dic[num[i]] += 1
-        else:
-            dic[num[i]] = 1
-    return dic
-# print(odd([1,2,2,3,3,3,4,3,3,3,2,2,1]))
+def vowels(srr):
+    return re.match("/[aeiou]/g", srr)
+print(vowels("aeiou"))
