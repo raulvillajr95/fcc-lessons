4fc1f40ba26f5d6e97c4c06d0665d76c69624237
Raul <raulvillajr95@gmail.com>
2023-07-09 12:33:10 -0700

    Create the basic variables needed for the 'Returning change' problem

--- main.cpp
+++ main.cpp
@@ -1,17 +1,27 @@
-#include <iostream>
+ #include <iostream>
 
-using namespace std;
+ using namespace std;
 
-int main() {
-  cout << "Enter price: ";
-  float price;
-  cin >> price;
+ int main() {
+  cout << "Amount due: ";
+  float amount_due;
+  cin >> amount_due;
+  cout << "Amount received: ";
+  float amount_received;
+  cin >> amount_received;
 
-  int dollars = price;
-  int cents = ((price - dollars) * 100) + 0.5;
+  float change = amount_received - amount_due;
 
-  cout << "Dollars: " << dollars << endl;
-  cout << "Cents: " << cents << endl;
-
+  cout << "Dollars: " << change << endl;
+  cout << "Quarters: " << change << endl;
+  cout << "Dimes: " << change << endl;
+  cout << "Nickels: " << change << endl;
+  cout << "Pennies: " << change << endl;
+  
   return 0;
 }
+
+/*
+go digit by digit
+  except dollars, were doing all in 1s
+*/
