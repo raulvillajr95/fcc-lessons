37d0c269138c0c47299c70de60809e5e687200d2
Raul <raulvillajr95@gmail.com>
2023-07-29 11:15:10 -0700

    The beggings of P2.9

--- main.cpp
+++ main.cpp
@@ -1,34 +1,11 @@
- #include <iostream>
- #include <math.h>
+#include <iostream>
 
- using namespace std;
+using namespace std;
 
- int main() {
-  cout << "Amount due: ";
-  float amount_due;
-  cin >> amount_due;
-  cout << "Amount received: ";
-  float amount_received;
-  cin >> amount_received;
-
-  float change = amount_received - amount_due;
-  
-  int dollars = change;
-  int quarters = (change - dollars) / 0.25;
-  int dimes = (change - dollars - (quarters * 0.25)) / 0.1;
-  int nickels = (change - dollars - (quarters * 0.25) - (dimes * 0.1)) / 0.05;
-  float pennies = round((change - dollars - (quarters * 0.25) - (dimes * 0.1) - (nickels * 0.05)) / 0.01);
-
-  cout << "Dollars: " << dollars << endl;
-  cout << "Quarters: " << quarters << endl;
-  cout << "Dimes: " << dimes << endl;
-  cout << "Nickels: " << nickels << endl;
-  cout << "Pennies: " << pennies << endl;
-  
-  return 0;
+int main () {
+  cout << "R1: ";
+  int
+  cin 
+  cout << "R1: ";
+  cout << "R1: ";
 }
-
-/*
-go digit by digit
-  except dollars, were doing all in 1s
-*/
