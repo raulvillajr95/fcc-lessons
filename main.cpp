5629162c180c5c65e950c4184080a7d438c3c922
Raul <raulvillajr95@gmail.com>
2023-08-10 15:35:41 -0700

    Declare variable in C++

--- main.cpp
+++ main.cpp
@@ -2,10 +2,5 @@
 
 int main()
 {
-
-  std::cout << "Dear Self,\n";
-  std::cout << "\n";
-  std::cout << "My goal this year is to do well in my COMPSCI-1 class.\n";
-  std::cout << "\n";
-  std::cout << "Raul Villalobos 08/10/23\n";
+  int year;
 }
