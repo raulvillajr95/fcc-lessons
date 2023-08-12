1c767153a25e094ebed89684c8c5dade73e9ac83
Raul <raulvillajr95@gmail.com>
2023-08-12 09:53:49 -0700

    calculate weights with 'switch' statement

--- main.cpp
+++ main.cpp
@@ -2,15 +2,40 @@
 
 int main()
 {
-  double ph = 4.6;
+  std::cout << "Enter earth weight: ";
+  double e_weight;
+  std::cin >> e_weight;
 
-  if (ph > 7) {
-    std::cout << "Basic\n";
+  std::cout << "Enter planet: ";
+  int planet;
+  std::cin >> planet;
+
+  double multiplier;
+  
+  switch (planet) {
+    case 1 :
+      multiplier = 0.38;
+      break;
+    case 2 :
+      multiplier = 0.91;
+      break;
+    case 3 :
+      multiplier = 0.38;
+      break;
+    case 4 :
+      multiplier = 2.34;
+      break;
+    case 5 :
+      multiplier = 1.06;
+      break;
+    case 6 :
+      multiplier = 0.92;
+      break;
+    case 7 :
+      multiplier = 1.19;
+      break;
   }
-  else if (ph < 7) {
-    std::cout << "Acidic\n";
-  }
-  else {
-    std::cout << "Neutral\n";
-  }
+
+  std::cout << "Your weight will be: " << e_weight * multiplier << "\n";
+
 }
