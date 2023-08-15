a961dc7cf1bab4cda4fe2b6eeec63cf6cfb2ab9a
Raul <raulvillajr95@gmail.com>
2023-08-15 07:58:11 -0700

    Leap year challenge Codecademy

--- main.cpp
+++ main.cpp
@@ -2,40 +2,25 @@
 
 int main()
 {
-  std::cout << "Enter earth weight: ";
-  double e_weight;
-  std::cin >> e_weight;
+  std::cout << "Enter year: ";
+  int year;
+  std::cin >> year;
 
-  std::cout << "Enter planet: ";
-  int planet;
-  std::cin >> planet;
+  bool is_leap_year;
 
-  double multiplier;
-  
-  switch (planet) {
-    case 1 :
-      multiplier = 0.38;
-      break;
-    case 2 :
-      multiplier = 0.91;
-      break;
-    case 3 :
-      multiplier = 0.38;
-      break;
-    case 4 :
-      multiplier = 2.34;
-      break;
-    case 5 :
-      multiplier = 1.06;
-      break;
-    case 6 :
-      multiplier = 0.92;
-      break;
-    case 7 :
-      multiplier = 1.19;
-      break;
+
+
+  if(year < 10000 && year > 999) {
+    if (year % 4 == 0) {
+      if (year % 100 == 0 && !(year % 400 == 0)) {
+        std::cout << "Is not a leap year ❌\n";
+      }
+      else {
+        std::cout << "Is leap year 👍\n";
+      }
+    }
   }
-
-  std::cout << "Your weight will be: " << e_weight * multiplier << "\n";
-
+  else {
+    std::cout << "Enter a 4 digit number\n";
+  }
 }
