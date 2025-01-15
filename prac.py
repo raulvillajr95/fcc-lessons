8bdace2f0b24dc3f051c7017d960e18c386918fc
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-01-15 08:23:44 -0800

    Exercise 5 Proj 1 CS4

--- prac.py
+++ prac.py
@@ -1,46 +1,4 @@
-# Problem Set 3 (Part I)
-# Name: Raul Villalobos
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is $%s."
 
-from string import *
-
-example = "atgacatgcacaagtatgcat"
-ex = "atgc"
-
-# def countSubStringMatch(target, key):
-
-#   found = 0
-#   totalFound = 0
-#   while (found != -1):
-#     found = target.find(key, found)
-#     if found != -1:
-#       found += 1
-#       totalFound += 1
-#   return totalFound
-
-# print(countSubStringMatch(example, ex))
-
-
-
-# print(example[example.find(ex,0)+1:])
-
-# def countSubStringMatchRecursive(target, key):
-
-#   targetOne = target[target.find(key,0)+1:]
-
-#   if(target.find(key,0) == -1):
-#     return 0
-#   else:
-#     return 1 + countSubStringMatchRecursive(targetOne, key)
-
-# print(countSubStringMatchRecursive(example, ex))
-
-def newtonsMethod(n, iterations):
-  start = n/2.0
-  result = 0.0
-
-  for i in range(0,iterations):
-    start = 0.5 * (start + 3/start)
-
-  return start
-
-print(newtonsMethod(3, 10))
+print(format_string % (data[0], data[1], data[2]))
