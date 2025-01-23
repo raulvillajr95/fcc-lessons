639458b05696de0fc30eae9601c8a3d4dfcba79f
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-01-23 11:51:13 -0800

    Project 1: Truth table

--- prac.py
+++ prac.py
@@ -1,10 +1,56 @@
-import re
+#Truth Table Generator - www.101computing.net/truth-table-generator/
 
-find_members = []
+def truthTable(expression, inputs=2):
+    print("Boolean Expression:")
+    print("  X = " + expression.upper())
+    expression = expression.lower()
 
-for member in dir(re):
-  if re.findall('find', member):
-    find_members.append(member)
+    #replace Boolean Operators with bitwise operators
+    expression = expression.replace("and", "&")
+    expression = expression.replace("xor", "^")
+    expression = expression.replace("or", "|")
+    expression = expression.replace("not", "~")
 
-find_members.sort()
-print(find_members)
+    print("\nTruth Table:")
+    if inputs==2:
+        print("  -------------")
+        print("  | A | B | X |")
+        print("  -------------")
+
+        for a in range(0,2):
+            for b in range(0,2):
+                x = eval(expression)
+                print("  | " + str(a) + " | " + str(b) + " | " + str(x) + " |")
+                print(" ")
+                print("  -------------")
+
+    elif inputs==3:
+        print("  -----------------")
+        print("  | A | B | C | X |")
+        print("  -----------------")
+
+        for a in range(0,2):
+            for b in range(0,2):
+                for c in range(0,2):
+                    x = eval(expression)
+                    print("  | " + str(a) + " | " + str(b) + " | " + str(c) + " | "+ str(x) + " |")
+                    print("  -----------------")
+    
+    elif inputs==4:
+        print("  ---------------------")
+        print("  | A | B | C | D | X |")
+        print("  ---------------------")
+
+        for a in range(0,2):
+            for b in range(0,2):
+                for c in range(0,2):
+                    for d in range(0,2):
+                        x = eval(expression)
+                        print("  | " + str(a) + " | " + str(b) + " | " + str(c) + " | " + str(d) + " | " + str(x) + " |")
+                        print("  ---------------------")
+
+########################################################
+
+expression = "A AND NOT (B XOR C)"
+truthTable(expression, 3)
+
