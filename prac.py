282a958deee70bbd487fb7de7c0e65b55ca20f81
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-02-15 07:41:43 -0800

    List Comprehensions exercise

--- prac.py
+++ prac.py
@@ -1,35 +1,7 @@
-# fill in this function
-def fib():
-  a = 1
-  b = 1
+numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+newlist = []
 
-  for i in range(10):
-    yield a
-    yield b
-    a = a+b
-    b = a+b
-######
-import random
+for num in numbers:
+  newlist.append(int(abs(num)))
 
-def lottery():
-  # returns 6 numbers between 1 and 40
-  for i in range(6):
-    yield random.randint(1, 40)
-  
-  yield random.randint(1, 15)
-
-for random_number in lottery():
-  print("And the next number is... %d!" %(random_number))
-######
-
-# testing code
-import types
-if type(fib()) == types.GeneratorType:
-  print("Good, The fib function is a generator.")
-
-  counter = 0
-  for n in fib():
-    print(n)
-    counter += 1
-    if counter == 10:
-      break
+print(newlist)
