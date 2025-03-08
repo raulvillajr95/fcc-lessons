5726acda28c7e10317f30e8fadeccacb1c5e22e0
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-03-08 07:47:43 -0800

    Python app for CS4 group proj A

--- prac.py
+++ prac.py
@@ -1,8 +1,14 @@
-a = ["Jake", "John", "Eric"]
-b = ["John", "Jill"]
+import math
 
-setA = set(a)
-setB = set(b)
+repeat = True
 
-print(setA.difference(setB))
-
+while repeat:
+  x = input("Input value within 0 and π/2 (1.570796): ")
+  sinxCosx = math.sin(float(x)) + math.cos(float(x))
+  result = "sin(" + x + ") + cos(" + x + ") = " + f'{sinxCosx:.6f}'
+  print(result)
+  choice = input("Try again? (Y/N): ")
+  if (choice.upper() == "Y"):
+    continue
+  else:
+    repeat = False
