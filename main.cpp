127e4197a2795450ccdd89af5333351e79f80c6d
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-10-27 06:58:43 -0700

    CW Price of Mangoes (done)

--- main.cpp
+++ main.cpp
@@ -1,36 +1,7 @@
 #include <iostream>
 using namespace std;
 
-double calculator(double a, double b, char op)
-{
-    double res = 0.0;
-
-    if(op == '+')
-    {
-        res = a+b;
-    } else if(op == '-')
-    {
-        res = a-b;
-    } else if(op == '*')
-    {
-        res = a*b;
-    } else if(op == '/')
-    {
-        res = a/b;
-    } else
-    {
-        throw invalid_argument ("Invalid argument");
-    }
-
-    return res;
-}
-
 int main()
 {
-    cout << calculator(1, 2, '+') << endl;
-    cout << calculator(1, 2, '-') << endl;
-    cout << calculator(3, 5, '*') << endl;
-    cout << calculator(6, 2, '/') << endl;
-
     return 0;
 }
