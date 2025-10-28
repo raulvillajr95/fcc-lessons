37aa841d61023afa7050a259927a2853c8dc6ab1
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-10-28 08:26:02 -0700

    CW Collatz Conjecture (3n+1) (done)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
+unsigned int hotpo(unsigned int n)
+{
+    int count = 0;
+
+    while(n != 1)
+    {
+        if(n % 2 == 0)
+        {
+            n /= 2;
+        }
+        else
+        {
+            n = 3*n + 1;
+        }
+        count += 1;
+    }
+
+    return count;
+}
+
 int main()
 {
+    cout << hotpo(1) << endl;
+    cout << hotpo(5) << endl;
+
     return 0;
 }
