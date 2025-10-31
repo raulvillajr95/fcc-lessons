f2b1aa1e09c27e0baefb48c63a88c38a951e10f5
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-10-31 08:25:28 -0700

    CW Area of a Square (done)

--- main.cpp
+++ main.cpp
@@ -1,45 +1,25 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-bool isOpposite(const string& s1, const string& s2)
+double square_area(double s)
 {
-    int n1 = s1.size();
-    int n2 = s2.size();
+    double r = s / (M_PI / 2.0);
 
-    if(n1 == 0 || n2 == 0 || (n1 != n2))
-    {
-        return false;
-    }
-
-    bool res = true;
-
-    for(int i = 0; i < n1; i++)
-    {
-        int diff = s1[i] - s2[i];
-        char s1Low = tolower(s1[i]);
-        char s2Low = tolower(s2[i]);
-
-        if(diff == 0 || (s1Low != s2Low))
-        {
-            res = false;
-        }
-    }
+    double res = round(r*r*100)/100;
 
     return res;
 }
 
 int main()
 {
+    cout << square_area(2) << endl;
+
     return 0;
 }
 
 /*
-9*6 = 54
-54 - (2*8) = 38
-38 - (1*12) = 26
-
-(n+1)*(n+1) * 6 = a
-a - (2*8) = b
-b - ((n+1-2)*12) = c
-
+s = rt
+r = s/t
+r = s/(π/2)
 */
