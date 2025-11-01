8bbb7a8193463a732604bb1426deba8af9977f3d
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-01 09:28:26 -0700

    CW Logical calculator (done)

--- main.cpp
+++ main.cpp
@@ -1,19 +1,56 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 using namespace std;
 
-double square_area(double s)
+bool oper(bool a, bool b, string str)
 {
-    double r = s / (M_PI / 2.0);
 
-    double res = round(r*r*100)/100;
+    bool res = false;
+
+    if (str == "AND")
+    {
+        res = a && b;
+    }
+    else if (str == "OR")
+    {
+        res = a || b;
+    }
+    else if (str == "XOR")
+    {
+        res = a xor b;
+    }
+
+    return res;
+}
+
+bool logicalCalc(const vector<bool>& items, const string& op)
+{
+    bool res = true;
+
+    int n = items.size();
+
+    for (int i = 0; i < n-1; i++)
+    {
+        if(i == 0)
+        {
+            res = oper(items[i], items[i+1], op);
+        } else
+        {
+            res = oper(res, items[i+1], op);
+        }
+    }
 
     return res;
 }
 
 int main()
 {
-    cout << square_area(2) << endl;
+    cout << oper(true, true, "AND") << endl;
+    cout << oper(true, false, "AND") << endl;
+    cout << oper(true, true, "OR") << endl;
+    cout << oper(true, false, "OR") << endl;
+    cout << oper(true, true, "XOR") << endl;
+    cout << oper(false, false, "XOR") << endl;
 
     return 0;
 }
