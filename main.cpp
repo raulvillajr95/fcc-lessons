fa4db49f3a94b480bc99342abf9aa82011e057a3
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-05 07:11:21 -0800

    CW Playing with cubes I (done)

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-using point = const pair<double, double>;
-
-pair<double, double> barTriang(point a, point b, point c)
+class Cube
 {
-    double xo = (a.first + b.first + c.first) / 3.0;
-    double yo = (a.second + b.second + c.second) / 3.0;
+public:
+    void SetSide(int num);
+    int GetSide();
+private:
+    int Side = 0;
+};
 
-    xo = round(xo * 10000.0) / 10000.0;
-    yo = round(yo * 10000.0) / 10000.0;
+int Cube::GetSide(void)
+{
+    return Side;
+}
 
-    cout << "xo: " << xo << endl;
-    cout << "yo: " << yo << endl;
-
-    return {xo, yo};
+void Cube::SetSide(int num)
+{
+    Side = num;
 }
 
 int main()
 {
-    barTriang({4,6}, {12,4},{10,10});
-    barTriang({4,2}, {12,2},{6,10});
-
     return 0;
 }
