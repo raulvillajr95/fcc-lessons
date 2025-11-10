d2963dcbcfa7a3c7bf8ca9a131ebb4d75053bea7
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-09 20:50:00 -0800

    CW Summation Triangle #1 (partial 2)

--- main.cpp
+++ main.cpp
@@ -26,24 +26,29 @@ void displayArrInt(vector<int> arr)
    cout << endl;
 }
 
-int triSum(int num)
+unsigned long long get_sum(unsigned int num)
 {
-   num++;
-   int count = 0;
-   int start = 1;
-   // int temp = num;
-   while (num > 0)
+   unsigned long long int count = 0;
+   
+   for(unsigned int i = 1; i <= (num + 1); i++)
    {
-      cout << "num: " << num << endl;
-      for(int i = start; i < start+num; i++)
+      if (i % 2 == 0)
       {
-         cout << i << endl;
-         count += i;
+         unsigned int temp = i;
+         for (unsigned int j = 0; j < i; j++)
+         {
+            count += temp;
+            temp += 2;
+         }
+      } else
+      {
+         unsigned int temp = i;
+         for (unsigned int j = 0; j < i; j++)
+         {
+            count += temp;
+            temp += 2;
+         }
       }
-
-      start += num;
-      
-      num--;
    }
 
    return count;
@@ -54,7 +59,12 @@ int main()
    // vector<int> test1 = latinRow(4, 0);
    // displayArrInt(test1);
 
-   cout << triSum(2) << endl;
+   cout << get_sum(0) << endl; // 
+   cout << get_sum(1) << endl; // 7 + 15
+   cout << get_sum(2) << endl; // 22 + 28
+   cout << get_sum(3) << endl; // 50 + 45
+   cout << get_sum(4) << endl; // 95 + 64
+   cout << get_sum(5) << endl; // 161
 
    return 0;
 }
