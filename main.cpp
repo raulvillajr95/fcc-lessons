8d6d7da283977da485e820db9194559c84a8f529
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-11 08:30:44 -0800

    CW Jumps in a cycle #1 (partial)

--- main.cpp
+++ main.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+void displayInt(vector<int> arr)
+{
+   int n = arr.size();
+   for (int i = 0; i < n; i++)
+   {
+      cout << arr[i] << " ";
+   }
+   cout << endl;
+}
+
+long getJumps(const vector<int>& cycle, long long jump)
+{
+   int n = cycle.size();
+   long long pos = jump;
+   long long count = 1;
+
+   while(pos % n != 0)
+   {
+      pos += jump;
+      count++;
+   }
+
+   return count;
+}
 
 int main()
 {
-   int n = 3;
-   int count = n+n;
-   for (int i = 0; i < n; i++)
+   vector<int> test1 = {1,5,7,8,9};
+   int jump = 2;
+   int n = test1.size();
+   int pos = jump;
+   int count = 1;
+
+   while(pos % n != 0)
    {
-      for (int j = 1; j <= n; j++)
-      {
-         cout << i << "|" << j << endl;
-         count += i;
-         count += j;
-      }
+      pos += jump;
+      count++;
    }
 
    cout << count << endl;
+
 
    return 0;
 }
