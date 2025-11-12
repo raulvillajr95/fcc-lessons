454bb2842b6f818dfe8b38947afc648c9f4fe062
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-11 18:28:26 -0800

    CW Compare 2 digit numbers (done)

--- main.cpp
+++ main.cpp
@@ -2,47 +2,89 @@
 #include <vector>
 using namespace std;
 
-void displayInt(vector<int> arr)
+vector<short> splitSort(unsigned short num)
 {
-   int n = arr.size();
-   for (int i = 0; i < n; i++)
+   vector<short> res;
+   string strNum = to_string(num);
+
+   int n = strNum.size();
+   for (int i = 0; i < n;i++)
    {
-      cout << arr[i] << " ";
+      res.push_back(strNum[i] - '0');
    }
-   cout << endl;
+   
+   sort(res.begin(), res.end());
+
+   return res;
 }
 
-long getJumps(const vector<int>& cycle, long long jump)
+string compare(unsigned short a, unsigned short b)
 {
-   int n = cycle.size();
-   long long pos = jump;
-   long long count = 1;
+   // vector<short> arrA = splitSort(a);
+   // vector<short> arrB = splitSort(b);
 
-   while(pos % n != 0)
+   string strNumA = to_string(a);
+   string strNumB = to_string(b);
+
+   if ((strNumA[0] == strNumB[1]) && (strNumA[1] == strNumB[0]))
    {
-      pos += jump;
+      cout << strNumA[0] << " = " << strNumB[1] << endl;
+      cout << strNumA[1] << " = " << strNumB[0] << endl;
+      // shouldn't run for 66, 16
+      cout << "F" << endl;
+      return "100%";
+   } else if ((strNumA[0] == strNumB[1]) && (strNumA[1] != strNumB[0]))
+   {
+      return "50%";
+   } else if ((strNumA[0] == strNumB[0]) && (strNumA[1] != strNumB[1]))
+   {
+      return "50%";
+   }
+
+
+   // if ((strNumA[0] == strNumB[1]) && (strNumA[1] == strNumB[0]))
+   // {
+   //    return "100%";
+   // }
+
+   // 66 , 61
+
+   int count = 0;
+   if(strNumA[0] == strNumB[0])
+   {
+      count++;
+   }
+   else if (strNumA[0] == strNumB[1])
+   {
       count++;
    }
 
-   return count;
+   if(strNumA[1] == strNumB[1])
+   {
+      count++;
+   }
+   else if (strNumA[1] == strNumB[0])
+   {
+      count++;
+   }
+
+
+   string message = "0%";
+   if(count == 2)
+   {
+      message = "100%";
+   }
+   else if (count == 1)
+   {
+      message = "50%";
+   }
+
+   return message;
 }
 
 int main()
 {
-   vector<int> test1 = {1,5,7,8,9};
-   int jump = 2;
-   int n = test1.size();
-   int pos = jump;
-   int count = 1;
-
-   while(pos % n != 0)
-   {
-      pos += jump;
-      count++;
-   }
-
-   cout << count << endl;
-
+   cout << compare(66,16) << endl;
 
    return 0;
 }
