917ff4a8bd5c9d28b8bfe7d266e1ab0700117bde
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-12 07:06:58 -0800

    CW Noisy Cell Counts (done)

--- main.cpp
+++ main.cpp
@@ -2,89 +2,35 @@
 #include <vector>
 using namespace std;
 
-vector<short> splitSort(unsigned short num)
+vector<int> cleaned_counts(const vector<int>& data)
 {
-   vector<short> res;
-   string strNum = to_string(num);
+   vector<int> res = {data[0]};
 
-   int n = strNum.size();
-   for (int i = 0; i < n;i++)
+   int n = data.size();
+   int biggest = data[0];
+   for (int i = 0; i < n-1; i++)
    {
-      res.push_back(strNum[i] - '0');
+      if (biggest > data[i+1])
+      {
+         res.push_back(biggest);
+      }
+      else if (data[i+1] > biggest)
+      {
+         biggest = data[i+1];
+         res.push_back(biggest);
+      }
+      else
+      {
+         res.push_back(data[i+1]);
+      }
+      
    }
-   
-   sort(res.begin(), res.end());
 
    return res;
 }
 
-string compare(unsigned short a, unsigned short b)
-{
-   // vector<short> arrA = splitSort(a);
-   // vector<short> arrB = splitSort(b);
-
-   string strNumA = to_string(a);
-   string strNumB = to_string(b);
-
-   if ((strNumA[0] == strNumB[1]) && (strNumA[1] == strNumB[0]))
-   {
-      cout << strNumA[0] << " = " << strNumB[1] << endl;
-      cout << strNumA[1] << " = " << strNumB[0] << endl;
-      // shouldn't run for 66, 16
-      cout << "F" << endl;
-      return "100%";
-   } else if ((strNumA[0] == strNumB[1]) && (strNumA[1] != strNumB[0]))
-   {
-      return "50%";
-   } else if ((strNumA[0] == strNumB[0]) && (strNumA[1] != strNumB[1]))
-   {
-      return "50%";
-   }
-
-
-   // if ((strNumA[0] == strNumB[1]) && (strNumA[1] == strNumB[0]))
-   // {
-   //    return "100%";
-   // }
-
-   // 66 , 61
-
-   int count = 0;
-   if(strNumA[0] == strNumB[0])
-   {
-      count++;
-   }
-   else if (strNumA[0] == strNumB[1])
-   {
-      count++;
-   }
-
-   if(strNumA[1] == strNumB[1])
-   {
-      count++;
-   }
-   else if (strNumA[1] == strNumB[0])
-   {
-      count++;
-   }
-
-
-   string message = "0%";
-   if(count == 2)
-   {
-      message = "100%";
-   }
-   else if (count == 1)
-   {
-      message = "50%";
-   }
-
-   return message;
-}
-
 int main()
 {
-   cout << compare(66,16) << endl;
 
    return 0;
 }
