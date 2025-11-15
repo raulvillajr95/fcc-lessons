92db21168063d4720f593ccaee0b121e8e0c398b
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-14 17:23:40 -0800

    CW Interlocking Binary Pairs (partial)

--- main.cpp
+++ main.cpp
@@ -1,23 +1,50 @@
 #include <iostream>
-#include <map>
+#include <vector>
+#include <cmath>
 using namespace std;
 
-map<char, unsigned> count(const string& str)
+vector<int> bin(unsigned long long num)
 {
-   map<char, unsigned> chars;
+   vector<int> res;
 
-   int n = str.size();
-   for(int i = 0; i < n; i++)
+   int n = to_string(num).size();
+
+   unsigned long long two = 0;
+   int power = 0;
+   while(true)
    {
-      chars[str[i]]++;
+      two = pow(2, power);
+      power++;
    }
+   
+   cout << two << endl;
 
-   return chars;
+   // for (int i = 0; i < n; i++)
+   // {
+   //    int n = pow(2, i);
+   //    if (num % n == 0)
+   // }
+   
+   return res;
+}
+
+bool interlockable(unsigned long long a, unsigned long long b)
+{
+
+   return false;
 }
 
 int main()
 {
-
+   bin(9);
 
    return 0;
 }
+
+/*
+turn to binary
+find shortest
+loop through shortest
+   compare from right to left
+   if both 1's exit
+*/
