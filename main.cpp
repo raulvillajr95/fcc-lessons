a67b0aa1981871f5bf87440d2c864c8f593afe4a
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-15 21:17:36 -0800

    CW Barista problem (done)

--- main.cpp
+++ main.cpp
@@ -1,40 +1,74 @@
 #include <iostream>
 #include <vector>
-#include <utility>
 using namespace std;
 
-vector<string> openOrSenior(vector<pair<int, int>>& data)
+int barista(const vector<int>& v)
 {
-   vector<string> res;
+   vector<int> coff = v;
+   int n = coff.size();
 
-   int n = data.size();
-   for(int i = 0; i < n; i++)
+   if(n == 0)
    {
-      if (data[i].first >= 55 && data[i].second > 7)
-      {
-         res.push_back("Senior");
-      }
-      else
-      {
-         res.push_back("Open");
-      }
-      
+      return 0;
+   }
+   else if (n == 1)
+   {
+      return coff[0];
    }
 
-   return res;
+   sort(coff.begin(), coff.end());
+
+   int tot = coff[0];
+   int temp = coff[0];
+   for (int i = 1; i < n; i++)
+   {
+      tot += (temp + 2 + coff[i]);
+      temp += 2 + coff[i];
+   }
+
+   return tot;
 }
 
 int main()
 {
-   bin(9);
+
+   vector<int> coff = {4,3,2};
+   int n = coff.size();
+
+   if(n == 0)
+   {
+      cout << 0 << endl;
+   }
+   else if (n == 1)
+   {
+      cout << coff[0] << endl;
+   }
+
+   sort(coff.begin(), coff.end());
+
+   int tot = coff[0];
+   int temp = coff[0];
+   cout << "start: " << tot << endl << endl;
+   for (int i = 1; i < n; i++)
+   {
+      cout << tot << " " << 2 << " " << coff[i] << endl;
+      tot += (temp + 2 + coff[i]);
+      temp += 2 + coff[i];
+      cout << "after: " << i << ", tot: " << tot << endl << endl;
+   }
+
+   cout << "res: " << tot << endl;
 
    return 0;
 }
 
 /*
-turn to binary
-find shortest
-loop through shortest
-   compare from right to left
-   if both 1's exit
+per coffee:
+   make coffee + 2 min to clean
+
+if 0, then 0
+if 1, then that 1 wait time
+else,
+   sort
+
 */
