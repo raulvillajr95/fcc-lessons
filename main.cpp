fa8212ec3fcb86ce7dfa0098b388d00d5cee4719
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-18 15:28:28 -0800

    CW Max sum between two negatives (done)

--- main.cpp
+++ main.cpp
@@ -1,64 +1,90 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-string plant(string seed, int stem, int flower, int temp)
+int sumNums(vector<int> arr, int a, int b)
 {
-   string res = "";
-
-   // Broken plant
-   if (temp < 20 || temp > 30)
+   int sum = 0;
+   
+   if (a == (b-1))
    {
-      for(int i = 0; i < stem; i++)
+      return sum;
+   }
+   else
+   {
+      for(int i = a+1; i < b; i++)
       {
-         string temp = "";
-
-         for (int j = 0; j < stem; j++)
-         {
-            temp += "-";
-         }
-
-         res += temp;
+         sum += arr[i];
       }
-      res += seed;
-
-      return res;
    }
 
-   for(int i = 0; i < stem; i++)
+   return sum;
+}
+
+vector<int> posNeg(vector<int> arr)
+{
+   vector<int> negatives;
+   int nA = arr.size();
+
+   for(int i = 0; i < nA; i++)
    {
-      string temp = "";
-
-      for (int j = 0; j < stem; j++)
+      if(arr[i] < 0)
       {
-         temp += "-";
+         negatives.push_back(i);
       }
-
-      for (int k = 0; k < flower; k++)
-      {
-         temp += seed;
-      }
-
-      res += temp;
    }
 
-   return res;
+   return negatives;
+}
+
+int MaxSumBetweenTwoNegatives(vector<int> arr)
+{
+   // Get positions of negatives.
+   vector<int> negatives = posNeg(arr);
+   int nN = negatives.size();
+   
+   if (nN == 1 || nN == 0)
+   {
+      return -1;
+   }
+
+   int maxCount = 0;
+   for(int i = 0; i < nN-1; i++)
+   {
+      int a = negatives[i];
+      int b = negatives[i+1];
+      int sum = sumNums(arr, a, b);
+      if(sum > maxCount)
+      {
+         maxCount = sum;
+      }
+   }
+
+   return maxCount;
+}
+
+void dispInt(vector<int> arr)
+{
+   int n = arr.size();
+   for (int i = 0; i < n; i++)
+   {
+      cout << arr[i] << " ";
+   }
+   cout << endl;
 }
 
 int main()
 {
+   vector<int> test1 = {4, -1, 6, -2, 3, 5, -7, 7};
+   // cout << sumNums(test1, 1, 2) << endl;
+   cout << MaxSumBetweenTwoNegatives(test1) << endl;
 
    return 0;
 }
 
 /*
 
-1,1,1 : 12
-2,1,1 : 20
-
-2,2,2 : 
-
-
-start at 1(a) to n(some letter)
-   return one string
+find position of negatives
+get sum of nums
 
 */
