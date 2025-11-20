b1ffc0e4895f7aa2be90751ebdb9ab49f8749319
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-20 13:46:47 -0800

    CW Fusion Chamber Shutdown (done)

--- main.cpp
+++ main.cpp
@@ -1,33 +1,75 @@
 #include <iostream>
-#include <vector>
+#include <array>
 using namespace std;
 
-int main()
+array<int, 3> burner(int c, int h, int o)
 {
-   // C, H, O
-   vector<int> elements = {45,11,100};
-
-   // H2O, CO2, CH4
-   vector<int> molecules = {0,0,0};
+   array<int, 3> molecules = {0,0,0};
 
    // Get H2O
-   while (elements[1] >= 2 && elements[2] >= 1)
+   while(h >= 2 && o >= 1)
    {
-      elements[1] -= 2;
-      elements[2]--;
+      h -= 2;
+      o--;
       molecules[0]++;
    }
 
    // Get CO2
-   while (elements[0] >= 1 && elements[2] >= 2)
+   while(c >= 1 && o >= 2)
    {
-      elements[0]--;
-      elements[2] -= 2;
+      c--;
+      o -= 2;
       molecules[1]++;
    }
 
    // Get CH4
-   
+   while(c >= 1 && h >= 4)
+   {
+      c--;
+      h -= 4;
+      molecules[2]++;
+   }
+
+   return molecules;
+}
+
+int main()
+{
+   // C, H, O
+   int c = 45;
+   int h = 11;
+   int o = 100;
+
+   // H2O, CO2, CH4
+   array<int, 3> molecules = {0,0,0};
+
+   // Get H2O
+   while(h >= 2 && o >= 1)
+   {
+      h -= 2;
+      o--;
+      molecules[0]++;
+   }
+
+   // Get CO2
+   while(c >= 1 && o >= 2)
+   {
+      c--;
+      o -= 2;
+      molecules[1]++;
+   }
+
+   // Get CH4
+   while(c >= 1 && h >= 4)
+   {
+      c--;
+      h -= 4;
+      molecules[2]++;
+   }
+
+   cout << molecules[0] << " ";
+   cout << molecules[1] << " ";
+   cout << molecules[2] << endl;
 
    return 0;
 }
