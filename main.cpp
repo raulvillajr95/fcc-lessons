3baf154e9a1e3b84e421a0c5d6c2ea6f94d8132f
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-20 10:25:04 -0800

    hw 16 5)d wrong formula

--- main.cpp
+++ main.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-
-string uncensor(const string& word, const string& cens)
-{
-   if (cens.size() == 0)
-   {
-      return word;
-   }
-
-   int n = word.size();
-   int pos = 0;
-   string res = "";
-   for(int i = 0; i < n; i++)
-   {
-      if (word[i] == '*')
-      {
-         res += cens[pos];
-         pos++;
-      }
-      else
-      {
-         res += word[i];
-      }
-   }
-
-   return res;
-}
 
 int main()
 {
-   cout << uncensor("A**Z*N*", "MAIG") << endl;
+   // C, H, O
+   vector<int> elements = {45,11,100};
+
+   // H2O, CO2, CH4
+   vector<int> molecules = {0,0,0};
+
+   // Get H2O
+   while (elements[1] >= 2 && elements[2] >= 1)
+   {
+      elements[1] -= 2;
+      elements[2]--;
+      molecules[0]++;
+   }
+
+   // Get CO2
+   while (elements[0] >= 1 && elements[2] >= 2)
+   {
+      elements[0]--;
+      elements[2] -= 2;
+      molecules[1]++;
+   }
+
+   // Get CH4
+   
 
    return 0;
 }
