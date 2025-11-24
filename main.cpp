b072fa1a8d405e7208871bec3d48a95d44fb03cb
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-23 19:16:43 -0800

    Quantum Tunneling Probability program

--- main.cpp
+++ main.cpp
@@ -1,52 +1,54 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-long pentagonal(long num)
+long double tunnProb(long double m, long double L, long double U_0, long double E)
 {
-   if (num <= 0)
-   {
-      return -1;
-   }
+   // // Conversions
+   // L = 
 
-   long count = 1 + (5*((num - 1)*(num))/2);
+   // Prep computation
+   long double beta = sqrt((2*m*(U_0 - E))/(pow((6.626e-34/(2*M_PI)),2)));
+   long double k = sqrt((2*m*E)/(pow((6.626e-34/(2*M_PI)),2)));
+   long double gamma = (beta/k) - (k/beta);
+   long double betaL = beta * L;
+   long double cosh2 = cosh(betaL) * cosh(betaL);
+   long double sinh2 = sinh(betaL) * sinh(betaL);
 
-   // for (long i = 0; i < num; i++)
-   // {
-   //    if (i == 0)
-   //    {
-   //       count++;
-   //    }
-   //    else
-   //    {
-   //       count += (i * 5);
-   //    }
-   // } 
+   // debug
+   cout << beta << endl;
+   cout << k << endl;
+   cout << gamma << endl;
 
-   return count;
+   // Final comp
+   long double P = 100 / (cosh2 + ((gamma/2)*(gamma/2))*sinh2);
+   return P;
 }
 
 int main()
 {
+   cout << tunnProb(6.64e-27, 2e-15, 4.806e-12, 4.646e-12) << endl;
+   cout << tunnProb(6.64e-27, 2e-15, 4.806e-12, 3.204e-12) << endl;
 
-   // int num = 8;
-   // int count = 0;
-   // for (int i = 0; i < num; i++)
-   // {
-   //    if (i == 0)
-   //    {
-   //       count++;
-   //    }
-   //    else
-   //    {
-   //       count += (i * 5);
-   //    }
-   // }
+   string word = "helLo";
+   string wordLow = "";
 
-   // cout << count << endl;
+   // Lower letters;
+   int n = word.size();
+   for (int i = 0; i < n; i++)
+   {
+      wordLow += tolower(word[i]);
+   }
 
-   cout << pentagonal(1) << endl;
-   cout << pentagonal(2) << endl;
-   cout << pentagonal(3) << endl;
+   // If 1 letter, return "0"
+
+   // Check if
+   int num = 0;
+   for(int i = 1; i < n; i++)
+   {
+
+   }
+
    return 0;
 }
 
