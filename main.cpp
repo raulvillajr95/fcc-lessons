41b371af053b44ae126351cdae3b28f91d6dabb9
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-23 19:50:18 -0800

    CW Cryptanalysis Word Patterns (partial)

--- main.cpp
+++ main.cpp
@@ -1,36 +1,9 @@
 #include <iostream>
-#include <cmath>
+#include <map>
 using namespace std;
 
-long double tunnProb(long double m, long double L, long double U_0, long double E)
+string wordPattern(const string &word)
 {
-   // // Conversions
-   // L = 
-
-   // Prep computation
-   long double beta = sqrt((2*m*(U_0 - E))/(pow((6.626e-34/(2*M_PI)),2)));
-   long double k = sqrt((2*m*E)/(pow((6.626e-34/(2*M_PI)),2)));
-   long double gamma = (beta/k) - (k/beta);
-   long double betaL = beta * L;
-   long double cosh2 = cosh(betaL) * cosh(betaL);
-   long double sinh2 = sinh(betaL) * sinh(betaL);
-
-   // debug
-   cout << beta << endl;
-   cout << k << endl;
-   cout << gamma << endl;
-
-   // Final comp
-   long double P = 100 / (cosh2 + ((gamma/2)*(gamma/2))*sinh2);
-   return P;
-}
-
-int main()
-{
-   cout << tunnProb(6.64e-27, 2e-15, 4.806e-12, 4.646e-12) << endl;
-   cout << tunnProb(6.64e-27, 2e-15, 4.806e-12, 3.204e-12) << endl;
-
-   string word = "helLo";
    string wordLow = "";
 
    // Lower letters;
@@ -40,14 +13,51 @@ int main()
       wordLow += tolower(word[i]);
    }
 
+   // cout << wordLow << endl;
+
+   string res = "0.";
+   map<char, int> myMap;
    // If 1 letter, return "0"
+   if(n == 1)
+   {
+      return "0";
+   }
+   else if (n == 0)
+   {
+      return "";
+   }
 
    // Check if
    int num = 0;
    for(int i = 1; i < n; i++)
    {
+      // cout << word[i] << " " << word[i-1] << endl;
+      if(wordLow[i] != wordLow[i-1])
+      {
+         // cout << "DIFF" << endl;
+         num++;
+         res += to_string(num);
+      }
+      else
+      {
+         res += to_string(num);
+      }
 
+      if (i < n-1)
+      {
+         res += ".";
+      }
    }
+
+   return res;
+}
+
+int main()
+{
+   cout << wordPattern("") << endl;
+   cout << wordPattern("hello") << endl;
+   cout << wordPattern("helLo") << endl;
+   cout << wordPattern("helLo") << endl;
 
    return 0;
 }
