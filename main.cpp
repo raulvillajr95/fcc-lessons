853b21c4545efe6d3eecb8c575255f2d1a221e3a
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-24 10:35:13 -0800

    1 on final review TeX

--- main.cpp
+++ main.cpp
@@ -1,42 +1,11 @@
 #include <iostream>
 using namespace std;
 
-string womens_age(unsigned n)
-{
-   string nStr = to_string(n);
-   string baseStr = "";
-
-   unsigned base = 0;
-   int age = 0;
-   if(n % 2 == 0)
-   {
-      base = n/2;
-      age = 20;
-   }
-   else
-   {
-      base = (n-1)/2;
-      age = 21;
-   }
-   baseStr = to_string(base);
-
-   return nStr + "? That's just " + to_string(age) + ", in base " + baseStr + "!";
-}
-
 int main()
 {
-   cout << womens_age(32) << endl;
-   cout << womens_age(39) << endl;
 
    return 0;
 }
 
 /*
-
-if odd:
-   divide by 2
-if even:
-   take away 1
-   divide by 2
-
 */
