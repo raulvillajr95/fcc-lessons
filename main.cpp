0a0329b3674f3586af2172104ccddb929a54da56
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-24 07:56:20 -0800

    CW Happy Birthday, Darling! (done)

--- main.cpp
+++ main.cpp
@@ -1,56 +1,42 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-string getTurkishNumber(unsigned int n)
+string womens_age(unsigned n)
 {
-   vector<string> ones = {"sıfır","bir","iki","üç","dört","beş","altı","yedi","sekiz","dokuz"};
-   vector<string> tens = {"on","yirmi","otuz","kırk","elli","altmış","yetmiş","seksen","doksan"};
+   string nStr = to_string(n);
+   string baseStr = "";
 
-   int one = 0;
-   int two = 0;
-
-   string message = "";
-
-   if(n < 10)
+   unsigned base = 0;
+   int age = 0;
+   if(n % 2 == 0)
    {
-      message += ones[n];
+      base = n/2;
+      age = 20;
    }
    else
    {
-      one = n % 10;
-      two = (n - one)/10;
-
-      if(one == 0)
-      {
-         message += tens[two-1];
-      }
-      else
-      {
-         message += tens[two-1];
-         message += " ";
-         message += ones[one];
-      }
-
-      
+      base = (n-1)/2;
+      age = 21;
    }
+   baseStr = to_string(base);
 
-   return message;
+   return nStr + "? That's just " + to_string(age) + ", in base " + baseStr + "!";
 }
 
 int main()
 {
-   cout << getTurkishNumber(0) << endl;
-   cout << getTurkishNumber(16) << endl;
-   cout << getTurkishNumber(70) << endl;
-   cout << getTurkishNumber(26) << endl;
+   cout << womens_age(32) << endl;
+   cout << womens_age(39) << endl;
 
    return 0;
 }
 
 /*
-need to make it faster
 
-possibly find a k + k+1 type pattern to get final result
+if odd:
+   divide by 2
+if even:
+   take away 1
+   divide by 2
 
 */
