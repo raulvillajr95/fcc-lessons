88775deeb2c2eef5e594eb7c497da8c3ebd642c0
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-25 17:40:16 -0800

    CW Square Pi's (done)

--- main.cpp
+++ main.cpp
@@ -1,91 +1,24 @@
 #include <iostream>
-#include <vector>
+#include <cmath>
 using namespace std;
 
-string baby_shark_lyrics(){string r="",d=", doo doo doo doo doo doo\n",s=" shark",l="Let's go hunt";vector<string>p={"Baby","Mommy","Daddy","Grandma","Grandpa"};for(auto i:p){for(int j=0;j<3;j++){r+=i+s+d;}r+=i+s+"!\n";}for(int i=0;i<3;i++){r+=l+d;}r+=l+"!\n"+"Run away,…";return r;}
-
-void displayStr(vector<string> arr)
+int square_pi(int digits)
 {
-   int n = arr.size();
-   for (int i = 0; i < n; i++)
+   string pee = "31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+   int sum = 0;
+   for (int i = 0; i < digits; i++)
    {
-      cout << arr[i] << endl;
-   }
-}
-
-int solve(string word)
-{
-   int n = word.size();
-
-   vector<string> prefixes;
-   for(int i = 0; i < n-1; i++)
-   {
-      string pre = word.substr(0,i+1);
-      prefixes.push_back(pre);
-   }
-   // displayStr(prefixes);
-
-   vector<string> suffixes;
-   for (int i = n-1; i >= 1; i--)
-   {
-      string suff = word.substr(i);
-      suffixes.push_back(suff);
-   }
-   // displayStr(suffixes);
-
-   int fix = suffixes.size();
-   int longest = 0;
-   for(int i = 0; i < fix/2; i++)
-   {
-      if(prefixes[i] == suffixes[i])
-      {
-         if(prefixes[i].size() > longest)
-         {
-            longest = prefixes[i].size();
-         }
-      }
+      int dig = pee[i] - '0';
+      sum += (dig * dig);
    }
 
-   return longest;
+   return ceil(sqrt(sum));
 }
 
 int main()
 {
-   // cout << baby_shark_lyrics() << endl;
-
-   string word = "abcabc";
-   int n = word.size();
-
-   vector<string> prefixes;
-   for(int i = 0; i < n-1; i++)
-   {
-      string pre = word.substr(0,i+1);
-      prefixes.push_back(pre);
-   }
-   // displayStr(prefixes);
-
-   vector<string> suffixes;
-   for (int i = n-1; i >= 1; i--)
-   {
-      string suff = word.substr(i);
-      suffixes.push_back(suff);
-   }
-   // displayStr(suffixes);
-
-   int fix = suffixes.size();
-   int longest = 0;
-   for(int i = 0; i <= fix/2; i++)
-   {
-      if(prefixes[i] == suffixes[i])
-      {
-         if(prefixes[i].size() > longest)
-         {
-            longest = prefixes[i].size();
-         }
-      }
-   }
-
-   cout << longest << endl;
+   cout << square_pi(1) << endl;
+   cout << square_pi(3) << endl;
 
    return 0;
 }
