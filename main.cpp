1242d5fc65d65c17f9cfc9ddc3d50763b1549ee2
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-25 16:39:55 -0800

    CW Baby shark lyrics generator (partial) v2

--- main.cpp
+++ main.cpp
@@ -2,26 +2,7 @@
 #include <vector>
 using namespace std;
 
-string baby_shark_lyrics()
-{
-   string r = "", d = ", doo doo doo doo doo doo\n", s = " shark", l = "Let's go hunt";
-   vector<string> p = {"Baby", "Mommy", "Daddy", "Grandma", "Grandpa"};
-
-   for(int i = 0; i < 5; i++)
-   {
-      for (int j = 0; j < 3; j++)
-         r += p[i] + s + d;
-
-      r += p[i] + s + "!\n";
-   }
-   
-   for(int i = 0; i < 3; i++)
-      r += l + d;
-
-   r += l + "!\n" + "Run away,…";
-
-   return r;
-}
+string baby_shark_lyrics(){string r="",d=", doo doo doo doo doo doo\n",s=" shark",l="Let's go hunt";vector<string>p={"Baby","Mommy","Daddy","Grandma","Grandpa"};for(int i=0;i<5;i++){for(int j=0;j<3;j++){r+=p[i]+s+d;}r+=p[i]+s+"!\n";}for(int i=0;i<3;i++){r+=l+d;}r+=l+"!\n"+"Run away,…";return r;}
 
 int main()
 {
