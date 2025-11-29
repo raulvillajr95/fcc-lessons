6843ec053130b7dcd85d9a617116328df45c4b7f
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-11-29 12:29:18 -0800

    CW Which section did you scroll to? (done)

--- main.cpp
+++ main.cpp
@@ -2,56 +2,27 @@
 #include <vector>
 using namespace std;
 
-void dispInt(vector<int> arr)
+int get_section_id(int n, vector<int> a)
 {
-   int n = arr.size();
-   for(int i = 0; i < n; i++)
-   {
-      cout << arr[i] << " ";
-   }
-   cout << endl;
-}
 
-vector<int> Pendulum(vector<int> arr)
-{
-   sort(arr.begin(), arr.end());
-   int n = arr.size();
-   int pos = 0;
-   vector<int> res;
-   vector<int> pB;
-   vector<int> pF;
-
-   while(n > 0)
+   int Na = a.size();
+   int res = 0;
+   int count = 0;
+   for (int i = 0; i < Na; i++)
    {
-      if (n > 0)
+      count += a[i];
+      if (n >= count)
       {
-         res.insert(res.begin(), arr[pos]);
-         pF.push_back(arr[pos]);
-
-         n--;
-         pos++;
+         res++;
       }
 
-      if (n > 0)
+      if(i == Na-1)
       {
-         // res.push_back(arr[pos]);
-         pB.push_back(arr[pos]);
-
-         n--;
-         pos++;
+         if(n >= count)
+         {
+            res = -1;
+         }
       }
-   }
-
-   int nF = pF.size();
-   for(int i = nF-1; i >= 0; i--)
-   {
-      res.push_back(pF[i]);
-   }
-
-   int nB = pB.size();
-   for(int i = 0; i < nB; i++)
-   {
-      res.push_back(pB[i]);
    }
 
    return res;
@@ -59,35 +30,6 @@ vector<int> Pendulum(vector<int> arr)
 
 int main()
 {
-
-   vector<int> arr = {6, 6, 8 ,5 ,10};
-   sort(arr.begin(), arr.end());
-   int n = arr.size();
-   int count = n;
-   int posB = 0;
-   int posE = n-1;
-   vector<int> res;
-
-   while(n > 0)
-   {
-      if (n > 0)
-      {
-         res.insert(res.begin(), arr[posB]);
-
-         n--;
-         posB++;
-      }
-
-      if (n > 0)
-      {
-         res.push_back(arr[posB]);
-
-         n--;
-         posB++;
-      }
-   }
-
-   dispInt(res);
 
    return 0;
 }
