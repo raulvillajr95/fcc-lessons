70516c7b5d15fdde6b4280fb1d5d416c4399f9fd
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-12-01 13:46:21 -0800

    A: ItA 425 (done)

--- main.cpp
+++ main.cpp
@@ -1,32 +1,5 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
-int get_section_id(int n, vector<int> a)
-{
-
-   int Na = a.size();
-   int res = 0;
-   int count = 0;
-   for (int i = 0; i < Na; i++)
-   {
-      count += a[i];
-      if (n >= count)
-      {
-         res++;
-      }
-
-      if(i == Na-1)
-      {
-         if(n >= count)
-         {
-            res = -1;
-         }
-      }
-   }
-
-   return res;
-}
 
 int main()
 {
