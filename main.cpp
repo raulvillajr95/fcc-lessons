28b10e559f51d363ada5d294f441a8dcbdc0c20a
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-12-03 09:54:54 -0800

    Phys QM Practice Problem 8 (done)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+int pyramid(int n)
+{
+   return (-1.0 + sqrt(1.0 - 4.0*(-2*n)))/2.0;
+}
 
 int main()
 {
+   cout << pyramid() << endl;
+   cout << pyramid() << endl;
+   cout << pyramid() << endl;
+   cout << pyramid() << endl;
+   cout << pyramid() << endl;
 
    return 0;
 }
