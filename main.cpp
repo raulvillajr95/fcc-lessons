001df197ba1a715b11eceee13e150a89cd74e21c
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-12-07 17:17:40 -0800

    CW Recursion 101 (done)

--- main.cpp
+++ main.cpp
@@ -1,101 +1,49 @@
 #include <iostream>
-#include <vector>
+#include <utility>
 using namespace std;
 
-void dispStr(vector<string> arr)
+void dispPair(pair<int, int> p)
 {
-   int n = arr.size();
-   for(int i = 0; i < n; i++)
-   {
-      cout << "|" << arr[i] << "| ";
-   }
-   cout << endl;
+   cout << p.first << " " << p.second << endl;
 }
 
-vector<string> sepBySpace(string sentence)
+pair<int, int> solve(int a, int b)
 {
-   vector<string> res;
-
-   int n = sentence.size();
-   string temp = "";
-   for(int i = 0; i < n; i++)
+   if (a == 0 || b == 0)
    {
-      if(sentence[i] != ' ' && i == n-1)
-      {
-         temp += sentence[i];
-         res.push_back(temp);
-         temp = "";
-      }
-      else if (sentence[i] != ' ')
-      {
-         temp += sentence[i];
-      }
-      else if (sentence[i] == ' ')
-      {
-         res.push_back(temp);
-         temp = "";
-      }
+      // cout << "ZERO" << endl;
+      return {a,b};
+   }
+   
+   if (a >= (2*b))
+   {
+      a = a - 2*b;
+      return solve(a,b);
+   }
+   
+   if (b >= (2*a))
+   {
+      b = b - 2*a;
+      return solve(a,b);
    }
 
-   return res;
-}
-
-vector<string> expandPoss(vector<string> arr)
-{
-   vector<string> res;
-
-   int n = arr.size();
-   for(int i = 0; i < n; i++)
-   {
-      for(int j = 0; j < n; j++)
-      {
-         if(i == j)
-         {
-            continue;
-         }
-         else
-         {
-            res.push_back(arr[i] + arr[j]);
-         }
-      }
-   }
-
-   return res;
-}
-
-unsigned solve(const string &nums, unsigned k)
-{
-   // Seperate by space
-   vector<string> numsSep = sepBySpace(nums);
-
-   // Expansion of combos
-   vector<string> numsExp = expandPoss(numsSep);
-
-   int count = 0;
-   int n = numsExp.size();
-   for (int i = 0; i < n; i++)
-   {
-      int numInt = stoi(numsExp[i]);
-      if(numInt % k == 0)
-      {
-         count++;
-      }
-   }
-
-   return count;
+   return {a,b};
 }
 
 int main()
 {
+   // pair<int, int> test1 = solve(6,19);
+   // dispPair(test1);
 
+   // test1 = solve(2,1);
+   // dispPair(test1);
+   
+   pair<int, int> test1 = solve(22,5);
+   dispPair(test1);
    return 0;
 }
 
 /*
-separate as string✅
-expand to string combinations✅
-turn to integers
-check divisibility
 
 
 
