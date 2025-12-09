45fe5fc831feaaaf9b95ce39745cf5123fb4df1a
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-12-09 08:49:23 -0800

    CW Redacted! (done)

--- main.cpp
+++ main.cpp
@@ -1,102 +1,42 @@
 #include <iostream>
-#include <vector>
-#include <utility>
 using namespace std;
 
-void dispIntP(pair<int, int> par)
+bool redacted(const string &doc1, const string &doc2)
 {
-   cout << par.first << " " << par.second << endl;
-}
+   bool res = true;
 
-pair<int, int> evaluatePath(const vector<int>& numbers)
-{
-
-   int lives = 2;
-   int pos = 0;
-   int moves = 0;
-   int n = numbers.size();
-
-   while (numbers[pos] != 0 && lives > 0)
+   int n1 = doc1.size();
+   int n2 = doc2.size();
+   if(n1 != n2)
    {
-      cout << "round pos: " << pos << endl;
-      if(lives == 2)
-      {
-         cout << "value: " << numbers[pos] << endl;
-
-         // Try
-         if ((pos + numbers[pos]) < 0 || (pos + numbers[pos]) >= n)
-         {
-            lives--;
-            continue;
-         }
-
-         pos += numbers[pos];
-         cout << "Life2 pos: " << pos << endl;
-         moves++;
-      } else if (lives == 1)
-      {
-         cout << "value: " << -numbers[pos] << endl;
-         
-         // Try
-         if ((pos - numbers[pos]) < 0 || (pos - numbers[pos]) >= n)
-         {
-            break;
-         }
-
-         pos -= numbers[pos];
-         cout << "Life1 pos: " << pos << endl;
-         moves++;
-      }
-
-      // if(pos < 0)
-      // {
-      //    lives--;
-      //    // if(lives == 0)
-      //    // {
-      //    //    break;
-      //    // }
-
-      //    pos = -pos;
-      //    cout << "too small pos: " << pos << endl;
-      // } else if (pos >= n)
-      // {
-      //    lives--;
-      //    // if(lives == 0)
-      //    // {
-      //    //    break;
-      //    // }
-
-      //    pos = (n-1)-(pos-(n-1));
-      //    cout << "too big pos: " << pos << endl;
-      // }
-      
-      cout << "total moves: " << moves << endl;
-      cout << "----------------" << endl;
+      return false;
    }
 
-   return {pos, moves};
+   for(int i = 0; i < n1; i++)
+   {
+      if(doc2[i] == '\n' && doc1[i] != '\n')
+      {
+         res = false;
+         break;
+      }
+
+      if(doc1[i] != 'X' && (doc1[i] != doc2[i]))
+      {
+         res = false;
+         break;
+      }
+
+   }
+
+   return res;
 }
 
 int main()
 {
-   vector<int> test1 = {3, 4, 1, 1, -3, 1};
-   pair<int, int> par1 = evaluatePath(test1);
-   dispIntP(par1); // 4,5
-
-   // vector<int> test2 = {2, 1, -3, 4};
-   // pair<int, int> par2 = evaluatePath(test2);
-   // dispIntP(par2); // 2,1
-
    return 0;
 }
 
 /*
-if char: 
-   could be same char or X
-if newline:
-   must be a newline
-
-must be same length
 
 
 check for negative bit
