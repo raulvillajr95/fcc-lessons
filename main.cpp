bb06e8716a3660620cbb37429f8ff2c2aea0c920
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-12-09 09:30:24 -0800

    CW Sub-array elements sum (done)

--- main.cpp
+++ main.cpp
@@ -1,43 +1,56 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-bool redacted(const string &doc1, const string &doc2)
+long elementsSum(const vector<vector<int>>& arr, int d = 0)
 {
-   bool res = true;
-
-   int n1 = doc1.size();
-   int n2 = doc2.size();
-   if(n1 != n2)
+   int n = arr.size();
+   long count = 0;
+   for(int i = 0; i < n; i++)
    {
-      return false;
+      unsigned long pos = n - i - 1;
+
+      // if(arr[i].size() == 0)
+      // {
+
+      // }
+
+      if (arr[i].size() == 0 || pos > arr[i].size()-1)
+      {
+         count += d;
+      }
+      else
+      {
+         count += arr[i][pos];
+      }
    }
 
-   for(int i = 0; i < n1; i++)
-   {
-      if(doc2[i] == '\n' && doc1[i] != '\n')
-      {
-         res = false;
-         break;
-      }
-
-      if(doc1[i] != 'X' && (doc1[i] != doc2[i]))
-      {
-         res = false;
-         break;
-      }
-
-   }
-
-   return res;
+   return count;
 }
 
 int main()
 {
+   vector<vector<int>> test1 = {{3, 2, 1, 0}, {4, 6, 5, 3, 2}, {9, 8, 7, 4}};
+   cout << elementsSum(test1) << endl;
+
+   vector<vector<int>> test2 = {{}, {4, 6, 5, 3, 2}, {9, 8, 7, 4}};
+   cout << elementsSum(test2) << endl;
+
+   vector<vector<int>> test3 = {{3, 2, 1, 0}, {4, 6, 5, 3, 2}, {}};
+   cout << elementsSum(test3) << endl;
+
+
    return 0;
 }
 
 /*
-
+n = size of main array
+grab number n-1 from 1st sub
+grab number n-2 from 2nd sub
+...
+if n-num is too big for sub
+   get number d
+sum all number
 
 check for negative bit
 check for exponents
