8e2c321da870c50ec34102904fa0ccfb49e9c7ca
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-12-10 08:17:11 -0800

    CW Offload your work! (done)

--- main.cpp
+++ main.cpp
@@ -3,113 +3,66 @@
 #include <utility>
 using namespace std;
 
-// int totMinFreeL(vector<pair<int,int>> freeLanc)
-// {
-//    int count = 0;
-//    int n = freeLanc.size();
-//    for (int i = 0; i < n; i++)
-//    {
-//       count += freeLanc[i].first * 60;
-//       count += freeLanc[i].second;
-//    }
-
-//    return count;
-// }
-
-// pair<int,int> hrsMins(int mins)
-// {
-//    // convert mins to [hrs, mins]
-// }
-
-// string workNeeded(int projMin, vector<pair<int,int>> freeL)
-// {
-//    int freeLMins = totMinFreeL(freeL);
-//    int myMin = projMin - freeLMins;
-//    if(myMin < 0)
-//       myMin = 0;
-
-   
-
-
-// }
-
-
-//////// Code Signal
-void dispInt(vector<int> nums)
+int totMinFreeL(vector<pair<int,int>> freeLanc)
 {
-   int n = nums.size();
+   int count = 0;
+   int n = freeLanc.size();
    for (int i = 0; i < n; i++)
    {
-      cout << nums[i] << " ";
+      count += freeLanc[i].first * 60;
+      count += freeLanc[i].second;
    }
-   cout << endl;
+
+   return count;
 }
 
-vector<int> solution(const vector<int> &numbers, int obstacle)
+pair<int,int> hrsMins(int mins)
 {
-   vector<int> res;
+   // convert mins to [hrs, mins]
+   int hours = 0;
 
-   int n = numbers.size();
-   for (int i = 0; i < n; i++)
+   hours = mins / 60;
+   mins -= hours*60;
+
+   return {hours, mins};
+}
+
+string workNeeded(int projMin, vector<pair<int,int>> freeL)
+{
+   int freeLMins = totMinFreeL(freeL);
+   int myMin = projMin - freeLMins;
+   string message = "";
+   if(myMin < 0)
    {
-      int pos = i;
-      int steps = 0;
+      myMin = 0;
+      message = "Easy Money!";
+   } else
+   {
 
-      // Debug
-      // cout << i << endl;
-
-      // -1 Exit
-      if(numbers[i] == obstacle)
+      pair<int, int> hM = hrsMins(myMin);
+      if (hM.first == 0 && hM.second == 0)
       {
-         // cout << "-1 Exit" << endl;
-         res.push_back(-1);
-         continue;
-      }
-
-      while(pos < n)
+         message = "Easy Money!";
+      } else
       {
-         // cout << pos << endl;
-         if(numbers[pos] == obstacle)
-         {
-            res.push_back(-1);
-            steps++;
-            break;
-         }
-
-         pos += numbers[pos];
-         steps++;
-      }
-
-      // if(numbers[pos] == obstacle)
-      // {
-      //    res.push_back(-1);
-      //    continue;
-      // } else
-      // {
-
-      //    res.push_back(steps);
-      // }
-
-      if(numbers[pos] != obstacle)
-      {
-         res.push_back(steps);
+         message += "I need to work ";
+         message += to_string(hM.first);
+         message += " hour(s) and ";
+         message += to_string(hM.second);
+         message += " minute(s)";
       }
    }
-   
-   return res;
+
+   return message;
 }
+
+// string addLargeNumbers(string num1, string num2)
+// {
+//    int 
+// }
 
 int main()
 {
-   vector<int> test1 = solution({5, 3, 2, 6, 2, 1, 7}, 3);
-   dispInt(test1); // 3, -1, 3, 1, 2, 2, 1
-
-   vector<int> test2 = solution({2, 4, 2, 1, 3, 2, 8, 4, 7}, 4);
-   dispInt(test2); 
-   // -1, -1, -1, -1, -1, -1, 1, -1, 1
-   // -1 -1 -1 -1 -1 -1 1 -1 1
-   // -1, -1, -1, -1, -1, -1, 1, -1, 1
-   // -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 1 -1 1
 
    return 0;
 }
