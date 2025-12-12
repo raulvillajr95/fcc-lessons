0f872bba21381896ce7636301a5466d10f59557c
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-12-11 19:54:47 -0800

    CS String Subtraction Without Conversion (done)

--- main.cpp
+++ main.cpp
@@ -1,116 +1,115 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <iomanip>
-#include <sstream>
 using namespace std;
 
-// string solution(const string &num1, const string &num2)
-// {
-//    vector<char> nums;
+string remLead0(string num)
+{
+   int n = num.size();
+   string res = "";
+   bool hasNum = false;
 
-//    int n1 = num1.size();
-//    int n2 = num2.size();
+   for(int i = 0; i < n; i++)
+   {
+      if(num[i] != '0')
+      {
+         hasNum = true;
+      }
 
-//    if (n2 < n1)
-//    {
-//       // fill with zeros
+      if(hasNum)
+      {
+         res += num[i];
+      }
+   }
+
+   if(!hasNum)
+   {
+      res = "0";
+   }
+   return res;
+}
+
+string fillW0(string num, int size)
+{
+   int n = num.size();
+   int num0 = size - n;
+   string zeros = "";
+   for(int i = 0; i < num0; i++)
+   {
+      zeros += "0";
+   }
+
+   return zeros + num;
+}
+
+
+string solution(const string &num1, const string &num2)
+{
+   vector<char> nums;
+
+   int n1 = num1.size();
+   int n2 = num2.size();
+
+   // cout << n1 << " " << n2 << endl;
+
+   string filledW0 = num2;
+   if (n2 < n1)
+   {
+      filledW0 = fillW0(num2, n1);
       
-//       n2 = n1;
-//    }
+      n2 = n1;
+   }
 
-//    int carry = 0;
+   int carry = 0;
 
-//    // Proceed through longest num for 0's
-//    for(int i = n1-1; i >= 0; i--)
-//    {
-//       int n1P = num1[i + (n1-n2)] - '0' - carry;
-//       int n2P = num2[i] - '0';
-//       char c = ' ';
+   // Proceed through longest num for 0's
+   for(int i = n1-1; i >= 0; i--)
+   {
+      int n1P = num1[i + (n1-n2)] - '0' - carry;
+      int n2P = filledW0[i] - '0';
+      char c = ' ';
 
-//       if(n1P >= n2P)
-//       {
-//          c = (n1P - n2P) + '0';
-//          nums.push_back(c);
-//          carry = 0;
-//       } else {
-//          c = (n1P + 10 - n2P) + '0';
-//          nums.push_back(c);
-//          carry = 1;
-//       }
-//    }
+      if(n1P >= n2P)
+      {
+         c = (n1P - n2P) + '0';
+         nums.push_back(c);
+         carry = 0;
+      } else {
+         c = (n1P + 10 - n2P) + '0';
+         nums.push_back(c);
+         carry = 1;
+      }
+   }
 
-//    reverse(nums.begin(), nums.end());
-//    return string(nums.begin(), nums.end());
-// }
+   reverse(nums.begin(), nums.end());
+   string res = string(nums.begin(), nums.end());
+   string no0 = remLead0(res);
+   return no0;
+}
 
 //////////////////
 
-string createIngredients(double sa = 0, double se = 0, double ch = 0)
-{
-   if(sa < 0 || se < 0 || ch < 0)
-   {
-      return "";
-   }
 
-   if (sa == 0 && se == 0 && ch == 0)
-   {
-      return "[60.00;180.00;5.00]";
-   }
-
-   if (sa > 0)
-   {
-      se = sa*3.0;
-      ch = sa/12.0;
-   } else if (se > 0)
-   {
-      sa = se/3.0;
-      ch = se/36.0;
-   } else if (ch > 0)
-   {
-      sa = ch*12.0;
-      se = ch*36.0;
-   }
-
-   stringstream ss;
-   ss << "[" << fixed << setprecision(2) << sa << ";";
-   ss << fixed << setprecision(2) << se << ";";
-   ss << fixed << setprecision(2) << ch << "]";
-
-   string message = ss.str();
-   // message += "[" + to_string(sa) + ";";
-   // message += to_string(se) + ";";
-   // message += to_string(ch) + "]";
-
-   return message;
-}
 
 int main()
 {
-   // cout << solution("9", "2") << endl;
-   // cout << solution("70", "22") << endl;
-   // cout << solution("300", "299") << endl;
-   // cout << solution("1000", "1") << endl;
-   // cout << solution("10", "1") << endl;
-   // cout << solution("100", "1") << endl;
+   cout << solution("70", "22") << endl;
+   cout << solution("100", "1") << endl; // 99
+   cout << solution("1000", "999") << endl; // 1
+   cout << solution("10000", "1000") << endl; // 9000
+   cout << solution("123456789101112131415", "98765432101012131415") << endl; // 24691357000100000000
 
    ////////////
-   cout << createIngredients() << endl;
-   cout << createIngredients(60) << endl;
-   cout << createIngredients(0,180) << endl;
-   cout << createIngredients(100) << endl;
+
 
 
    return 0;
 }
 
 /*
-ratio: 12/36/1
-get only 2 decimal places
+If all zeros, keep only 1 '0'
 
+/////////////
 
-check for negative bit
-check for exponents
-check for mantissa
 
 */
