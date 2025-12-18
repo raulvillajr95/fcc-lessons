1ebb09ad18efe65f4827ac0477a0c454d6fdb0fe
Raul <84646523+raulvillajr95@users.noreply.github.com>
2025-12-17 18:52:02 -0800

    AoPS 5.37 (partial)

--- main.cpp
+++ main.cpp
@@ -12,30 +12,23 @@ void dispInt(vector<int> arr)
    cout << endl;
 }
 
-vector<int> solution(const vector<int>& arrayA, const vector<int>& arrayB)
+int solution(const vector<int> &arrayA, const vector<int> &arrayB, const vector<int> &arrayC)
 {
-
-   bool ran = false;
    int pos = 0;
-   vector<int> res;
-   // for(int i = 0; i < 5; i++)
-   while(pos != 0 || ran != true)
+
+   for (int i = 0; i < 3; i++)
    {
-      cout << "added: " << arrayA[pos] << endl;
-      res.push_back(arrayA[pos]);
-
-      ran = true;
-
+      cout << arrayA[pos] << endl;
       pos = arrayA[pos];
-      cout << "first: " << pos << endl;
 
-      pos = arrayB[pos-1] - 1;
-      cout << "second: " << pos << endl;
+      cout << arrayB[pos] << endl;
+      pos = arrayB[pos];
 
-      cout << "----------" << endl;
+      cout << arrayC[pos] << endl;
+      pos = arrayC[pos];
    }
 
-   return res;
+   return 0;
 }
 
 
@@ -45,9 +38,11 @@ vector<int> solution(const vector<int>& arrayA, const vector<int>& arrayB)
 
 int main()
 {
-   // vector<int> arrA = {1,3,2,5,4};
-   // vector<int> arrB = {5,4,3,2,1};
-   // dispInt(solution(arrA, arrB));
+   vector<int> arrA = {2,1,3,0};
+   vector<int> arrB = {1,3,2,4};
+   vector<int> arrC = {4,2,5,1};
+
+   solution(arrA, arrB, arrC);
 
    ////////////
 
