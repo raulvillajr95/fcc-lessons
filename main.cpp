d49d504c538efa2c1039dfb9e2525b0261238a61
Raul <84646523+raulvillajr95@users.noreply.github.com>
2026-03-17 21:39:21 -0700

    CW Count the smiley faces! (done)

--- main.cpp
+++ main.cpp
@@ -1,110 +1,54 @@
 #include <iostream>
 #include <vector>
-#include <climits>
-#include <cmath>
 using namespace std;
 
-// void dispInt(const vector<int> arr)
-// {
-//    int n = arr.size();
-//    for (int i = 0; i < n; i++)
-//    {
-//       cout << arr[i] << " ";
-//    }
-//    cout << endl;
-// }
-
-vector<int> solution(const vector<int>& roadA, const vector<int>& roadB)
+int countSmileys(vector<string> arr)
 {
-   int nA = roadA.size();
-   int nB = roadB.size();
-   vector<int> counts;
-   for(int i = 0; i < nA; i++)
-   {
-      int count = 0;
-      vector<bool> visitA(nA);
-      vector<bool> visitB(nB);
-      int pos = i;
-      while(true)
-      {
-         if(visitA[pos])
-         {
-            break;
-         } else {
-            visitA[pos] = true;
-         }
-         pos = roadA[pos];
-         count++;
-
-         if(visitB[pos])
-         {
-            break;
-         } else {
-            visitB[pos] = true;
-         }
-         pos = roadB[pos];
-         count++;
-      }
-
-      counts.push_back(count);
-   }
-
-   return counts;
-}
-
-
-
-//////////////////
-string firstCap(string word)
-{
-   string res = "";
-   int n = word.size();
+   int count = 0;
+   int n = arr.size();
    for(int i = 0; i < n; i++)
    {
-      if(i = 0)
+      int sizeL = arr[i].size();
+      if(sizeL==2)
       {
-         res += toupper(word[i]);
-      } else
+         char f = arr[i][0];
+         char s = arr[i][1];
+         if(f == ':' || f == ';')
+         {
+            if(s == ')' || s == 'D')
+            {
+               count++;
+            }
+         }
+      } else if (sizeL==3)
       {
-         res += tolower(word[i]);
+         char f = arr[i][0];
+         char s = arr[i][1];
+         char t = arr[i][2];
+         if(f == ':' || f == ';')
+         {
+            if(s == '-' || s == '~')
+            {
+               if(t == ')' || t == 'D')
+               {
+                  count++;
+               }
+            }
+         }
       }
    }
 
-   return res;
+   return count;
 }
 
-vector<string> capMe(const vector<string>& strings)
+int main(int argc, char* argv[])
 {
-   vector<string> res;
-
-   int n = strings.size();
-   for(int i = 0; i < n; i++)
-   {
-      res.push_back(firstCap(strings[i]));
-   }
-
-   return res;
-}
-
-
-int main()
-{
-
-
-
-
-   ////////////
-
+   vector<string> test = {":D", ":~)", ";~)", ":)"};
+   cout << countSmileys(test) << endl;
    return 0;
 }
 
 /*
-
-get maxes
-check repetition
-
-/////////////
-
 
 
 */
--- prac.py
+++ prac.py
@@ -1,1 +1,14 @@
-print("hello world")
+def fibonacci(n):
+  if(n == 1):
+    return 0
+  elif (n == 2):
+    return 1
+  else:
+    return fibonacci(n-1) + fibonacci(n-2)
+
+
+for n in range(0,10):
+  # print(n)
+  print(fibonacci(n+1))
+
+# print(fibonacci(1))
