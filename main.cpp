a27a2d15574a71d9139d11292e34c8149e173578
Raul <84646523+raulvillajr95@users.noreply.github.com>
2026-03-19 21:49:37 -0700

    CW Bin to Decimal (done)

--- main.cpp
+++ main.cpp
@@ -1,50 +1,92 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-int countSmileys(vector<string> arr)
+bool inArr(vector<int> arr, int n)
 {
-   int count = 0;
-   int n = arr.size();
-   for(int i = 0; i < n; i++)
+   bool in = false;
+   for(int i = 0; i < arr.size(); i++)
    {
-      int sizeL = arr[i].size();
-      if(sizeL==2)
+      if(arr[i] == n)
       {
-         char f = arr[i][0];
-         char s = arr[i][1];
-         if(f == ':' || f == ';')
-         {
-            if(s == ')' || s == 'D')
-            {
-               count++;
-            }
-         }
-      } else if (sizeL==3)
-      {
-         char f = arr[i][0];
-         char s = arr[i][1];
-         char t = arr[i][2];
-         if(f == ':' || f == ';')
-         {
-            if(s == '-' || s == '~')
-            {
-               if(t == ')' || t == 'D')
-               {
-                  count++;
-               }
-            }
-         }
+         in = true;
+         break;
       }
    }
 
-   return count;
+   return in;
 }
+
+vector<int> uniqueInt(vector<int> arr)
+{
+   vector<int> res;
+
+   for(int i = 0; i < arr.size(); i++)
+   {
+      if(!inArr(res,arr[i]))
+      {
+         res.push_back(arr[i]);
+      }
+   }
+
+   return res;
+}
+
+unsigned long power2(int cnt)
+{
+   unsigned long res = 1;
+   for(int i = 0; i < cnt; i++)
+   {
+      res *= 2;
+   }
+
+   return res;
+}
+
+unsigned long binToDec(const string &str)
+{
+   unsigned long n = str.size();
+
+   unsigned long res = 0;
+   
+   int count = 0;
+   for(int i = n-1; i >= 0; i--)
+   {
+      if(str[i] == '1')
+      {
+         res += power2(count);
+      }
+      count ++;
+   }
+
+   return res;
+}
+
+vector<int> deletNth(vector<int> arr, int n)
+{
+   vector<int> uni = uniqueInt(arr);
+   vector<int> res;
+
+
+   return {};
+}
+
+
 
 int main(int argc, char* argv[])
 {
-   vector<string> test = {":D", ":~)", ";~)", ":)"};
-   cout << countSmileys(test) << endl;
+   cout << binToDec("0") << endl;
+   cout << binToDec("1") << endl;
+   cout << binToDec("10") << endl;
+   cout << binToDec("11") << endl;
+   cout << binToDec("110") << endl;
+   cout << binToDec("1111111111111111111111111111111111111111111111111111111111111111") << endl;
+
+
+unsigned long int i = -1;
+std::cout << i;
+
    return 0;
 }
 
