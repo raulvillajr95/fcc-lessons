92a728796cb3f3d4c6258ec4a362d56da46bee3f
Raul <84646523+raulvillajr95@users.noreply.github.com>
2026-05-25 10:40:29 -0700

    CSCI-104 notes memAlloc

--- main.cpp
+++ main.cpp
@@ -1,96 +1,22 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
 using namespace std;
 
-bool inArr(vector<int> arr, int n)
+// Computes rectangle area
+//    prints it, & returns it
+int* area3(int, int);
+
+int main()
 {
-   bool in = false;
-   for(int i = 0; i < arr.size(); i++)
-   {
-      if(arr[i] == n)
-      {
-         in = true;
-         break;
-      }
-   }
-
-   return in;
-}
-
-vector<int> uniqueInt(vector<int> arr)
-{
-   vector<int> res;
-
-   for(int i = 0; i < arr.size(); i++)
-   {
-      if(!inArr(res,arr[i]))
-      {
-         res.push_back(arr[i]);
-      }
-   }
-
-   return res;
-}
-
-unsigned long power2(int cnt)
-{
-   unsigned long res = 1;
-   for(int i = 0; i < cnt; i++)
-   {
-      res *= 2;
-   }
-
-   return res;
-}
-
-unsigned long binToDec(const string &str)
-{
-   unsigned long n = str.size();
-
-   unsigned long res = 0;
-   
-   int count = 0;
-   for(int i = n-1; i >= 0; i--)
-   {
-      if(str[i] == '1')
-      {
-         res += power2(count);
-      }
-      count ++;
-   }
-
-   return res;
-}
-
-vector<int> deletNth(vector<int> arr, int n)
-{
-   vector<int> uni = uniqueInt(arr);
-   vector<int> res;
-
-
-   return {};
-}
-
-
-
-int main(int argc, char* argv[])
-{
-   cout << binToDec("0") << endl;
-   cout << binToDec("1") << endl;
-   cout << binToDec("10") << endl;
-   cout << binToDec("11") << endl;
-   cout << binToDec("110") << endl;
-   cout << binToDec("1111111111111111111111111111111111111111111111111111111111111111") << endl;
-
-
-unsigned long int i = -1;
-std::cout << i;
-
+   int wid = 8, len = 5, *a;
+   a = area3(wid,len);
+   cout << *a << endl; // 40
+   delete a;
    return 0;
 }
 
-/*
-
-
-*/
+int* area3(int w, int l)
+{
+   int* ans = new int;
+   *ans = w * l;
+   return ans;
+}
---
+++ nm-lc4.m
@@ -0,0 +1,15 @@
+% Octave Environment Test Script
+fprintf('===========================\n');
+fprintf('SUCCESS! Octave is running in VS Code.\n');
+fprintf('===========================\n');
+
+% Define two matrices
+A = [1, 2; 3, 4];
+B = [5, 6; 7, 8];
+
+% Multiply them together
+C = A * B;
+
+% Display the result
+disp('Matrix Multiplication Output (A * B):');
+disp(C);
